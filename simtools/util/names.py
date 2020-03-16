acb8a4aefc4f5c6cd8c4937d04dc98b2973c4814
RaulRPrado <raul.prado@usp.br>
2020-03-16 16:28:35 +0100

    more progress on the corsika config
    Former-commit-id: 5e35ebbb55a06f69f02c3f34c753cbdf103abeaa

--- simtools/util/names.py
+++ simtools/util/names.py
@@ -15,7 +15,8 @@ def validateName(name, allNames):
         raise ValueError('Invalid name {}'.format(name))
     for mainName in allNames.keys():
         if name.lower() in allNames[mainName] + [mainName.lower()]:
-            logger.debug('Correcting name {} -> {}'.format(name, mainName))
+            if name != mainName:
+                logger.debug('Correcting name {} -> {}'.format(name, mainName))
             return mainName
 
 
@@ -53,6 +54,15 @@ allSimtelModeNames = {
     'Trigger': []
 }
 
+allArrayNames = {
+    '4LST': ['4-lst', '4lst'],
+    '1LST': ['1-lst', '1lst'],
+    '4MST': ['4-mst', '4mst'],
+    '1MST': ['1-mst', 'mst'],
+    '4SST': ['4-sst', '4sst'],
+    '1SST': ['1-sst', 'sst']
+}
+
 
 def rayTracingFileName(telescopeType, sourceDistance, zenithAngle, offAxisAngle, label, base):
     ''' base has to be log, stars or photons'''
@@ -72,4 +82,15 @@ def rayTracingResultsFileName(telescopeType, sourceDistance, zenithAngle, label)
     name = 'ray-tracing-{}-d{:.1f}-za{:.1f}'.format(telescopeType, sourceDistance, zenithAngle)
     name += '_{}'.format(label) if label is not None else ''
     name += '.cvs'
+    return name
+
+
+def corsikaConfigFileName(arrayName, site, zenith, viewCone, label=None):
+    def isDifuse(viewCone):
+        return viewCone[0] != 0 or viewCone[1] != 0
+
+    name = 'corsika-config-{}-{}-za{:.1f}-{:.1f}'.format(arrayName, site, zenith[0], zenith[1])
+    name += '-cone{:.1f}-{:.1f}'.format(viewCone[0], viewCone[1]) if isDifuse(viewCone) else ''
+    name += '_{}'.format(label) if label is not None else ''
+    name += '.txt'
     return name
