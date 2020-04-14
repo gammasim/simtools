a35b0f1583dcc599154b66e3074c9389bf4d8318
RaulRPrado <raul.prado@usp.br>
2020-04-14 11:17:51 +0200

    start implementing integral curve

--- applications/derive_mirror_reflection_random_angle.py
+++ applications/derive_mirror_reflection_random_angle.py
@@ -14,29 +14,9 @@ from simtools.ray_tracing import RayTracing
 from simtools.telescope_model import TelescopeModel
 
 logger = logging.getLogger(__name__)
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger().setLevel(logging.INFO)
 
 config = cfg.loadConfig()
-
-
-def computeRndaRange(tel, n=10):
-    rndaStd = tel.getParameter('mirror_reflection_random_angle')
-    rndaStd = rndaStd.split()
-    rndaStd = float(rndaStd[0])
-    rndaRange = np.linspace(rndaStd * 0.8, rndaStd * 1.2, n)
-    return np.round(rndaRange, 8)
-
-
-def loadResults(force=False):
-    fileName = 'results.ecsv'
-    if Path(fileName).exists() and not force:
-        return dict(ascii.read(fileName, format='basic'))
-    else:
-        res = dict()
-        res['rnda'] = list()
-        res['mean_d80_cm'] = list()
-        res['sig_d80_cm'] = list()
-        return res
 
 
 def sortResults(res):
@@ -75,7 +55,7 @@ if __name__ == '__main__':
             filesLocation=config['outputLocation'],
             telescopeModel=tel,
             singleMirrorMode=True,
-            mirrorNumbers='all'  # list(range(1, 10))
+            mirrorNumbers='all'  # list(range(1, 50))
         )
         ray.simulate(test=False, force=True)
         ray.analyze(force=True)
@@ -125,6 +105,15 @@ if __name__ == '__main__':
     rndaOpt = np.interp(x=measMean, xp=results['mean'], fp=results['rnda'])
     meanD80, sigD80 = run(rndaOpt, plot=True)
 
+    print('--- Measured -----')
+    print('Mean = {:.3f}, StdDev = {:.3f}'.format(measMean, measSig))
+    print('--- Simulated -----')
+    print('Mean = {:.3f}, StdDev = {:.3f}'.format(meanD80, sigD80))
+    print('--- mirror_random_reflection_angle ----')
+    print('Previous value = {:.6f}'.format(rndaStart))
+    print('New value = {:.6f}'.format(rndaOpt))
+    print('-------')
+
     plt.figure(figsize=(8, 6), tight_layout=True)
     ax = plt.gca()
     ax.set_xlabel('mirror_random_reflection_angle')
@@ -144,12 +133,15 @@ if __name__ == '__main__':
         yerr=[sigD80],
         color='r',
         marker='o',
-        linestyle='none'
+        linestyle='none',
+        label='rnda = {:.6f} (mean = {:.3f}, sig = {:.3f})'.format(rndaOpt, meanD80, sigD80)
     )
 
     xlim = ax.get_xlim()
     ax.plot(xlim, [measMean, measMean], color='k', linestyle='-')
     ax.plot(xlim, [measMean + measSig, measMean + measSig], color='k', linestyle=':')
     ax.plot(xlim, [measMean - measSig, measMean - measSig], color='k', linestyle=':')
+
+    ax.legend(frameon=False, loc='upper left')
 
     plt.show()
