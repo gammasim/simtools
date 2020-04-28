942b7baa7a90e2b3df37f4b5d6dfe6ded7aa03ef
RaulRPrado <raul.prado@usp.br>
2020-04-28 11:52:42 +0200

    collectrguments reimplemented

--- simtools/tests/test_simtel_runner.py
+++ simtools/tests/test_simtel_runner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import logging
+from astropy import units
 
 from simtools.util import config as cfg
 from simtools.simtel_runner import SimtelRunner
@@ -20,13 +21,13 @@ def test_ray_tracing_mode():
     simtel = SimtelRunner(
         mode='ray-tracing',
         telescopeModel=tel,
-        zenithAngle=20,
+        zenithAngle=20 * units.deg,
         offAxisAngle=2,
         sourceDistance=10
     )
 
     logging.info(simtel)
-    simtel.run(test=True, force=True)
+    # simtel.run(test=True, force=True)
 
 
 if __name__ == '__main__':
