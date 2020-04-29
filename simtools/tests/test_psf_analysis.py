0d5e504ba02c8dc206da1a83a6304955f9bcec40
RaulRPrado <raul.prado@usp.br>
2020-04-29 16:43:20 +0200

    reviewing and docing psf_analysis - in progress

---
+++ simtools/tests/test_psf_analysis.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import logging
+
+import yaml
+
+
+from simtools.psf_analysis import PSFImage
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def test_reading_simtel_file():
+    testFile = 'data/test-data/photons-LST-d12.0-za20.0-off0.000_lst_integral.lis'
+    image = PSFImage(focalLength=2800.)
+    image.readSimtelFile(testFile)
+    logger.info(image.getPSF(0.8, 'cm'))
+
+
+if __name__ == '__main__':
+
+    test_reading_simtel_file()
