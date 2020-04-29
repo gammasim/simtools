17e7339cd6ec65eeb62a4a2e437f889f0a408413
RaulRPrado <raul.prado@usp.br>
2020-04-29 16:51:33 +0200

    test-data added to the config file
    Former-commit-id: d0b0cebb010c059b6639424babdebd5656fbe380

--- simtools/tests/test_psf_analysis.py
+++ simtools/tests/test_psf_analysis.py
@@ -6,13 +6,15 @@ import yaml
 
 
 from simtools.psf_analysis import PSFImage
+# from simtools.util import config as cfg
+import simtools.io_handler as io
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 def test_reading_simtel_file():
-    testFile = 'data/test-data/photons-LST-d12.0-za20.0-off0.000_lst_integral.lis'
+    testFile = io.getTestDataFile('photons-LST-d12.0-za20.0-off0.000_lst_integral.lis')
     image = PSFImage(focalLength=2800.)
     image.readSimtelFile(testFile)
     logger.info(image.getPSF(0.8, 'cm'))
