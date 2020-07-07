0d5f9e6ad4e3eb539237eeb2af881e94fcae210d
Orel Gueta <orel.gueta@desy.de>
2020-07-07 16:42:39 +0200

    First rough implementation of the basic DB functions

--- simtools/tests/test_db_handler.py
+++ simtools/tests/test_db_handler.py
@@ -10,48 +10,71 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-def test_get_model_parameters():
+def test_readring_db_lst():
 
-    pars = db.getModelParameters('lst', 'prod4')
     logger.info('----LST-----')
-    logger.info(pars)
+    pars = db.getModelParameters('north-lst-1', 'prod4')
+    assert(pars['parabolic_dish']['Value'] == 1)
+    assert(pars['camera_pixels']['Value'] == 1855)
 
-    pars = db.getModelParameters('sst', 'prod4')
-    logger.info('----SST-----')
-    logger.info(pars)
+    return
 
-    pars = db.getModelParameters('mst-flashcam', 'prod4')
+
+def test_readring_db_mst_nc():
+
+    logger.info('----MST-NectarCam-----')
+    pars = db.getModelParameters('north-mst-nectarcam-d', 'prod4')
+    assert(pars['camera_pixels']['Value'] == 1855)
+
+    return
+
+
+def test_readring_db_mst_fc():
+
     logger.info('----MST-FlashCam-----')
-    logger.info(pars)
+    pars = db.getModelParameters('north-mst-flashcam-d', 'prod4')
+    assert(pars['camera_pixels']['Value'] == 1764)
 
     return
 
 
-def test_get_model_file():
+def test_readring_db_sst():
 
-    tel = TelescopeModel(
-        telescopeType='lst',
-        site='south',
-        version='prod4',
-        label='test-lst'
-    )
-
-    fileName = tel.getParameter('camera_config_file')
-
-    logger.info('FileName = {}'.format(fileName))
-
-    file = db.getModelFile(fileName)
-
-    logger.info('FilePath = {}'.format(file))
-
-    destDir = Path.cwd()
-    db.writeModelFile(fileName, destDir)
+    logger.info('----SST-----')
+    pars = db.getModelParameters('south-sst-d', 'prod4')
+    assert(pars['camera_pixels']['Value'] == 2048)
 
     return
+
+
+# def test_get_model_file():
+
+#     tel = TelescopeModel(
+#         telescopeType='lst',
+#         site='south',
+#         version='prod4',
+#         label='test-lst'
+#     )
+
+#     fileName = tel.getParameter('camera_config_file')
+
+#     logger.info('FileName = {}'.format(fileName))
+
+#     file = db.getModelFile(fileName)
+
+#     logger.info('FilePath = {}'.format(file))
+
+#     destDir = Path.cwd()
+#     db.writeModelFile(fileName, destDir)
+
+#     return
 
 
 if __name__ == '__main__':
 
-    # test_get_model_parameters()
-    test_get_model_file()
+    # test_get_model_file()
+    test_readring_db_lst()
+    test_readring_db_mst_nc()
+    test_readring_db_mst_fc()
+    test_readring_db_sst()
     pass
