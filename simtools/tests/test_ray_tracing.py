1dbc6d501106495330967107589cdf58474a4b5f
RaulRPrado <raul.prado@usp.br>
2020-07-27 15:26:31 +0200

    fixing the issue with units

--- simtools/tests/test_ray_tracing.py
+++ simtools/tests/test_ray_tracing.py
@@ -221,9 +221,9 @@ def test_integral_curve():
 
 if __name__ == '__main__':
 
-    # test_ssts()
+    test_ssts()
     # test_rx()
-    test_single_mirror()
+    # test_single_mirror()
     # test_plot_image()
     # test_integral_curve()
     pass
