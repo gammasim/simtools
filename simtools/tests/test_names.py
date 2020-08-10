d15b7de4ce5bdd28ed6b0612fef8516f90fb5337
RaulRPrado <raul.prado@usp.br>
2020-08-10 10:55:49 +0200

    adding test_names

---
+++ simtools/tests/test_names.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import logging
+from simtools.util import names
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+def test_validate_telescope_names():
+    newName = names.validateTelescopeName('north-sst-D')
+    print(newName) 
+
+
+def test_validate_other_names():
+    modelVersion = names.validateModelVersionName('p4')
+    print(modelVersion) 
+
+if __name__ == '__main__':
+
+    test_validate_telescope_names()
+    test_validate_other_names()
+    pass
