655157955b90004451afc0220dd9d033aeab1e1b
Orel Gueta <orel.gueta@desy.de>
2020-08-17 11:13:36 +0200

    Change list of functions

--- simtools/db_handler.py
+++ simtools/db_handler.py
@@ -22,20 +22,7 @@ import simtools.config as cfg
 from simtools.util import names
 from simtools.util.model import getTelescopeClass
 
-__all__ = [
-    'getArrayDB',
-    'getModelParameters',
-    'readMongoDB',
-    'writeModelFile',
-    'getModelFile',
-    'getFileMongoDB',
-    'writeFileFromMongoToDisk',
-    'copyTelescope',
-    'deleteQuery',
-    'updateParameter',
-    'addParameter',
-    'addNewParameter'
-]
+__all__ = ['DatabaseHandler']
 
 
 class DatabaseHandler:
