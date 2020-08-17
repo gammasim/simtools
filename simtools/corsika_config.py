a3a71350976051245c6c5acf273df8d49650cdab
Orel Gueta <orel.gueta@desy.de>
2020-08-17 17:14:43 +0200

    Make logger persistent in CorsikaConfig class
    Former-commit-id: de432c8c3522fda35f3faf299d035cc53af35a4f

--- simtools/corsika_config.py
+++ simtools/corsika_config.py
@@ -15,8 +15,6 @@ from simtools.model.array_model import getArray
 
 __all__ = ['CorsikaConfig']
 
-logger = logging.getLogger(__name__)
-
 
 class RequiredInputNotGiven(Exception):
     pass
@@ -32,66 +30,6 @@ class ArgumentWithWrongUnit(Exception):
 
 class InvalidPrimary(Exception):
     pass
-
-
-def _writeTelescopes(file, array):
-    '''
-    Write telescope positions in the corsika input file.
-
-    Parameters
-    ----------
-    file: file
-        File where the telescope positions will be written.
-    array: str
-        Array type.
-    '''
-    logger.warning('Function is changing the inputs - Make it return the string instead')
-    mToCm = 1e2
-    for n, tel in array.items():
-        file.write('\nTELESCOPE {} {} {} {} # {}'.format(
-            tel['xPos'] * mToCm,
-            tel['yPos'] * mToCm,
-            cors_pars.TELESCOPE_Z[tel['size']] * mToCm,
-            cors_pars.TELESCOPE_SPHERE_RADIUS[tel['size']] * mToCm,
-            tel['size']
-        ))
-    file.write('\n')
-
-
-def _writeSeeds(file, seeds):
-    '''
-    Write seeds in the corsika input file.
-
-    Parameters
-    ----------
-    file: file
-        File where the telescope positions will be written.
-    seeds: list of int
-        List of seeds to be written.
-    '''
-    for s in seeds:
-        file.write('SEED {} 0 0\n'.format(s))
-
-
-def _convertPrimaryInput(value):
-    '''
-    Convert a primary name into the right number.
-
-    Parameters
-    ----------
-    value: str
-        Input primary name.
-
-    Raises
-    ------
-    InvalidPrimary
-        If the input name is not found.
-    '''
-    for primName, primInfo in cors_pars.PRIMARIES.items():
-        if value[0].upper() == primName or value[0].upper() in primInfo['names']:
-            return [primInfo['number']]
-    logger.error('Primary not valid')
-    raise InvalidPrimary('Primary not valid')
 
 
 class CorsikaConfig:
@@ -112,6 +50,7 @@ class CorsikaConfig:
         databaseLocation=None,
         filesLocation=None,
         randomSeeds=False,
+        logger=__name__,
         **kwargs
     ):
         '''
@@ -132,10 +71,14 @@ class CorsikaConfig:
         randomSeeds: bool
             If True, seeds will be set randomly. If False, seeds will be defined based on the run
             number.
+        logger: str
+            Logger name to use in this instance
         **kwargs
             Set of parameters for the corsika config.
         '''
-        logger.debug('Init CorsikaConfig')
+
+        self._logger = logging.getLogger(logger)
+        self._logger.debug('Init CorsikaConfig')
 
         self._label = label
         self._filesLocation = cfg.getConfigArg('outputLocation', filesLocation)
@@ -165,10 +108,10 @@ class CorsikaConfig:
             if len(valueArgs) == 1 and parName == 'VIEWCONE':  # fixing single value viewcone
                 valueArgs = [0 * parInfo['unit'][0], valueArgs[0]]
             if parName == 'PRMPAR':
-                valueArgs = _convertPrimaryInput(valueArgs)
+                valueArgs = self._convertPrimaryInput(valueArgs)
 
             if len(valueArgs) != parInfo['len']:
-                logger.warning('Argument {} has wrong len'.format(keyArgs.upper()))
+                self._logger.warning('Argument {} has wrong len'.format(keyArgs.upper()))
 
             if 'unit' in parInfo.keys():
                 parUnit = (
@@ -184,7 +127,7 @@ class CorsikaConfig:
                     try:
                         newValueArgs.append(v.to(u).value)
                     except u.core.UnitConversionError:
-                        logger.error('Argument given with wrong unit: {}'.format(parName))
+                        self._logger.error('Argument given with wrong unit: {}'.format(parName))
                         raise ArgumentWithWrongUnit()
                 valueArgs = newValueArgs
 
@@ -202,7 +145,7 @@ class CorsikaConfig:
         # Checking for unindetified parameters
         unindentifiedArgs = [p for p in kwargs.keys() if p not in indentifiedArgs]
         if len(unindentifiedArgs) > 0:
-            logger.warning(
+            self._logger.warning(
                 '{} argument were not properly identified: {} ...'.format(
                     len(unindentifiedArgs),
                     unindentifiedArgs[0]
@@ -221,7 +164,7 @@ class CorsikaConfig:
             else:
                 requiredButNotGiven.append(parName)
         if len(requiredButNotGiven) > 0:
-            logger.error(
+            self._logger.error(
                 'Required parameters not given ({} parameters: {} ...)'.format(
                     len(requiredButNotGiven),
                     requiredButNotGiven[0]
@@ -229,10 +172,69 @@ class CorsikaConfig:
             )
             raise RequiredInputNotGiven()
 
+    def _writeTelescopes(self, file, array):
+        '''
+        Write telescope positions in the corsika input file.
+
+        Parameters
+        ----------
+        file: file
+            File where the telescope positions will be written.
+        array: str
+            Array type.
+        '''
+        self._logger.warning('Function is changing the inputs - Make it return the string instead')
+        mToCm = 1e2
+        for n, tel in array.items():
+            file.write('\nTELESCOPE {} {} {} {} # {}'.format(
+                tel['xPos'] * mToCm,
+                tel['yPos'] * mToCm,
+                cors_pars.TELESCOPE_Z[tel['size']] * mToCm,
+                cors_pars.TELESCOPE_SPHERE_RADIUS[tel['size']] * mToCm,
+                tel['size']
+            ))
+        file.write('\n')
+
+
+    def _writeSeeds(self, file, seeds):
+        '''
+        Write seeds in the corsika input file.
+
+        Parameters
+        ----------
+        file: file
+            File where the telescope positions will be written.
+        seeds: list of int
+            List of seeds to be written.
+        '''
+        for s in seeds:
+            file.write('SEED {} 0 0\n'.format(s))
+
+
+    def _convertPrimaryInput(self, value):
+        '''
+        Convert a primary name into the right number.
+
+        Parameters
+        ----------
+        value: str
+            Input primary name.
+
+        Raises
+        ------
+        InvalidPrimary
+            If the input name is not found.
+        '''
+        for primName, primInfo in cors_pars.PRIMARIES.items():
+            if value[0].upper() == primName or value[0].upper() in primInfo['names']:
+                return [primInfo['number']]
+        self._logger.error('Primary not valid')
+        raise InvalidPrimary('Primary not valid')
+
     def _loadSeeds(self, randomSeeds):
         ''' Load seeds and store it in _seeds. '''
         if '_parameters' not in self.__dict__.keys():
-            logger.error('_loadSeeds has be called after _loadArguments')
+            self._logger.error('_loadSeeds has be called after _loadArguments')
             raise ArgumentsNotLoaded()
         if randomSeeds:
             s = random.uniform(0, 1000)
@@ -262,7 +264,7 @@ class CorsikaConfig:
 
         if not fileDirectory.exists():
             fileDirectory.mkdir(parents=True, exist_ok=True)
-            logger.info('Creating directory {}'.format(fileDirectory))
+            self._logger.info('Creating directory {}'.format(fileDirectory))
         self._configFilePath = fileDirectory.joinpath(configFileName)
         self._outputFilePath = fileDirectory.joinpath(outputFileName)
 
@@ -285,9 +287,9 @@ class CorsikaConfig:
             file.write('\n# SITE PARAMETERS\n')
             _writeParametersSingleLine(file, cors_pars.SITE_PARAMETERS[self._site])
             file.write('\n# SEEDS\n')
-            _writeSeeds(file, self._seeds)
+            self._writeSeeds(file, self._seeds)
             file.write('\n# TELESCOPES\n')
-            _writeTelescopes(file, self._array)
+            self._writeTelescopes(file, self._array)
             file.write('\n# INTERACTION FLAGS\n')
             _writeParametersSingleLine(file, cors_pars.INTERACTION_FLAGS)
             file.write('\n# CHERENKOV EMISSION PARAMETERS\n')
