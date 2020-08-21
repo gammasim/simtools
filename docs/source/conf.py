ecc5b271c41dcbd198065b65ba57e89c6c5d2ee4
RaulRPrado <raul.prado@usp.br>
2020-08-21 09:17:02 +0200

    initial changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../simtools'))
+sys.path.insert(0, os.path.abspath('../../applications'))
 
 
 # -- Project information -----------------------------------------------------
