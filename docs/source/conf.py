d49fb0b1bfe0311ee020ba9c32c70c329c67f5e0
RaulRPrado <raul.prado@usp.br>
2020-09-04 10:16:34 +0200

    testing docs
    Former-commit-id: af3ed1bc0621dd38f8c7cbc585d499b1e3fbbae4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../simtools'))
 sys.path.insert(0, os.path.abspath('../../applications'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
