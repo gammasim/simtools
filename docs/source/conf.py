4ea7debffa676d284e6b0e87775a8c509ba11d4b
RaulRPrado <raul.prado@usp.br>
2020-09-04 10:30:01 +0200

    testing docs 3
    Former-commit-id: 648f0c13b92e3e78eb2ba303b5b7914acb277061

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ autosectionlabel_prefix_document = True
 # Display todos by setting to True
 todo_include_todos = True
 
-autodoc_mock_imports = ['matplotlib', 'yaml', 'numpy']
+autodoc_mock_imports = ['matplotlib', 'yaml', 'numpy', 'astropy', 'bson']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
