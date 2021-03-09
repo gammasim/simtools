90d84954c7f240a68c2c0f0cf8ffb9153ac7969a
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-09 15:31:46 +0100

    adding set functions

--- simtools/tests/test_layout_array.py
+++ simtools/tests/test_layout_array.py
@@ -14,7 +14,7 @@ logger.setLevel(logging.DEBUG)
 def test_read_tel_list():
     layout = LayoutArray(name='testLayout')
     telFile = io.getTestDataFile('telescope_positions_prod5_north.ecsv')
-    layout.readTelescopeList(telFile)
+    layout.readTelescopeListFile(telFile)
     layout.convertCoordinates()
 
 
@@ -29,8 +29,27 @@ def test_dict_input():
     # layout.convertCoordinates()
 
 
+def test_add_tel():
+    layout = LayoutArray(name='testLayout')
+    telFile = io.getTestDataFile('telescope_positions_prod5_north.ecsv')
+    layout.readTelescopeListFile(telFile)
+    # layout.addTelescope(telescopeName='bla')
+
+    layout.printTelescopeList()
+
+    # layout.convertCoordinates()
+
+
+# def test_export_tel_list_file():
+#     layout = LayoutArray(name='testLayout')
+#     telFile = io.getTestDataFile('telescope_positions_prod5_north.ecsv')
+#     layout.readTelescopeListFile(telFile)
+#     layout.convertCoordinates()
+
+
 if __name__ == '__main__':
 
     # test_read_tel_list()
-    test_dict_input()
+    test_add_tel()
+    # test_dict_input()
     pass
