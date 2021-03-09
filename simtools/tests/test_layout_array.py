2cd54de62ef49eaba733115cca0bcf97e72a18b7
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-09 16:34:25 +0100

    many fixed
    Former-commit-id: 29e3b6eefa2066ae85f745e690fbc24282132688

--- simtools/tests/test_layout_array.py
+++ simtools/tests/test_layout_array.py
@@ -3,6 +3,7 @@
 import logging
 
 import astropy.units as u
+import pyproj
 
 import simtools.io_handler as io
 from simtools.layout.layout_array import LayoutArray
@@ -33,12 +34,27 @@ def test_add_tel():
     layout = LayoutArray(name='testLayout')
     telFile = io.getTestDataFile('telescope_positions_prod5_north.ecsv')
     layout.readTelescopeListFile(telFile)
-    layout.addTelescope(telescopeName='bla')
+    layout.addTelescope(telescopeName='L-05', posX=100 * u.m, posY=100 * u.m, posZ=100 * u.m)
 
     layout.printTelescopeList()
 
     # layout.convertCoordinates()
 
+
+def test_build_layout():
+    layout = LayoutArray(
+        name='LST4',
+        centerLongitude=-17.8920302 * u.deg,
+        centerLatitude=28.7621661 * u.deg,
+        epsg=32628
+    )
+    layout.addTelescope(telescopeName='L-01', posX=57.5 * u.m, posY=57.5 * u.m, posZ=0 * u.m)
+    layout.addTelescope(telescopeName='L-02', posX=-57.5 * u.m, posY=57.5 * u.m, posZ=0 * u.m)
+    layout.addTelescope(telescopeName='L-03', posX=57.5 * u.m, posY=-57.5 * u.m, posZ=0 * u.m)
+    layout.addTelescope(telescopeName='L-04', posX=-57.5 * u.m, posY=-57.5 * u.m, posZ=0 * u.m)
+
+    layout.convertCoordinates()
+    layout.printTelescopeList()
 
 # def test_export_tel_list_file():
 #     layout = LayoutArray(name='testLayout')
@@ -50,6 +66,7 @@ def test_add_tel():
 if __name__ == '__main__':
 
     # test_read_tel_list()
-    test_add_tel()
+    # test_add_tel()
     # test_dict_input()
+    test_build_layout()
     pass
