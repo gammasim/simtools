85ac690d8f54420a3eae3367f87a45991a1d5653
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-11 13:50:11 +0100

    app in progress
    Former-commit-id: 5e788c35bb209243f04575929b64e8817cf02b06

---
+++ applications/make_regular_arrays.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python3
+
+'''
+    Summary
+    -------
+    .. _compare_cumulative_psf_plot:
+    .. image::  images/compare_cumulative_psf_North-LST-1_cumulativePSF.png
+      :width: 49 %
+    .. image::  images/compare_cumulative_psf_North-LST-1_image.png
+      :width: 49 %
+
+    Command line arguments
+    ----------------------
+    tel_name (str, required)
+        Telescope name (e.g. North-LST-1, South-SST-D, ...).
+    model_version (str, optional)
+        Model version (default=prod4).
+    src_distance (float, optional)
+        Source distance in km (default=10).
+    zenith (float, optional)
+        Zenith angle in deg (default=20).
+    data (str, optional)
+        Name of the data file with the measured cumulative PSF.
+    pars (str, optional)
+        Yaml file with the new model parameters to replace the default ones.
+    test (activation mode, optional)
+        If activated, application will be faster by simulating fewer photons.
+    verbosity (str, optional)
+        Log level to print (default=INFO).
+
+    Example
+    -------
+    LST-1 Prod5
+
+    Runtime < 1 min.
+
+    First, create an yml file named lst_pars.yml with the following content:
+
+    .. code-block:: yaml
+
+        mirror_reflection_random_angle: '0.0075,0.15,0.035'
+        mirror_align_random_horizontal: '0.0040,28.,0.0,0.0'
+        mirror_align_random_vertical: '0.0040,28.,0.0,0.0'
+
+    And the run:
+
+    .. code-block:: console
+
+        python applications/compare_cumulative_psf.py --tel_name North-LST-1 \
+        --model_version prod4 --pars lst_pars.yml --data PSFcurve_data_v2.txt
+
+    .. todo::
+
+        * Change default model to default (after this feature is implemented in db_handler)
+'''
+
+import logging
+import argparse
+# import yaml
+from collections import OrderedDict
+
+from astropy.io.misc import yaml
+
+import numpy as np
+import astropy.units as u
+
+import simtools.io_handler as io
+import simtools.util.general as gen
+import simtools.config as cfg
+from simtools.layout.layout_array import LayoutArray
+from simtools.model.telescope_model import TelescopeModel
+from simtools import visualize
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description=(
+            'Calculate and plot the PSF and eff. mirror area as a function of off-axis angle '
+            'of the telescope requested.'
+        )
+    )
+    # parser.add_argument(
+    #     '-n',
+    #     '--name',
+    #     help='Telescope name (e.g. North-MST-FlashCam-D, North-LST-1)',
+    #     type=str,
+    #     required=True
+    # )
+    parser.add_argument(
+        '-v',
+        '--verbosity',
+        dest='logLevel',
+        action='store',
+        default='info',
+        help='Log level to print (default is INFO)'
+    )
+
+    args = parser.parse_args()
+    label = 'make_regular_arrays'
+
+    logger = logging.getLogger(label)
+    logger.setLevel(gen.getLogLevelFromUser(args.logLevel))
+
+    # Output directory to save files related directly to this app
+    outputDir = io.getApplicationOutputDirectory(cfg.get('outputLocation'), label)
+
+    siteParsFile = io.getDataFile('layout', 'site_parameters.yml')
+
+    with open(siteParsFile, 'r') as f:
+        sitePars = yaml.load(f)
+
+    layout = LayoutArray(label=label, name='LST4', **sitePars['South'])
+
+    print(layout.__dict__)
+
+    # sitePars = {
+    #     'South': {
+    #         'centerLongitude': -70.316345 * u.deg,
+    #         'centerLatitude': -24.683429 * u.deg,
+    #         'centerAltitude': 2162.35 * u.m,
+    #         'centerNorthing': 7269466.0 * u.m,
+    #         'centerEasting': 366822.0 * u.m,
+    #         'corsikaObsLevel': 2147 * u.m,
+    #         'corsikaSphereRadius': {'LST': 12.5 * u.m, 'MST': 9.6 * u.m, 'SST': 3 * u.m},
+    #         'corsikaSphereCenter': {'LST': 16 * u.m, 'MST': 9 * u.m, 'SST': 3.25 * u.m},
+    #         'epsg': 32719
+    #     },
+    #     'North': {
+    #         'centerLongitude': -17.8920302 * u.deg,
+    #         'centerLatitude': 28.7621661 * u.deg,
+    #         'centerAltitude': 2177 * u.m,
+    #         'centerNorthing': 3185066.0 * u.m,
+    #         'centerEasting': 217611.0 * u.m,
+    #         'corsikaObsLevel': 2158 * u.m,
+    #         'corsikaSphereRadius': {'LST': 12.5 * u.m, 'MST': 9.6 * u.m, 'SST': 3 * u.m},
+    #         'corsikaSphereCenter': {'LST': 16 * u.m, 'MST': 9 * u.m, 'SST': 3.25 * u.m},
+    #         'epsg': 32628
+    #     }
+    # }
+
+    # for site in ['South', 'North']
+    #     for arrayName in ['1SST', '4SST', '1MST', '4MST', '1LST', '4LST']
+
+
+    exit()
+
+
+    telModel = TelescopeModel(
+        telescopeName=args.tel_name,
+        version=args.model_version,
+        label=label,
+        logger=logger.name
+    )
+
+    # New parameters
+    if args.pars is not None:
+        with open(args.pars) as file:
+            newPars = yaml.load(file, Loader=yaml.FullLoader)
+        telModel.changeParameters(**newPars)
+
+    ray = RayTracing(
+        telescopeModel=telModel,
+        sourceDistance=args.src_distance * u.km,
+        zenithAngle=args.zenith * u.deg,
+        offAxisAngle=[0. * u.deg],
+        logger=logger.name
+    )
+
+    ray.simulate(test=args.test, force=False)
+    ray.analyze(force=False)
+
+    # Plotting cumulative PSF
+    im = ray.images()[0]
+
+    print('d80 in cm = {}'.format(im.getPSF()))
+
+    # Plotting cumulative PSF
+    dataToPlot = OrderedDict()
+    dataToPlot[r'sim$\_$telarray'] = im.getCumulativeData()
+    if args.data is not None:
+        dataFile = cfg.findFile(args.data)
+        dataToPlot['measured'] = loadData(dataFile)
+    plt = visualize.plot1D(dataToPlot)
+    plt.gca().set_ylim(0, 1.05)
+
+    plotFileName = label + '_' + telModel.telescopeName + '_cumulativePSF'
+    plotFile = outputDir.joinpath(plotFileName)
+    for f in ['pdf', 'png']:
+        plt.savefig(str(plotFile) + '.' + f, format=f, bbox_inches='tight')
+    plt.clf()
+
+    # Plotting image
+    dataToPlot = im.getImageData()
+    visualize.plotHist2D(dataToPlot, bins=80)
+    circle = plt.Circle((0, 0), im.getPSF(0.8) / 2, color='k', fill=False, lw=2, ls='--')
+    plt.gca().add_artist(circle)
+
+    plotFileName = label + '_' + telModel.telescopeName + '_image'
+    plotFile = outputDir.joinpath(plotFileName)
+    for f in ['pdf', 'png']:
+        plt.savefig(str(plotFile) + '.' + f, format=f, bbox_inches='tight')
+    plt.clf()
