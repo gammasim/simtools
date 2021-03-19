2a8a54a815f3d03a58a8e8d9ab74b4dbce5cbe60
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-19 13:41:35 +0100

    more structure to array_model
    Former-commit-id: 77080bd21671b31b3d41dc5f270b2d19dfc59777

--- simtools/model/array_model.py
+++ simtools/model/array_model.py
@@ -11,14 +11,39 @@ __all__ = ['ArrayModel']
 
 
 class ArrayModel:
-    def __init__(self, label, layoutName):
+    def __init__(
+        self,
+        label,
+        arrayConfigFile=None,
+        arrayConfigData=None,
+        logger=__name__
+    ):
 
         self.layout = None
 
+        if arrayConfigData is not None and arrayConfigFile is not None:
+            self._logger.warning(
+                'Both arrayConfigData and arrayConfigFile were given '
+                '- arrayConfigData will be used'
+            )
+            # read file and load config
+            pass
+        elif arrayConfigFile is not None:
+            # read file and load config
+            pass
+        elif arrayConfigData is not None:
+            self._loadArrayData(arrayConfigData)
+        else:
+            self._logger.error('No arrayConfigData was given - aborting')
+
+    def _loadArrayData(arrayConfigData):
         pass
 
     def exportCorsikaInputFile():
         pass
 
     def exportSimtelConfigFiles():
+        pass
+
+    def exportArrayConfigFile():
         pass
