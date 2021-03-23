87eb24b59776bd5926d43c39eb649ea5e437c373
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-23 15:51:16 +0100

    exporting simtel config files done

--- simtools/tests/test_array_model.py
+++ simtools/tests/test_array_model.py
@@ -19,10 +19,27 @@ def test_input_validation():
         },
         'M-05': 'NectarCam-D'
     }
-    am = ArrayModel(arrayConfigData=arrayConfigData)
+    am = ArrayModel(label='test', arrayConfigData=arrayConfigData)
 
     am.printTelescopeList()
 
 
+def test_exporting_config_files():
+    arrayConfigData = {
+        'site': 'North',
+        'arrayName': 'Prod5',
+        'modelVersion': 'Prod5',
+        'default': {
+            'LST': '1',
+            'MST': 'FlashCam-D'
+        },
+        'M-05': 'NectarCam-D'
+    }
+    am = ArrayModel(label='test', arrayConfigData=arrayConfigData)
+
+    am.exportSimtelTelescopeConfigFiles()
+
+
 if __name__ == '__main__':
-    test_input_validation()
+    # test_input_validation()
+    test_exporting_config_files()
