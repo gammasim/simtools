5f01b55038bbc87f26f675ffe047c0e4978a13e7
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-03-24 15:11:44 +0100

    utils and apps done

--- simtools/util/legend_handlers.py
+++ simtools/util/legend_handlers.py
@@ -35,13 +35,14 @@ class hexPixelHandler(object):
     '''
     Legend handler class to plot a hexagonal "on" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
-        x0, y0 = handlebox.xdescent + handlebox.width/3, handlebox.ydescent + handlebox.height/3
+        x0, y0 = handlebox.xdescent + handlebox.width / 3, handlebox.ydescent + handlebox.height / 3
         width = height = handlebox.height
         patch = mpatches.RegularPolygon(
             (x0, y0),
             numVertices=6,
-            radius=0.7*handlebox.height,
+            radius=0.7 * handlebox.height,
             orientation=np.deg2rad(30),
             facecolor=(1, 1, 1, 0),
             edgecolor=(0, 0, 0, 1),
@@ -55,13 +56,14 @@ class hexEdgePixelHandler(object):
     '''
     Legend handler class to plot a hexagonal "edge" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
-        x0, y0 = handlebox.xdescent + handlebox.width/3, handlebox.ydescent + handlebox.height/3
+        x0, y0 = handlebox.xdescent + handlebox.width / 3, handlebox.ydescent + handlebox.height / 3
         width = height = handlebox.height
         patch = mpatches.RegularPolygon(
             (x0, y0),
             numVertices=6,
-            radius=0.7*handlebox.height,
+            radius=0.7 * handlebox.height,
             orientation=np.deg2rad(30),
             facecolor=mcolors.to_rgb('brown') + (0.5,),
             edgecolor=mcolors.to_rgb('black') + (1,),
@@ -75,13 +77,14 @@ class hexOffPixelHandler(object):
     '''
     Legend handler class to plot a hexagonal "off" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
-        x0, y0 = handlebox.xdescent + handlebox.width/3, handlebox.ydescent + handlebox.height/3
+        x0, y0 = handlebox.xdescent + handlebox.width / 3, handlebox.ydescent + handlebox.height / 3
         width = height = handlebox.height
         patch = mpatches.RegularPolygon(
             (x0, y0),
             numVertices=6,
-            radius=0.7*handlebox.height,
+            radius=0.7 * handlebox.height,
             orientation=np.deg2rad(30),
             facecolor='black',
             edgecolor='black',
@@ -95,6 +98,7 @@ class squarePixelHandler(object):
     '''
     Legend handler class to plot a square "on" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         x0, y0 = handlebox.xdescent, handlebox.ydescent
         width = height = handlebox.height
@@ -114,6 +118,7 @@ class squareEdgePixelHandler(object):
     '''
     Legend handler class to plot a square "edge" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         x0, y0 = handlebox.xdescent, handlebox.ydescent
         width = height = handlebox.height
@@ -133,6 +138,7 @@ class squareOffPixelHandler(object):
     '''
     Legend handler class to plot a square "off" pixel.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         x0, y0 = handlebox.xdescent, handlebox.ydescent
         width = height = handlebox.height
@@ -152,6 +158,7 @@ class lstHandler(object):
     '''
     Legend handler class to plot a representation of an LST in an array layout.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         center = (
             handlebox.xdescent + 0.3 * handlebox.width,
@@ -173,6 +180,7 @@ class mstHandler(object):
     '''
     Legend handler class to plot a representation of an MST in an array layout.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         x0, y0 = handlebox.xdescent + 0.1 * handlebox.width, handlebox.ydescent
         width = height = handlebox.height
@@ -192,6 +200,7 @@ class sstHandler(object):
     '''
     Legend handler class to plot a representation of an SST in an array layout.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         center = (
             handlebox.xdescent + 0.25 * handlebox.width,
@@ -200,7 +209,7 @@ class sstHandler(object):
         radius = handlebox.height
         patch = mpatches.Circle(
             xy=center,
-            radius=radius*(2.8/12),
+            radius=radius * (2.8 / 12),
             facecolor='black',
             edgecolor='black',
             transform=handlebox.get_transform()
@@ -213,6 +222,7 @@ class meanRadiusOuterEdgeHandler(object):
     '''
     Legend handler class to plot a the mean radius outer edge of the dish.
     '''
+
     def legend_artist(self, legend, orig_handle, fontsize, handlebox):
         center = (
             handlebox.xdescent + 0.25 * handlebox.width,
