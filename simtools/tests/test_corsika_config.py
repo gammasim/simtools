37fb91c4d0e6e8ad74490b8535d1702ecc2bc208
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-04-09 09:30:22 +0200

    updating test_corsika_config

--- simtools/tests/test_corsika_config.py
+++ simtools/tests/test_corsika_config.py
@@ -3,7 +3,6 @@
 import logging
 from astropy import units as u
 
-import simtools.config as cfg
 from simtools.corsika.corsika_config import CorsikaConfig
 
 logger = logging.getLogger()
@@ -29,62 +28,64 @@ def test_general():
     cc.printParameters()
     cc.exportInputFile()
 
-#     cc2 = CorsikaConfig(
-#         site='LaPalma',
-#         arrayName='1SST',
-#         nshow=1000,
-#         nrun=11,
-#         zenith=[0 * u.deg, 60 * u.deg],
-#         viewcone=[0 * u.deg, 10 * u.deg],
-#         erange=[0.01 * u.TeV, 10 * u.TeV],
-#         eslope=2,
-#         phi=0 * u.deg,
-#         cscat=[10, 1500 * u.m, 0],
-#         primary='proton',
-#         label='test-corsika-config',
-#         logger=logger.name
-#     )
-#     cc2.exportFile()
+    cc2 = CorsikaConfig(
+        site='LaPalma',
+        layoutName='1SST',
+        nshow=1000,
+        nrun=11,
+        zenith=[0 * u.deg, 60 * u.deg],
+        viewcone=[0 * u.deg, 10 * u.deg],
+        erange=[0.01 * u.TeV, 10 * u.TeV],
+        eslope=2,
+        phi=0 * u.deg,
+        cscat=[10, 1500 * u.m, 0],
+        primary='proton',
+        label='test-corsika-config',
+        logger=logger.name
+    )
+    cc2.exportInputFile()
 
-#     cc3 = CorsikaConfig(
-#         site='LaPalma',
-#         arrayName='1MST',
-#         nshow=10000,
-#         zenith=[0 * u.deg, 60 * u.deg],
-#         viewcone=[0 * u.deg, 0 * u.deg],
-#         erange=[0.01 * u.TeV, 10 * u.TeV],
-#         eslope=2,
-#         phi=0 * u.deg,
-#         cscat=[10, 1500 * u.m, 0],
-#         primary='electron',
-#         label='test-corsika-config',
-#         logger=logger.name
-#     )
-#     # Testing default parameters
-#     assert cc3._parameters['RUNNR'] == [1]
-#     assert cc3._parameters['EVTNR'] == [1]
-#     cc3.exportFile()
+    cc3 = CorsikaConfig(
+        site='LaPalma',
+        layoutName='1MST',
+        nshow=10000,
+        zenith=[0 * u.deg, 60 * u.deg],
+        viewcone=[0 * u.deg, 0 * u.deg],
+        erange=[0.01 * u.TeV, 10 * u.TeV],
+        eslope=2,
+        phi=0 * u.deg,
+        cscat=[10, 1500 * u.m, 0],
+        primary='electron',
+        label='test-corsika-config',
+        logger=logger.name
+    )
+    # Testing default parameters
+    assert cc3._userParameters['RUNNR'] == [1]
+    assert cc3._userParameters['EVTNR'] == [1]
+    cc3.exportInputFile()
 
-# def test_units():
-#     cc = CorsikaConfig(
-#         site='Paranal',
-#         arrayName='4LST',
-#         nshow=100,
-#         nrun=10,
-#         zenith=0.1 * u.rad,
-#         viewcone=5 * u.deg,
-#         erange=[0.01 * u.TeV, 10 * u.TeV],
-#         eslope=2,
-#         phi=0 * u.deg,
-#         cscat=[10, 1500 * u.m, 0],
-#         primary='proton',
-#         label='test-corsika-config',
-#         logger=logger.name
-#     )
-#     cc.exportFile()
+
+def test_units():
+    cc = CorsikaConfig(
+        site='Paranal',
+        layoutName='4LST',
+        nshow=100,
+        nrun=10,
+        zenith=0.1 * u.rad,
+        viewcone=5 * u.deg,
+        erange=[0.01 * u.TeV, 10 * u.TeV],
+        eslope=2,
+        phi=0 * u.deg,
+        cscat=[10, 1500 * u.m, 0],
+        primary='proton',
+        label='test-corsika-config',
+        logger=logger.name
+    )
+    cc.exportInputFile()
+
 
 if __name__ == '__main__':
 
     test_general()
-#    test_units()
+    test_units()
     pass
