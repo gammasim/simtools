400f1d6f2ecf484e5ee59dc18ce69798f869c9c5
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-04-14 16:12:17 +0200

    cleaning up test_corsika_runner
    Former-commit-id: ff3a923afe5ff0c7928a1169e68b316398a33164

--- simtools/tests/test_corsika_runner.py
+++ simtools/tests/test_corsika_runner.py
@@ -44,34 +44,5 @@ class TestCorsikaRunner(unittest.TestCase):
                 _ = self.corsikaRunner.getRunScriptFile(run)
 
 
-# def test_arguments_and_script():
-#     corsikaConfigData = {
-#         'corsikaDataDirectory': './corsika-data',
-#         'nshow': 10,
-#         'primary': 'gamma',
-#         'erange': [100 * u.GeV, 1 * u.TeV],
-#         'eslope': -2,
-#         'zenith': 20 * u.deg,
-#         'azimuth': 0 * u.deg,
-#         'viewcone': 0 * u.deg,
-#         'cscat': [10, 1500 * u.m, 0]
-#     }
-
-#     cr = CorsikaRunner(
-#         site='south',
-#         layoutName='Prod5',
-#         label='test-corsika-runner',
-#         corsikaConfigData=corsikaConfigData
-#     )
-
-#     runNumber = 3
-#     script = cr.getRunScriptFile(runNumber=runNumber)
-#     logger.debug('Script file written in {}'.format(script))
-
-#     logger.debug('Run log file in {}'.format(cr.getRunLogFile(runNumber)))
-#     logger.debug('CORSIKA log file in {}'.format(cr.getCorsikaLogFile(runNumber)))
-#     logger.debug('CORSIKA output file in {}'.format(cr.getCorsikaOutputFile(runNumber)))
-
-
 if __name__ == '__main__':
     unittest.main()
