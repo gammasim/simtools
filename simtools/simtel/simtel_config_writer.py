cbf7927a40a01be5f376477b54ea162331dd8f6b
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-04-19 14:47:05 +0200

    checking out layout files from corsika-config to fix bug with units

--- simtools/simtel/simtel_config_writer.py
+++ simtools/simtel/simtel_config_writer.py
@@ -1,308 +1,137 @@
 #!/usr/bin/python3
 
 import logging
-import random
-
-import simtools.config as cfg
-import simtools.io_handler as io
-import simtools.corsika_parameters as cors_pars
-from simtools.util import names
-from simtools.util.arrays import getArrayInfo
-
-__all__ = ['CorsikaConfig']
 
 
-class RequiredInputNotGiven(Exception):
-    pass
+__all__ = ['SimtelConfigWriter']
 
 
-class ArgumentsNotLoaded(Exception):
-    pass
-
-
-class ArgumentWithWrongUnit(Exception):
-    pass
-
-
-class InvalidPrimary(Exception):
-    pass
-
-
-class CorsikaConfig:
+class SimtelConfigWriter:
     '''
-    CorsikaConfig class.
-
-    Methods
-    -------
-    setParameters(**kwargs)
-    exportFile()
-    getFile()
     '''
+    TAB = ' ' * 3
+    SITE_PARS_TO_WRITE_IN_CONFIG = ['altitude', 'atmospheric_transmission']
+    COMMON_PARS = {
+        'trigger_telescopes': 1,
+        'array_trigger': 'none',
+        'trigger_telescopes': 2,
+        'only_triggered_telescopes': 1,
+        'array_window': 1000,
+        'output_format': 1,
+        'mirror_list': 'none',
+        'telescope_random_angle': 0.,
+        'telescope_random_error': 0.,
+        'convergent_depth': 0,
+        'iobuf_maximum': 1000000000,
+        'iobuf_output_maximum': 400000000,
+        'multiplicity_offset': -0.5,
+        'discriminator_pulse_shape': 'none',
+        'discriminator_amplitude': 0.,
+        'discriminator_threshold': 99999.,
+        'fadc_noise': 0.,
+        'asum_threshold': 0.,
+        'asum_shaping_file': 'none',
+        'asum_offset': 0.0,
+        'dsum_threshold': 0,
+        'fadc_pulse_shape': 'none',
+        'fadc_amplitude': 0.,
+        'fadc_pedestal': 100.,
+        'fadc_max_signal': 4095,
+        'fadc_max_sum': 16777215,
+        'store_photoelectrons': 30,
+        'pulse_analysis': -30,
+        'sum_before_peak': 3,
+        'sum_after_peak': 4
+    }
 
-    def __init__(
-        self,
-        site,
-        arrayName,
-        label=None,
-        filesLocation=None,
-        randomSeeds=False,
-        **kwargs
-    ):
+    def __init__(self, site, modelVersion, layoutName=None, label=None):
         '''
-        CorsikaConfig init.
-
-        Parameters
-        ----------
-        site: str
-            Paranal or LaPalma
-        arrayName: str
-            Name of the array type. Ex 4LST, baseline ...
-        label: str
-            Instance label.
-        filesLocation: str or Path.
-            Main location of the output file.
-        randomSeeds: bool
-            If True, seeds will be set randomly. If False, seeds will be defined based on the run
-            number.
-        **kwargs
-            Set of parameters for the corsika config.
         '''
 
         self._logger = logging.getLogger(__name__)
-        self._logger.debug('Init CorsikaConfig')
+        self._logger.debug('Init SimtelConfigWriter')
 
+        self._site = site
+        self._modelVersion = modelVersion
         self._label = label
-        self._filesLocation = cfg.getConfigArg('outputLocation', filesLocation)
-        self._site = names.validateSiteName(site)
-        self._arrayName = names.validateArrayName(arrayName)
-        self._array = getArrayInfo(self._arrayName)
+        self._layoutName = layoutName
 
-        self.setParameters(**kwargs)
-        self._loadSeeds(randomSeeds)
-        self._isFileUpdated = False
-
-    def setParameters(self, **kwargs):
+    def writeSimtelArrayConfigFile(
+        self,
+        configFilePath,
+        layout,
+        telescopeModels,
+        siteParameters
+    ):
         '''
-        Set parameters for the corsika config.
-
-        Parameters
-        ----------
-        **kwargs
         '''
-        self._parameters = dict()
 
-        def _validateAndFixArgs(parName, parInfo, valueArgs):
-            valueArgs = valueArgs if isinstance(valueArgs, list) else [valueArgs]
-            if len(valueArgs) == 1 and parName == 'THETAP':  # fixing single value zenith angle
-                valueArgs = valueArgs * 2
-            if len(valueArgs) == 1 and parName == 'VIEWCONE':  # fixing single value viewcone
-                valueArgs = [0 * parInfo['unit'][0], valueArgs[0]]
-            if parName == 'PRMPAR':
-                valueArgs = self._convertPrimaryInput(valueArgs)
+        with open(configFilePath, 'w') as file:
+            self._writeHeader(file, 'ARRAY CONFIGURATION FILE')
 
-            if len(valueArgs) != parInfo['len']:
-                self._logger.warning('Argument {} has wrong len'.format(keyArgs.upper()))
+            # Be carefull with the formating - simtel is sensitive
+            file.write('#ifndef TELESCOPE\n')
+            file.write('# define TELESCOPE 0\n')
+            file.write('#endif\n\n')
 
-            if 'unit' in parInfo.keys():
-                parUnit = (
-                    [parInfo['unit']] if not isinstance(parInfo['unit'], list) else parInfo['unit']
-                )
+            # TELESCOPE 0 - global parameters
+            file.write('#if TELESCOPE == 0\n')
+            file.write(self.TAB + 'echo *****************************\n')
+            file.write(self.TAB + 'echo Site: {}\n'.format(self._site))
+            file.write(self.TAB + 'echo LayoutName: {}\n'.format(self._layoutName))
+            file.write(self.TAB + 'echo ModelVersion: {}\n'.format(self._modelVersion))
+            file.write(self.TAB + 'echo *****************************\n\n')
 
-                newValueArgs = list()
-                for (v, u) in zip(valueArgs, parUnit):
-                    if u is None:
-                        newValueArgs.append(v)
-                        continue
+            # Writing site parameters
+            self._writeSiteParameters(file, siteParameters)
 
-                    try:
-                        newValueArgs.append(v.to(u).value)
-                    except u.core.UnitConversionError:
-                        self._logger.error('Argument given with wrong unit: {}'.format(parName))
-                        raise ArgumentWithWrongUnit()
-                valueArgs = newValueArgs
+            # Writing common parameters
+            self._writeCommonParameters(file)
 
-            return valueArgs
+            # Maximum telescopes
+            file.write(self.TAB + 'maximum_telescopes = {}\n\n'.format(len(telescopeModels)))
 
-        # Collecting all parameters given as arguments
-        indentifiedArgs = list()
-        for keyArgs, valueArgs in kwargs.items():
-            for parName, parInfo in cors_pars.USER_PARAMETERS.items():
-                if keyArgs.upper() == parName or keyArgs.upper() in parInfo['names']:
-                    indentifiedArgs.append(keyArgs)
-                    valueArgs = _validateAndFixArgs(parName, parInfo, valueArgs)
-                    self._parameters[parName] = valueArgs
-
-        # Checking for unindetified parameters
-        unindentifiedArgs = [p for p in kwargs.keys() if p not in indentifiedArgs]
-        if len(unindentifiedArgs) > 0:
-            self._logger.warning(
-                '{} argument were not properly identified: {} ...'.format(
-                    len(unindentifiedArgs),
-                    unindentifiedArgs[0]
-                )
+            # Default telescope - 0th tel in telescope list
+            telConfigFile = (
+                self._telescopeModel[0].getConfigFile(noExport=True).name
             )
+            file.write('# include <{}>\n\n'.format(telConfigFile))
 
-        # Checking for parameters with default option
-        # If it is not given. filling it with the default value
-        requiredButNotGiven = list()
-        for parName, parInfo in cors_pars.USER_PARAMETERS.items():
-            if parName in self._parameters.keys():
+            # Looping over telescopes - from 1 to ...
+            for count, telModel in enumerate(self._telescopeModel):
+                telConfigFile = telModel.getConfigFile(noExport=True).name
+                file.write('%{}\n'.format(self.layout[count].name))
+                file.write('#elif TELESCOPE == {}\n\n'.format(count + 1))
+                file.write('# include <{}>\n\n'.format(telConfigFile))
+            file.write('#endif \n\n')
+    # END writeSimtelArrayConfigFile
+
+    def _writeHeader(self, file, title):
+        header = '%{}\n'.format(50 * '=')
+        header += '% {}\n'.format(title)
+        header += '% Site: {}\n'.format(self._site)
+        header += '% ModelVersion: {}\n'.format(self._modelVersion)
+        header += (
+            '% LayoutName: {}\n'.format(self._layoutName) if self._layoutName is not None else ''
+        )
+        header += ('% Label: {}\n'.format(self._label) if self._label is not None else '')
+        header += '%{}\n\n'.format(50 * '=')
+        file.write(header)
+
+    def _writeSiteParameters(self, file, siteParameters):
+        file.write(self.TAB + '% Site parameters\n')
+        for par in siteParameters:
+            if par not in self.SITE_PARS_TO_WRITE_IN_CONFIG:
                 continue
-            if 'default' in parInfo.keys():
-                parValue = _validateAndFixArgs(parName, parInfo, parInfo['default'])
-                self._parameters[parName] = parValue
-            else:
-                requiredButNotGiven.append(parName)
-        if len(requiredButNotGiven) > 0:
-            self._logger.error(
-                'Required parameters not given ({} parameters: {} ...)'.format(
-                    len(requiredButNotGiven),
-                    requiredButNotGiven[0]
-                )
-            )
-            raise RequiredInputNotGiven()
-
-    def _writeTelescopes(self, file, array):
-        '''
-        Write telescope positions in the corsika input file.
-
-        Parameters
-        ----------
-        file: file
-            File where the telescope positions will be written.
-        array: str
-            Array type.
-        '''
-        self._logger.warning('Function is changing the inputs - Make it return the string instead')
-        mToCm = 1e2
-        for n, tel in array.items():
-            file.write('\nTELESCOPE {} {} {} {} # {}'.format(
-                tel['xPos'] * mToCm,
-                tel['yPos'] * mToCm,
-                cors_pars.TELESCOPE_Z[tel['size']] * mToCm,
-                cors_pars.TELESCOPE_SPHERE_RADIUS[tel['size']] * mToCm,
-                tel['size']
-            ))
+            value = siteParameters[par]['Value']
+            file.write(self.TAB + '{} = {}\n'.format(par, value))
         file.write('\n')
 
-    def _writeSeeds(self, file, seeds):
-        '''
-        Write seeds in the corsika input file.
+    def _writeCommonParameters(self, file):
+        # Common parameters taken from CTA-PROD4-common.cfg
+        # TODO: Store these somewhere else
+        self._logger.warning('Common parameters are hardcoded!')
 
-        Parameters
-        ----------
-        file: file
-            File where the telescope positions will be written.
-        seeds: list of int
-            List of seeds to be written.
-        '''
-        for s in seeds:
-            file.write('SEED {} 0 0\n'.format(s))
-
-    def _convertPrimaryInput(self, value):
-        '''
-        Convert a primary name into the right number.
-
-        Parameters
-        ----------
-        value: str
-            Input primary name.
-
-        Raises
-        ------
-        InvalidPrimary
-            If the input name is not found.
-        '''
-        for primName, primInfo in cors_pars.PRIMARIES.items():
-            if value[0].upper() == primName or value[0].upper() in primInfo['names']:
-                return [primInfo['number']]
-        self._logger.error('Primary not valid')
-        raise InvalidPrimary('Primary not valid')
-
-    def _loadSeeds(self, randomSeeds):
-        ''' Load seeds and store it in _seeds. '''
-        if '_parameters' not in self.__dict__.keys():
-            self._logger.error('_loadSeeds has be called after _loadArguments')
-            raise ArgumentsNotLoaded()
-        if randomSeeds:
-            s = random.uniform(0, 1000)
-        else:
-            s = self._parameters['PRMPAR'][0] + self._parameters['RUNNR'][0]
-        random.seed(s)
-        self._seeds = [int(random.uniform(0, 1e7)) for i in range(4)]
-
-    def exportFile(self):
-        ''' Create and export corsika input file. '''
-        configFileName = names.corsikaConfigFileName(
-            arrayName=self._arrayName,
-            site=self._site,
-            zenith=self._parameters['THETAP'],
-            viewCone=self._parameters['VIEWCONE'],
-            label=self._label
-        )
-        outputFileName = names.corsikaOutputFileName(
-            arrayName=self._arrayName,
-            site=self._site,
-            zenith=self._parameters['THETAP'],
-            viewCone=self._parameters['VIEWCONE'],
-            run=self._parameters['RUNNR'][0],
-            label=self._label
-        )
-        fileDirectory = io.getCorsikaOutputDirectory(self._filesLocation, self._label)
-
-        if not fileDirectory.exists():
-            fileDirectory.mkdir(parents=True, exist_ok=True)
-            self._logger.info('Creating directory {}'.format(fileDirectory))
-        self._configFilePath = fileDirectory.joinpath(configFileName)
-        self._outputFilePath = fileDirectory.joinpath(outputFileName)
-
-        def _writeParametersSingleLine(file, pars):
-            for par, values in pars.items():
-                line = par + ' '
-                for v in values:
-                    line += str(v) + ' '
-                line += '\n'
-                file.write(line)
-
-        def _writeParametersMultipleLines(file, pars):
-            for par, valueList in pars.items():
-                for value in valueList:
-                    newPars = {par: value}
-                    _writeParametersSingleLine(file, newPars)
-
-        with open(self._configFilePath, 'w') as file:
-            _writeParametersSingleLine(file, self._parameters)
-            file.write('\n# SITE PARAMETERS\n')
-            _writeParametersSingleLine(file, cors_pars.SITE_PARAMETERS[self._site])
-            file.write('\n# SEEDS\n')
-            self._writeSeeds(file, self._seeds)
-            file.write('\n# TELESCOPES\n')
-            self._writeTelescopes(file, self._array)
-            file.write('\n# INTERACTION FLAGS\n')
-            _writeParametersSingleLine(file, cors_pars.INTERACTION_FLAGS)
-            file.write('\n# CHERENKOV EMISSION PARAMETERS\n')
-            _writeParametersSingleLine(file, cors_pars.CHERENKOV_EMISSION_PARAMETERS)
-            file.write('\n# DEBUGGING OUTPUT PARAMETERS\n')
-            _writeParametersSingleLine(file, cors_pars.DEBUGGING_OUTPUT_PARAMETERS)
-            file.write('\n# OUTUPUT FILE\n')
-            file.write('TELFIL {}'.format(self._outputFilePath))
-            file.write('\n# IACT TUNING PARAMETERS\n')
-            _writeParametersMultipleLines(file, cors_pars.IACT_TUNING_PARAMETERS)
-            file.write('\nEXIT')
-
-        self._isFileUpdated = True
-
-    def getFile(self):
-        '''
-        Get the full path of the corsika input file.
-
-        Returns
-        -------
-        Path of the input file.
-        '''
-        if not self._isFileUpdated:
-            self.exportFile()
-        return self._configFilePath
-
-    def addLine(self):
-        pass
+        for par, value in self.COMMON_PARS.items():
+            file.write('   {} = {}\n'.format(par, value))
+    # End of writeCommonParameters
--- simtools/tests/test_simtel_config_writer.py
+++ simtools/tests/test_simtel_config_writer.py
@@ -4,6 +4,7 @@ import logging
 import unittest
 
 from simtools.simtel.simtel_config_writer import SimtelConfigWriter
+from simtools.model.array_model import ArrayModel
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -11,11 +12,22 @@ logger.setLevel(logging.DEBUG)
 
 class TestSimtelConfigWriter(unittest.TestCase):
 
-    def setUp():
-        self.simtelConfigWriter = SimtelConfigWriter()
+    def setUp(self):
+        arrayConfigData = {
+            'site': 'North',
+            'layoutName': '4LST',
+            'modelVersion': 'Prod5',
+            'default': {
+                'LST': '1'
+            }
+        }
+        self.ArrayModel = ArrayModel(
+            label='test',
+            arrayConfigData=arrayConfigData
+        )
 
     def test_write_array_config_file():
-        pass
+        self.ArrayModel.exportSimtelArrayConfigFile()
 
 
 if __name__ == '__main__':
