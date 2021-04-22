e6460e2230966cf9063132d090868f4c659e4417
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-04-22 11:28:44 +0200

    moving fileHasText to util/general
    Former-commit-id: 7c99ce01ff8d4297705b1acae984b505f67b365a

--- simtools/tests/test_simtel_config_writer.py
+++ simtools/tests/test_simtel_config_writer.py
@@ -4,6 +4,7 @@ import logging
 import unittest
 
 import simtools.io_handler as io
+from simtools.util.general import fileHasText
 from simtools.simtel.simtel_config_writer import SimtelConfigWriter
 from simtools.model.telescope_model import TelescopeModel
 from simtools.layout.layout_array import LayoutArray
@@ -11,13 +12,6 @@ from simtools.layout.layout_array import LayoutArray
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-
-def file_has_text(file, text):
-    with open(file, 'r') as ff:
-        for ll in ff:
-            if text in ll:
-                return True
-    return False
 
 class TestSimtelConfigWriter(unittest.TestCase):
 
@@ -43,7 +37,7 @@ class TestSimtelConfigWriter(unittest.TestCase):
             telescopeModel=[self.telescopeModel] * 4,
             siteParameters={}
         )
-        self.assertTrue(file_has_text(file, 'TELESCOPE == 1'))
+        self.assertTrue(fileHasText(file, 'TELESCOPE == 1'))
 
     def test_write_tel_config_file(self):
         file = io.getTestOutputFile('simtel-config-writer_telescope.txt')
@@ -51,7 +45,7 @@ class TestSimtelConfigWriter(unittest.TestCase):
             configFilePath=file,
             parameters={'par': {'Value': 1}}
         )
-        self.assertTrue(file_has_text(file, 'par = 1'))
+        self.assertTrue(fileHasText(file, 'par = 1'))
 
 
 if __name__ == '__main__':
