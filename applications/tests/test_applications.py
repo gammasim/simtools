4c690fbeae7a740830b161876c5ba1f4aa924ac6
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-04-26 11:16:06 +0200

    testing app test script

--- applications/tests/test_applications.py
+++ applications/tests/test_applications.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
 
 import logging
-import unittest
 import os
+import pytest
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-class TestApplications(unittest.TestCase):
+APP_LIST = {
+    'validate_camera_efficiency': [
+        ['-s', 'North', '-t', 'MST-NectarCam-D']
+    ],
+    'validate_camera_fov': ['--bla']
 
-    def setUp(self):
-        pass
+}
 
-    def test_applications(self):
-        os.system('python applications/produce_array_config.py')
+
+@pytest.mark.parametrize('application', APP_LIST.keys())
+def test_applications(application):
+    logger.info('Testing {}'.format(application))
+
+    def makeCommand(app, args):
+        cmd = 'python applications/' + app + '.py'
+        for aa in args:
+            cmd += ' ' + aa
+        return cmd
+
+    for args in APP_LIST[application]:
+        logger.info('Running with args: {}'.format(args))
+        cmd = makeCommand(application, args)
+        out = os.system(cmd)
+        assert out == 0
 
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+
+    test_applications('test')
