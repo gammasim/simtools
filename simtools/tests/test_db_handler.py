d3f19a9fff3ab0887e69e3847956ce52a97e60c5
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-05-05 11:05:54 +0200

    refactoring test_db_handler using unittest.TestCase - done

--- simtools/tests/test_db_handler.py
+++ simtools/tests/test_db_handler.py
@@ -8,203 +8,218 @@ import simtools.config as cfg
 import simtools.io_handler as io
 from simtools import db_handler
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
 testDataDirectory = './data/test-output'
 DB_CTA_SIMULATION_MODEL = 'CTA-Simulation-Model'
 
 class TestDBHandler(unittest.TestCase):
 
-    def setUpClass(self):
+    def setUp(self):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+
         self.db = db_handler.DatabaseHandler()
         self.testDataDirectory = io.getTestDataDirectory()
+        self.DB_CTA_SIMULATION_MODEL = 'CTA-Simulation-Model'
 
+    def test_reading_db_lst(self):
 
-def test_reading_db_lst():
+        self.logger.info('----Testing reading LST-----')
+        pars = self.db.getModelParameters('north', 'lst-1', 'Current', self.testDataDirectory)
+        if cfg.get('useMongoDB'):
+            assert(pars['parabolic_dish']['Value'] == 1)
+            assert(pars['camera_pixels']['Value'] == 1855)
+        else:
+            assert(pars['parabolic_dish'] == 1)
+            assert(pars['camera_pixels'] == 1855)
 
-    logger.info('----Testing reading LST-----')
-    db = db_handler.DatabaseHandler()
-    pars = db.getModelParameters('north', 'lst-1', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['parabolic_dish']['Value'] == 1)
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
+
+    def test_reading_db_mst_nc(self):
+
+        self.logger.info('----Testing reading MST-NectarCam-----')
+        pars = self.db.getModelParameters(
+            'north',
+            'mst-NectarCam-D',
+            'Current',
+            self.testDataDirectory
+        )
+        if cfg.get('useMongoDB'):
+            assert(pars['camera_pixels']['Value'] == 1855)
+        else:
+            assert(pars['camera_pixels'] == 1855)
+
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
+
+        self.logger.info('Removing the files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['rm -f {}/*'.format(self.testDataDirectory)], shell=True)
+
+    def test_reading_db_mst_fc(self):
+
+        self.logger.info('----Testing reading MST-FlashCam-----')
+        pars = self.db.getModelParameters(
+            'north',
+            'mst-FlashCam-D',
+            'Current',
+            self.testDataDirectory
+        )
+        if cfg.get('useMongoDB'):
+            assert(pars['camera_pixels']['Value'] == 1764)
+        else:
+            assert(pars['camera_pixels'] == 1764)
+
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
+
+        self.logger.info('Removing the files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['rm -f {}/*'.format(self.testDataDirectory)], shell=True)
+
+    def test_reading_db_sst(self):
+
+        self.logger.info('----Testing reading SST-----')
+        pars = self.db.getModelParameters('south', 'sst-D', 'Current', self.testDataDirectory)
+        if cfg.get('useMongoDB'):
+            assert(pars['camera_pixels']['Value'] == 2048)
+        else:
+            assert(pars['camera_pixels'] == 2048)
+
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
+
+        self.logger.info('Removing the files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['rm -f {}/*'.format(self.testDataDirectory)], shell=True)
+
+    def test_modify_db(self):
+
+        # This test is only relevant for the MongoDB
+        if not cfg.get('useMongoDB'):
+            return
+
+        self.logger.info('----Testing copying a whole telescope-----')
+        self.db.copyTelescope(
+            DB_CTA_SIMULATION_MODEL,
+            'North-LST-1',
+            'Current',
+            'North-LST-Test',
+            'sandbox'
+        )
+        self.db.copyDocuments(
+            DB_CTA_SIMULATION_MODEL,
+            'metadata',
+            {'Entry': 'Simulation-Model-Tags'},
+            'sandbox'
+        )
+        pars = self.db.readMongoDB(
+            'sandbox',
+            'North-LST-Test',
+            'Current',
+            self.testDataDirectory,
+            False
+        )
         assert(pars['camera_pixels']['Value'] == 1855)
-    else:
-        assert(pars['parabolic_dish'] == 1)
-        assert(pars['camera_pixels'] == 1855)
 
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
+        self.logger.info('----Testing adding a parameter-----')
+        self.db.addParameter(
+            'sandbox',
+            'North-LST-Test',
+            'camera_config_version',
+            'test',
+            42
+        )
+        pars = self.db.readMongoDB(
+            'sandbox',
+            'North-LST-Test',
+            'test',
+            self.testDataDirectory,
+            False
+        )
+        assert(pars['camera_config_version']['Value'] == 42)
 
-    return
+        self.logger.info('----Testing updating a parameter-----')
+        self.db.updateParameter(
+            'sandbox',
+            'North-LST-Test',
+            'test',
+            'camera_config_version',
+            999
+        )
+        pars = self.db.readMongoDB(
+            'sandbox',
+            'North-LST-Test',
+            'test',
+            self.testDataDirectory,
+            False
+        )
+        assert(pars['camera_config_version']['Value'] == 999)
 
+        self.logger.info('----Testing adding a new parameter-----')
+        self.db.addNewParameter(
+            'sandbox',
+            'North-LST-Test',
+            'test',
+            'camera_config_version_test',
+            999
+        )
+        pars = self.db.readMongoDB(
+            'sandbox',
+            'North-LST-Test',
+            'test',
+            self.testDataDirectory,
+            False
+        )
+        assert(pars['camera_config_version_test']['Value'] == 999)
 
-def test_reading_db_mst_nc():
+        self.logger.info('Testing deleting a query (a whole telescope in this case and metadata)')
+        query = {'Telescope': 'North-LST-Test'}
+        self.db.deleteQuery('sandbox', 'telescopes', query)
+        query = {'Entry': 'Simulation-Model-Tags'}
+        self.db.deleteQuery('sandbox', 'metadata', query)
 
-    logger.info('----Testing reading MST-NectarCam-----')
-    db = db_handler.DatabaseHandler()
-    pars = db.getModelParameters('north', 'mst-NectarCam-D', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['camera_pixels']['Value'] == 1855)
-    else:
-        assert(pars['camera_pixels'] == 1855)
+    def test_reading_db_sites(self):
 
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
+        # This test is only relevant for the MongoDB
+        if not cfg.get('useMongoDB'):
+            return
 
-    logger.info('Removing the files written in {}'.format(testDataDirectory))
-    subprocess.call(['rm -f {}/*'.format(testDataDirectory)], shell=True)
+        self.logger.info('----Testing reading La Palma parameters-----')
+        pars = self.db.getSiteParameters('North', 'Current', self.testDataDirectory)
+        if cfg.get('useMongoDB'):
+            assert(pars['altitude']['Value'] == 2158)
+        else:
+            assert(pars['altitude'] == 2158)
 
-    return
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
 
+        self.logger.info('Removing the files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['rm -f {}/*'.format(self.testDataDirectory)], shell=True)
 
-def test_reading_db_mst_fc():
+        self.logger.info('----Testing reading Paranal parameters-----')
+        pars = self.db.getSiteParameters('South', 'Current', self.testDataDirectory)
+        if cfg.get('useMongoDB'):
+            assert(pars['altitude']['Value'] == 2147)
+        else:
+            assert(pars['altitude'] == 2147)
 
-    logger.info('----Testing reading MST-FlashCam-----')
-    db = db_handler.DatabaseHandler()
-    pars = db.getModelParameters('north', 'mst-FlashCam-D', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['camera_pixels']['Value'] == 1764)
-    else:
-        assert(pars['camera_pixels'] == 1764)
+        self.logger.info('Listing files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['ls -lh {}'.format(self.testDataDirectory)], shell=True)
 
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
+        self.logger.info('Removing the files written in {}'.format(self.testDataDirectory))
+        subprocess.call(['rm -f {}/*'.format(self.testDataDirectory)], shell=True)
 
-    logger.info('Removing the files written in {}'.format(testDataDirectory))
-    subprocess.call(['rm -f {}/*'.format(testDataDirectory)], shell=True)
-
-    return
-
-
-def test_reading_db_sst():
-
-    logger.info('----Testing reading SST-----')
-    db = db_handler.DatabaseHandler()
-    pars = db.getModelParameters('south', 'sst-D', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['camera_pixels']['Value'] == 2048)
-    else:
-        assert(pars['camera_pixels'] == 2048)
-
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
-
-    logger.info('Removing the files written in {}'.format(testDataDirectory))
-    subprocess.call(['rm -f {}/*'.format(testDataDirectory)], shell=True)
-
-    return
-
-
-def test_modify_db():
-
-    # This test is only relevant for the MongoDB
-    if not cfg.get('useMongoDB'):
         return
 
-    logger.info('----Testing copying a whole telescope-----')
-    db = db_handler.DatabaseHandler()
-    db.copyTelescope(
-        DB_CTA_SIMULATION_MODEL,
-        'North-LST-1',
-        'Current',
-        'North-LST-Test',
-        'sandbox'
-    )
-    db.copyDocuments(
-        DB_CTA_SIMULATION_MODEL,
-        'metadata',
-        {'Entry': 'Simulation-Model-Tags'},
-        'sandbox'
-    )
-    pars = db.readMongoDB('sandbox', 'North-LST-Test', 'Current', testDataDirectory, False)
-    assert(pars['camera_pixels']['Value'] == 1855)
+    def test_separating_get_and_write(self):
+        pass
 
-    logger.info('----Testing adding a parameter-----')
-    db.addParameter(
-        'sandbox',
-        'North-LST-Test',
-        'camera_config_version',
-        'test',
-        42
-    )
-    pars = db.readMongoDB('sandbox', 'North-LST-Test', 'test', testDataDirectory, False)
-    assert(pars['camera_config_version']['Value'] == 42)
-
-    logger.info('----Testing updating a parameter-----')
-    db.updateParameter(
-        'sandbox',
-        'North-LST-Test',
-        'test',
-        'camera_config_version',
-        999
-    )
-    pars = db.readMongoDB('sandbox', 'North-LST-Test', 'test', testDataDirectory, False)
-    assert(pars['camera_config_version']['Value'] == 999)
-
-    logger.info('----Testing adding a new parameter-----')
-    db.addNewParameter(
-        'sandbox',
-        'North-LST-Test',
-        'test',
-        'camera_config_version_test',
-        999
-    )
-    pars = db.readMongoDB('sandbox', 'North-LST-Test', 'test', testDataDirectory, False)
-    assert(pars['camera_config_version_test']['Value'] == 999)
-
-    logger.info('----Testing deleting a query (a whole telescope in this case and metadata)-----')
-    query = {'Telescope': 'North-LST-Test'}
-    db.deleteQuery('sandbox', 'telescopes', query)
-    query = {'Entry': 'Simulation-Model-Tags'}
-    db.deleteQuery('sandbox', 'metadata', query)
-
-    return
-
-
-def test_reading_db_sites():
-
-    # This test is only relevant for the MongoDB
-    if not cfg.get('useMongoDB'):
-        return
-
-    db = db_handler.DatabaseHandler()
-    logger.info('----Testing reading La Palma parameters-----')
-    pars = db.getSiteParameters('North', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['altitude']['Value'] == 2158)
-    else:
-        assert(pars['altitude'] == 2158)
-
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
-
-    logger.info('Removing the files written in {}'.format(testDataDirectory))
-    subprocess.call(['rm -f {}/*'.format(testDataDirectory)], shell=True)
-
-    logger.info('----Testing reading Paranal parameters-----')
-    pars = db.getSiteParameters('South', 'Current', testDataDirectory)
-    if cfg.get('useMongoDB'):
-        assert(pars['altitude']['Value'] == 2147)
-    else:
-        assert(pars['altitude'] == 2147)
-
-    logger.info('Listing files written in {}'.format(testDataDirectory))
-    subprocess.call(['ls -lh {}'.format(testDataDirectory)], shell=True)
-
-    logger.info('Removing the files written in {}'.format(testDataDirectory))
-    subprocess.call(['rm -f {}/*'.format(testDataDirectory)], shell=True)
-
-    return
-
-
-def test_separating_get_and_write():
-    pass   
 
 if __name__ == '__main__':
+    unittest.main()
 
     # test_reading_db_lst()
-    test_reading_db_mst_nc()
+    # test_reading_db_mst_nc()
     # test_reading_db_mst_fc()
     # test_reading_db_sst()
     # test_modify_db()
