aab95f1999b719b3e649502d09874b7e9b56a8fc
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-10-19 13:43:03 +0200

    function to read hists and combine them

--- simtools/simtel/simtel_histograms.py
+++ simtools/simtel/simtel_histograms.py
@@ -1,5 +1,11 @@
+import copy
 import logging
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
+from eventio import EventIOFile, Histograms
+from eventio.search_utils import yield_toplevel_of_type
 
 __all__ = ['SimtelHistograms']
 
@@ -19,4 +25,47 @@ class SimtelHistograms:
         self.histogramFiles = histogramFiles
 
     def plotAndSaveFigures(self, figName):
-        pass
+        allHists = self._combineHistogramFiles()
+
+        print(allHists)
+
+    def _combineHistogramFiles(self):
+
+        # Processing and combining histograms from multiple files
+        combinedHists = list()
+
+        n_files = 0
+        for file in self.histogramFiles:
+
+            count_file = 1
+            with EventIOFile(file) as f:
+
+                for i, o in enumerate(yield_toplevel_of_type(f, Histograms)):
+                    try:
+                        hists = o.parse()
+                    except Exception:
+                        print('Problematic file {}'.format(file))
+                        count_file = 0
+                        continue
+
+                    if len(combinedHists) == 0:
+                        # First file
+                        combinedHists = copy.copy(hists)
+
+                    else:
+                        # Remaning files
+                        for hist, thisCombinedHist in zip(hists, combinedHists):
+
+                            # Checking consistency of histograms
+                            for key_to_test in ['lower_x', 'upper_x', 'n_bins_x', 'title']:
+                                if hist[key_to_test] != thisCombinedHist[key_to_test]:
+                                    print('WARNING: {} is not consistency'.format(key_to_test))
+
+                            thisCombinedHist['data'] = np.add(
+                                thisCombinedHist['data'],
+                                hist['data']
+                            )
+
+                    n_files += count_file
+
+        return combinedHists
