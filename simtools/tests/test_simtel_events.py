f197e1b859a5e69ea740ee4839dcddf0d252a95f
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-11-05 10:23:26 +0100

    creating simtel_events and test
    Former-commit-id: 70a21063f012c2b30ba35abaf2e59ea9909e7553

---
+++ simtools/tests/test_simtel_events.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import logging
+
+import simtools.io_handler as io
+from simtools.simtel.simtel_events import SimtelEvents
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def test_reading_files():
+    files = list()
+    files.append(io.getTestDataFile(
+        'run1_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst')
+    )
+    files.append(io.getTestDataFile(
+        'run2_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst')
+    )
+
+    print(files)
+
+    simtel_events = SimtelEvents(files=files)
+
+
+if __name__ == '__main__':
+    test_reading_files()
