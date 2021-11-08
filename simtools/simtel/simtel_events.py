e0efa225e604954b319401e643ec9c9acb2218d5
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-11-08 12:01:46 +0100

    implementing loadHeader
    Former-commit-id: 360d5d377f0950da35b842c25630f9d7d482b341

--- simtools/simtel/simtel_events.py
+++ simtools/simtel/simtel_events.py
@@ -10,6 +10,10 @@ from eventio.simtel import SimTelFile
 __all__ = ['SimtelEvents']
 
 
+class InconsistentInputFile(Exception):
+    pass
+
+
 class SimtelEvents:
     '''
     This class handle sim_telarray histograms.
@@ -51,3 +55,38 @@ class SimtelEvents:
 
         for file in files:
             self.inputFiles.append(file)
+
+    def loadHeader(self):
+
+        self._numberOfFiles = len(self.inputFiles)
+        self._mcHeader = dict()
+        keysToGrab = ['obsheight', 'n_showers', 'n_use', 'core_range', 'diffuse', 'viewcone',
+                      'E_range', 'spectral_index', 'B_total']
+
+        def _areHeadersConsistent(header0, header1):
+            comparison = dict()
+            for k in keysToGrab:
+                value = (header0[k] == header1[k])
+                comparison[k] = value if isinstance(value, bool) else all(value)
+
+            return all(comparison)
+
+        for file in self.inputFiles:
+            with SimTelFile(file) as f:
+
+                if len(self._mcHeader) == 0:
+                    # First file - grabbing parameters
+                    self._mcHeader = {key: f.mc_run_headers[0][key] for key in keysToGrab}
+                else:
+                    # Remaining files - Checking whether the parameters are consistent
+                    if not _areHeadersConsistent(self._mcHeader, f.mc_run_headers[0]):
+                        msg = 'MC header pamameters from different files are inconsistent'
+                        self._logger.error(msg)
+                        raise InconsistentInputFile(msg)
+
+        # Calculating number of events
+        self._mcHeader['n_events'] = (
+            self._mcHeader['n_use'] * self._mcHeader['n_showers'] * self._numberOfFiles
+        )
+
+        return
