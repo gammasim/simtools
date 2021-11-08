03e00df08fdf349050ee31126324f8e1b012e16a
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-11-08 14:52:48 +0100

    loadHeader finished

--- simtools/tests/test_simtel_events.py
+++ simtools/tests/test_simtel_events.py
@@ -21,20 +21,30 @@ class TestSimtelEvents(unittest.TestCase):
             'run202_proton_za20deg_azm0deg-North-Prod5_test-production-5-mini.simtel.zst')
         )
 
-    def test_reading_files(self):
-        simtel_events = SimtelEvents(inputFiles=self.testFiles)
-        self.assertEqual(len(simtel_events.inputFiles), 2)
+    # def test_reading_files(self):
+    #     simtel_events = SimtelEvents(inputFiles=self.testFiles)
+    #     self.assertEqual(len(simtel_events.inputFiles), 2)
 
-    def test_loading_files(self):
-        simtel_events = SimtelEvents()
-        self.assertEqual(len(simtel_events.inputFiles), 0)
+    # def test_loading_files(self):
+    #     simtel_events = SimtelEvents()
+    #     self.assertEqual(len(simtel_events.inputFiles), 0)
 
-        simtel_events.loadInputFiles(self.testFiles)
-        self.assertEqual(len(simtel_events.inputFiles), 2)
+    #     simtel_events.loadInputFiles(self.testFiles)
+    #     self.assertEqual(len(simtel_events.inputFiles), 2)
 
-    def test_loading_header(self):
-        simtel_events = SimtelEvents(inputFiles=self.testFiles)
-        simtel_events.loadHeader()
+    # def test_loading_header(self):
+    #     simtel_events = SimtelEvents(inputFiles=self.testFiles)
+    #     simtel_events.loadHeader()
+
+    def test_larger_files(self):
+        directory = '/lustre/fs24/group/cta/users/prado/gammasim-files/simtel-data/North/gamma/data'
+        files = [
+            directory + '/run93_gamma_za20deg_azm0deg-North-Prod5_test-production-5.simtel.zst',
+            directory + '/run94_gamma_za20deg_azm0deg-North-Prod5_test-production-5.simtel.zst'
+        ]
+
+        simtel_events = SimtelEvents(inputFiles=files)
+        print(simtel_events._mcHeader)
 
 
 if __name__ == '__main__':
