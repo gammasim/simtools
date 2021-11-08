5a964f5bf22e650aba19dc5aac27cdbc1bad6e7b
RaulRPrado <raul.ribeiro.prado@gmail.com>
2021-11-08 15:04:15 +0100

    loadHeader finished

--- simtools/simtel/simtel_events.py
+++ simtools/simtel/simtel_events.py
@@ -65,7 +65,6 @@ class SimtelEvents:
                       'E_range', 'spectral_index', 'B_total']
         # Dict has to have its keys defined and filled beforehands
         self._mcHeader = {k: 0 for k in keysToGrab}
-        self._mcHeader['rscat'] = 0
 
         def _areHeadersConsistent(header0, header1):
             comparison = dict()
@@ -74,10 +73,6 @@ class SimtelEvents:
                 comparison[k] = value if isinstance(value, bool) else all(value)
 
             return all(comparison)
-
-        def _readCSCAT(corsikaInputCards):
-            rscat = str(corsikaInputCards[0]).split('CSCAT')
-            return float(rscat[1].split()[1])
 
         isFirstFile = True
         numberOfTriggeredEvents = 0
@@ -90,7 +85,6 @@ class SimtelEvents:
                 if isFirstFile:
                     # First file - grabbing parameters
                     self._mcHeader.update({k: copy(f.mc_run_headers[0][k]) for k in keysToGrab})
-                    self._mcHeader['rscat'] = _readCSCAT(f.corsika_inputcards)
                 else:
                     # Remaining files - Checking whether the parameters are consistent
                     if not _areHeadersConsistent(self._mcHeader, f.mc_run_headers[0]):
@@ -106,3 +100,17 @@ class SimtelEvents:
         )
         self._mcHeader['n_triggered'] = numberOfTriggeredEvents
         return
+
+    def selectEvents(self, energyRange=None, coreMax=None, coneMax=None):
+        print(self._mcHeader)
+        if energyRange is None:
+            energyRange = self._mcHeader['E_range']
+
+        for file in self.inputFiles:
+            with SimTelFile(file) as f:
+
+                for event in f:
+                    print(event['mc_event'].keys())
+                    break
+
+        print(energyRange)
