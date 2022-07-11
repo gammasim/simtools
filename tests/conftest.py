3e37270925d1594a995a27e391c4ecbe6d7990e6
Gernot Maier <gernot.maier@desy.de>
2022-07-11 16:32:48 +0200

    use consistently join for paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,9 +28,9 @@ def write_configuration_test_file(config_file, config_dict):
 @pytest.fixture
 def tmp_test_directory(tmpdir_factory):
     tmp_test_dir = tmpdir_factory.mktemp("test-data")
-    tmp_sub_dirs = ['/resources/', '/output/', '/simtel/']
+    tmp_sub_dirs = ['resources', 'output', 'simtel']
     for sub_dir in tmp_sub_dirs:
-        tmp_sub_dir = tmp_test_dir + sub_dir
+        tmp_sub_dir = tmp_test_dir.join(sub_dir)
         tmp_sub_dir.mkdir()
 
     return tmp_test_dir
@@ -41,12 +41,12 @@ def configuration_parameters(tmp_test_directory):
 
     return {
         'modelFilesLocations':  [
-            str(tmp_test_directory) + '/resources/',
-            './tests/resources/'
+            str(tmp_test_directory.join('resources/')),
+            str(tmp_test_directory.join('tests/resources'))
         ],
         'dataLocation': './data/',
-        'outputLocation': str(tmp_test_directory) + '/output/',
-        'simtelPath': str(tmp_test_directory) + '/simtel/',
+        'outputLocation': str(tmp_test_directory.join('output')),
+        'simtelPath': str(tmp_test_directory.join('simtel')),
         'useMongoDB': False,
         'mongoDBConfigFile': None,
         'extraCommands': [''],
@@ -78,7 +78,7 @@ def db_connection(tmp_test_directory):
             else:
                 found_env = False
         if found_env:
-            dbDetailsFileName = str(tmp_test_directory) + "dbDetails.yml"
+            dbDetailsFileName = tmp_test_directory.join("dbDetails.yml")
             cfg.createDummyDbDetails(filename=dbDetailsFileName, **parsToDbDetails)
             return dbDetailsFileName
 
@@ -98,7 +98,7 @@ def set_db(db_connection, tmp_test_directory, configuration_parameters):
     if len(str(db_connection)) == 0:
         pytest.skip("Test requires database (DB) connection")
 
-    config_file = str(tmp_test_directory) + '/config-db-test.yml'
+    config_file = tmp_test_directory.join('config-db-test.yml')
     config_dict = dict(configuration_parameters)
     config_dict['useMongoDB'] = True
     config_dict['mongoDBConfigFile'] = str(db_connection)
@@ -129,7 +129,7 @@ def set_simtelarray(simtelpath, tmp_test_directory, configuration_parameters):
     if len(str(simtelpath)) == 0:
         pytest.skip('sim_telarray not found in {}'.format(simtelpath))
 
-    config_file = str(tmp_test_directory) + '/config-simtelarray-test.yml'
+    config_file = tmp_test_directory.join('config-simtelarray-test.yml')
     config_dict = dict(configuration_parameters)
     config_dict['simtelPath'] = str(simtelpath)
     write_configuration_test_file(config_file, config_dict)
@@ -151,7 +151,7 @@ def set_simtools(db_connection, simtelpath, tmp_test_directory, configuration_pa
     if len(str(db_connection)) == 0:
         pytest.skip("Test requires database (DB) connection")
 
-    config_file = str(tmp_test_directory) + '/config-simtools-test.yml'
+    config_file = tmp_test_directory.join('config-simtools-test.yml')
     config_dict = dict(configuration_parameters)
     config_dict['simtelPath'] = str(simtelpath)
     config_dict['useMongoDB'] = True
@@ -171,6 +171,6 @@ def cfg_setup(tmp_test_directory, configuration_parameters):
     (some code duplication with cfg_setup, set_simtelarray, set_simtools, set_db)
     """
 
-    config_file = str(tmp_test_directory) + '/config-test.yml'
+    config_file = tmp_test_directory.join('config-test.yml')
     write_configuration_test_file(config_file, dict(configuration_parameters))
     cfg.setConfigFileName(config_file)
