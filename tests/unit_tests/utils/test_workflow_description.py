915b3b226fadb4edc3a931ad798ce2b69958a7f3
Gernot Maier <gernot.maier@desy.de>
2022-10-26 11:08:14 +0200

    test adaption

--- tests/unit_tests/utils/test_workflow_description.py
+++ tests/unit_tests/utils/test_workflow_description.py
@@ -13,7 +13,7 @@ logger.setLevel(logging.DEBUG)
 
 def test_fill_product_association_identifier(args_dict_site):
 
-    workflow_1 = workflow.WorkflowDescription(label="unit-test", args_dict=args_dict_site)
+    workflow_1 = workflow.WorkflowDescription(args_dict=args_dict_site)
     workflow_1.top_level_meta = data_model.top_level_reference_schema()
     workflow_1.top_level_meta["CTA"]["CONTEXT"]["SIM"]["ASSOCIATION"] = get_generic_user_meta()[
         "PRODUCT"
@@ -28,7 +28,7 @@ def test_fill_product_association_identifier(args_dict_site):
 
 def test_product_data_file_format(args_dict_site):
 
-    workflow_1 = workflow.WorkflowDescription(label="unit-test", args_dict=args_dict_site)
+    workflow_1 = workflow.WorkflowDescription(args_dict=args_dict_site)
 
     assert workflow_1.product_data_file_format(False) == "ascii.ecsv"
     assert workflow_1.product_data_file_format(True) == "ecsv"
@@ -55,7 +55,7 @@ def test_merge_config_dicts(args_dict_site):
         "DATAMODEL": "model-B",
     }
 
-    _workflow = workflow.WorkflowDescription(label="unit-test", args_dict=args_dict_site)
+    _workflow = workflow.WorkflowDescription(args_dict=args_dict_site)
     _workflow._merge_config_dicts(d_high_priority, d_low_priority)
 
     d_merged = {
@@ -70,11 +70,11 @@ def test_merge_config_dicts(args_dict_site):
 
 def test_fill_activity_meta(args_dict_site):
 
-    file_writer_1 = workflow.WorkflowDescription(label="unit-test", args_dict=args_dict_site)
+    file_writer_1 = workflow.WorkflowDescription(args_dict=args_dict_site)
     file_writer_1.top_level_meta = data_model.top_level_reference_schema()
     file_writer_1._fill_activity_meta()
 
-    file_writer_2 = workflow.WorkflowDescription(label="unit-test", args_dict=args_dict_site)
+    file_writer_2 = workflow.WorkflowDescription(args_dict=args_dict_site)
     file_writer_2.top_level_meta = data_model.top_level_reference_schema()
 
     del file_writer_2.workflow_config["ACTIVITY"]["NAME"]
