3af9d308dbb3f2314ea117348d991b86672cff6a
Orel Gueta <orel.gueta@desy.de>
2022-11-02 20:54:22 +0100

    Change variables in some tests to be in snake_case

--- tests/unit_tests/simtel/test_simtel_config_writer.py
+++ tests/unit_tests/simtel/test_simtel_config_writer.py
@@ -5,7 +5,6 @@ import logging
 import pytest
 
 from simtools.layout.layout_array import LayoutArray
-from simtools.model.telescope_model import TelescopeModel
 from simtools.simtel.simtel_config_writer import SimtelConfigWriter
 from simtools.util.general import file_has_text
 
@@ -14,27 +13,15 @@ logger.setLevel(logging.DEBUG)
 
 
 @pytest.fixture
-def telescopeModel(db_config, io_handler):
-    telescopeModel = TelescopeModel(
+def simtel_config_writer():
+
+    simtel_config_writer = SimtelConfigWriter(
         site="North",
-        telescopeModelName="LST-1",
-        modelVersion="Current",
-        label="test-telescope-model",
-        mongoDBConfig=db_config,
-    )
-    return telescopeModel
-
-
-@pytest.fixture
-def simtelConfigWriter():
-
-    simtelConfigWriter = SimtelConfigWriter(
-        site="North",
-        modelVersion="Current",
+        model_version="Current",
         label="test-simtel-config-writer",
-        telescopeModelName="TestTelecope",
+        telescope_model_name="test_telecope",
     )
-    return simtelConfigWriter
+    return simtel_config_writer
 
 
 @pytest.fixture
@@ -44,20 +31,20 @@ def layout(io_handler):
 
 
 # @pytest.mark.skip(reason="TODO :test_write_array_config_file - KeyError: 'Current'")
-def test_write_array_config_file(simtelConfigWriter, layout, telescopeModel, io_handler):
-    file = io_handler.get_output_file(fileName="simtel-config-writer_array.txt", test=True)
-    simtelConfigWriter.write_array_config_file(
-        configFilePath=file,
+def test_write_array_config_file(simtel_config_writer, layout, telescope_model_lst, io_handler):
+    file = io_handler.get_output_file(file_name="simtel-config-writer_array.txt", test=True)
+    simtel_config_writer.write_array_config_file(
+        config_file_path=file,
         layout=layout,
-        telescopeModel=[telescopeModel] * 4,
-        siteParameters={},
+        telescope_model=[telescope_model_lst] * 4,
+        site_parameters={},
     )
     assert file_has_text(file, "TELESCOPE == 1")
 
 
-def test_write_tel_config_file(simtelConfigWriter, io_handler):
-    file = io_handler.get_output_file(fileName="simtel-config-writer_telescope.txt", test=True)
-    simtelConfigWriter.write_telescope_config_file(
-        configFilePath=file, parameters={"par": {"Value": 1}}
+def test_write_tel_config_file(simtel_config_writer, io_handler):
+    file = io_handler.get_output_file(file_name="simtel-config-writer_telescope.txt", test=True)
+    simtel_config_writer.write_telescope_config_file(
+        config_file_path=file, parameters={"par": {"Value": 1}}
     )
     assert file_has_text(file, "par = 1")
