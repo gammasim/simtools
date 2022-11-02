282457b087dc5f2c0bf9c450375689d174bfbfc7
Orel Gueta <orel.gueta@desy.de>
2022-11-02 20:50:46 +0100

    Change variables in some tests to be in snake_case

--- tests/unit_tests/simtel/test_simtel_histograms.py
+++ tests/unit_tests/simtel/test_simtel_histograms.py
@@ -12,20 +12,22 @@ def test_histograms(io_handler):
     histogram_files = list()
     histogram_files.append(
         io_handler.get_input_data_file(
-            fileName="run1_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst", test=True
+            file_name="run1_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst",
+            test=True,
         )
     )
     histogram_files.append(
         io_handler.get_input_data_file(
-            fileName="run2_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst", test=True
+            file_name="run2_gamma_za20deg_azm0deg-North-Prod5_test-production-5.hdata.zst",
+            test=True,
         )
     )
 
-    hists = SimtelHistograms(histogramFiles=histogram_files, test=True)
+    hists = SimtelHistograms(histogram_files=histogram_files, test=True)
 
-    figName = io_handler.get_output_file(
-        fileName="simtelHistograms.pdf", dirType="plots", test=True
+    fig_name = io_handler.get_output_file(
+        file_name="simtel_histograms.pdf", dir_type="plots", test=True
     )
-    hists.plot_and_save_figures(figName=figName)
+    hists.plot_and_save_figures(fig_name=fig_name)
 
-    assert figName.exists()
+    assert fig_name.exists()
