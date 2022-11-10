ce56d12dbeb8492451ca05dc65f9e32491debf45
Orel Gueta <orel.gueta@desy.de>
2022-11-10 11:27:23 +0100

    Add a test for has_file

--- tests/unit_tests/simtel/test_simtel_runner_array.py
+++ tests/unit_tests/simtel/test_simtel_runner_array.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import logging
+import shutil
 from pathlib import Path
 
 import astropy.units as u
@@ -48,7 +49,7 @@ def simtel_runner(array_model, simtelpath):
 @pytest.fixture
 def corsika_file(io_handler):
     corsika_file = io_handler.get_input_data_file(
-        file_name="run1_proton_za20deg_azm0deg-North-1LST_trigger_rates.corsika.zst", test=True
+        file_name="run1_proton_za20deg_azm0deg_North_1LST_test-lst-array.corsika.zst", test=True
     )
     return corsika_file
 
@@ -89,3 +90,17 @@ def test_get_file_name(simtel_runner):
     ) == simtel_runner._simtel_log_dir.joinpath(f"log_sub_{file_name}.out")
     with pytest.raises(ValueError):
         simtel_runner.get_file_name("foobar", **info_for_file_name, mode="out")
+
+
+def test_has_file(simtel_runner, corsika_file):
+    # Copying the corsika file to the expected location and
+    # changing its name for the sake of this test.
+    # This should not affect the efficacy of this test.
+    shutil.copy(
+        corsika_file,
+        simtel_runner._simtel_data_dir.joinpath(
+            "run1_proton_za20deg_azm0deg_North_1LST_test-lst-array.simtel.zst"
+        ),
+    )
+    assert simtel_runner.has_file(file_type="output", run_number=1)
+    assert not simtel_runner.has_file(file_type="log", run_number=1234)
