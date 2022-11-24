138beec1d03dfc411066a5a2a17f45aa77b42221
Gernot Maier <gernot.maier@desy.de>
2022-11-24 07:33:53 +0100

    sphinx error fixes

--- simtools/model/mirrors.py
+++ simtools/model/mirrors.py
@@ -14,6 +14,7 @@ class InvalidMirrorListFile(Exception):
 class Mirrors:
     """
     Mirrors class, created from a mirror list file.
+
     Parameters
     ----------
     mirror_list_file: (str, required)
