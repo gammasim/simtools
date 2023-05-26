66511b3efbeb99e464e90e58e12990aa96ff028d
Orel Gueta <orel.gueta@desy.de>
2023-05-26 17:06:06 +0200

    Add the run command

--- simtools/corsika_simtel/corsika_simtel_runner.py
+++ simtools/corsika_simtel/corsika_simtel_runner.py
@@ -1,4 +1,5 @@
 from simtools.corsika.corsika_runner import CorsikaRunner
+from simtools.simtel.simtel_runner_array import SimtelRunnerArray
 
 __all__ = ["CorsikaSimtelRunner"]
 
@@ -7,7 +8,7 @@ class MissingRequiredEntryInCorsikaConfig(Exception):
     """Exception for missing required entry in corsika config."""
 
 
-class CorsikaSimtelRunner(CorsikaRunner):
+class CorsikaSimtelRunner(CorsikaRunner, SimtelRunnerArray):
     """
     CorsikaSimtelRunner is responsible for running CORSIKA and piping it to sim_telarray
     using the multipipe functionality. CORSIKA is set up using corsika_autoinputs program
@@ -44,28 +45,18 @@ class CorsikaSimtelRunner(CorsikaRunner):
 
     Parameters
     ----------
-    mongo_db_config: dict
-        MongoDB configuration.
-    site: str
-        South or North.
-    layout_name: str
-        Name of the layout.
-    label: str
-        Instance label.
-    keep_seeds: bool
-        Use seeds based on run number and primary particle.  If False, use sim_telarray seeds.
-    simtel_source_path: str or Path
-        Location of source of the sim_telarray/CORSIKA package.
-    corsika_config_data: dict
-        Dict with CORSIKA config data.
-    corsika_config_file: str or Path
-        Path to yaml file containing CORSIKA config data.
-    corsika_parameters_file: str or Path
-        Path to yaml file containing CORSIKA parameters.
+    common_args: dict
+        Arguments common to both CORSIKA and sim_telarray runners
+    corsika_args: dict
+        Arguments for the CORSIKA runner (see full list in CorsikaRunner documentation).
+    simtel_args: dict
+        Arguments for the sim_telarray runner (see full list in SimtelRunnerArray documentation).
     """
 
-    def __init__(self, *args, **kwargs):
-        super(CorsikaSimtelRunner, self).__init__(use_multipipe=True, *args, **kwargs)
+    def __init__(self, common_args=None, corsika_args=None, simtel_args=None):
+
+        CorsikaRunner.__init__(self, use_multipipe=True, **(common_args | corsika_args))
+        SimtelRunnerArray.__init__(self, **(common_args | simtel_args))
 
     def get_run_script(self, **kwargs):
         """
@@ -84,7 +75,8 @@ class CorsikaSimtelRunner(CorsikaRunner):
             Full path of the run script file.
         """
         self.export_multipipe_script(**kwargs)
-        return super().get_run_script(use_pfp=False, **kwargs)
+        # SimtelRunnerArray.array_model.export_all_simtel_config_files(self)
+        return CorsikaRunner.get_run_script(self, use_pfp=False, **kwargs)
 
     def export_multipipe_script(self, **kwargs):
         """
@@ -102,16 +94,59 @@ class CorsikaSimtelRunner(CorsikaRunner):
         Path:
             Full path of the run script file.
         """
-        extra_commands = ""
-        if kwargs["extra_commands"] is not None and len(kwargs["extra_commands"]) > 0:
-            extra_commands = kwargs["extra_commands"]
+
+        run_command = self._make_run_command(
+            run_number=kwargs["run_number"],
+            input_file="-",  # Tell sim_telarray to take the input from standard output
+        )
         multipipe_file = (
             "/workdir/external/gammasim-tools/simtools-output/TEST/corsika_simtel/"
             f"multi_cta-{self.site}-{self.layout_name}.cfg"
         )
         with open(multipipe_file, "w") as file:
-            file.write(
-                'env offset="0.0" cfg="cta-prod5-lapalma" '
-                f'extra_defs="{extra_commands} -DNSB_AUTOSCALE -DNECTARCAM" '
-                'extra_suffix="-2100m-LaPalma-dark" extension="zst" ./generic_run.sh'
-            )
+            file.write(f"{run_command}")
+
+    def _make_run_command(self, **kwargs):
+        """
+        Builds and returns the command to run simtel_array.
+
+        Parameters
+        ----------
+        kwargs: dict
+            The dictionary must include the following parameters (unless listed as optional):
+                input_file: str
+                    Full path of the input CORSIKA file
+                run_number: int (optional)
+                    run number
+
+        """
+
+        # TODO: These definitions of the files can probably be separated from
+        # the the run command and put back into the parent class.
+        run_number = kwargs["run_number"] if "run_number" in kwargs else 1
+        info_for_file_name = SimtelRunnerArray.get_info_for_file_name(self, run_number)
+        self._log_file = SimtelRunnerArray.get_file_name(
+            self, file_type="log", **info_for_file_name
+        )
+        histogram_file = SimtelRunnerArray.get_file_name(
+            self, file_type="histogram", **info_for_file_name
+        )
+        output_file = SimtelRunnerArray.get_file_name(
+            self, file_type="output", **info_for_file_name
+        )
+
+        # Array
+        command = str(self._simtel_source_path.joinpath("sim_telarray/bin/sim_telarray"))
+        command += f" -c {self.array_model.get_config_file()}"
+        command += f" -I{self.array_model.get_config_directory()}"
+        command += super()._config_option("telescope_theta", self.config.zenith_angle)
+        command += super()._config_option("telescope_phi", self.config.azimuth_angle)
+        command += super()._config_option("power_law", "2.5")
+        command += super()._config_option("histogram_file", histogram_file)
+        command += super()._config_option("output_file", output_file)
+        command += super()._config_option("random_state", "auto")
+        command += super()._config_option("show", "all")
+        command += " " + str(kwargs["input_file"])
+        command += " > " + str(self._log_file) + " 2>&1"
+
+        return command
