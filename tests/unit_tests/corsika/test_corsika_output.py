8f13bb981a9bbdf30fbac1c4c9ae1ae441c1cd22
Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>
2023-06-05 12:24:16 +0200

    start tests

--- tests/unit_tests/corsika/test_corsika_output.py
+++ tests/unit_tests/corsika/test_corsika_output.py
@@ -6,7 +6,7 @@ import simtools.util.general as gen
 from simtools.corsika.corsika_output import CorsikaOutput
 
 test_file_name = "tel_output_10GeV-2-gamma-20deg-CTAO-South.dat"
-test_file_name = "tel_output.dat"
+# test_file_name = "tel_output.dat"
 
 
 @pytest.fixture
@@ -27,3 +27,17 @@ def corsika_output_instance(db, io_handler, corsika_output_file):
     # )
     # return CorsikaOutput(corsika_output_file)
     return CorsikaOutput(test_file_name)
+
+
+def test_file_exists(corsika_output_file):
+    assert corsika_output_file.exists()
+
+
+def test_init(corsika_output_instance):
+    assert corsika_output_instance.input_file.name == test_file_name
+    with pytest.raises(FileNotFoundError):
+        CorsikaOutput("wrong_file_name")
+    assert len(corsika_output_instance.event_information) > 15
+    assert "zenith" in corsika_output_instance.event_information
+    assert len(corsika_output_instance.header) > 10
+    assert corsika_output_instance.version == 7.741
