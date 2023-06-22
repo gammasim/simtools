3f49ff84c26643d3a2237368e72955c7ed6d96c5
Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>
2023-06-22 15:45:39 +0200

    test did not work, return to previous setup. rely on the new pyeventio release

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import find_packages, setup
 
-setup(name="simtools", packages=find_packages(), install_requires=["corsikaio >=0.3.3"])
+setup(name="simtools", packages=find_packages())
