197445171b736b70fe60addc2dbb6858aab157ae
orelgueta <orel.gueta@desy.de>
2023-08-04 14:05:31 +0200

    Update version.py

--- simtools/version.py
+++ simtools/version.py
@@ -1,5 +1,5 @@
-# this is adaped from https://github.com/cta-observatory/ctapipe/blob/main/ctapipe/version.py
-# this is adapted from https://github.com/astropy/astropy/blob/master/astropy/version.py
+# this is adapted from https://github.com/cta-observatory/ctapipe/blob/main/ctapipe/version.py
+# which is adapted from https://github.com/astropy/astropy/blob/master/astropy/version.py
 # see https://github.com/astropy/astropy/pull/10774 for a discussion on why this needed.
 
 try:
