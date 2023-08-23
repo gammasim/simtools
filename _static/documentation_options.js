cb821787969ec7bf6ee7a12b6f7aa73fa73220bf
GernotMaier <GernotMaier@users.noreply.github.com>
2023-08-23 15:52:49 +0000

    Deploying to gh-pages from @ gammasim/simtools@8b2ac9b2e1901e145ed9ca382c6e64fbf041465d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g469b903',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev1+g8b2ac9b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
