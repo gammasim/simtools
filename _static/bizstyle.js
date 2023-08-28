47a4d5d978b4f08735a5738f3a29ffba30d5c49d
orelgueta <orelgueta@users.noreply.github.com>
2023-08-28 14:39:35 +0000

    Deploying to gh-pages from @ gammasim/simtools@5b342f6e582c3a4f12a3ae2550186694565000b4 🚀

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "simtools v0.1.dev1+gc80dfa8 Manual")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "simtools v0.1.dev1+g5b342f6 Manual")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+gc80dfa8',
+    VERSION: '0.1.dev1+g5b342f6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
