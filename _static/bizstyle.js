8a0a1e6975ceb33f10f0f09378cd8be53a732ffd
GernotMaier <GernotMaier@users.noreply.github.com>
2023-08-29 08:25:34 +0000

    Deploying to gh-pages from @ gammasim/simtools@e185cdf53eda49c763a900b2c35a9eb81f45a6f4 🚀

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "simtools v0.1.dev1+g5b342f6 Manual")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "simtools v0.1.dev1+ge185cdf Manual")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g5b342f6',
+    VERSION: '0.1.dev1+ge185cdf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
