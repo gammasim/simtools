900df0151bfccf97f4b7f4829a750ae097eb86ea
Gernot Maier <gernot.maier@desy.de>
2023-08-30 11:10:00 +0200

    load environmental variables with dotenv

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# entrypoint script to configure development environment
-
-cd /workdir/external/simtools
-pip install -e .
-
-if [[ -e "set_DB_environ.sh" ]]; then
-    source ./set_DB_environ.sh
-fi
