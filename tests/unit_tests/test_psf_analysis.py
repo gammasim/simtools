54bc9ab7d1c38024d9498c6ab0a3a7ea2dbdb212
Orel Gueta <orel.gueta@desy.de>
2023-09-01 23:29:16 +0200

    Change to use new gzipped photon file

--- tests/unit_tests/test_psf_analysis.py
+++ tests/unit_tests/test_psf_analysis.py
@@ -12,11 +12,11 @@ logger.setLevel(logging.DEBUG)
 
 def test_reading_simtel_file(args_dict, io_handler):
     test_file = io_handler.get_input_data_file(
-        file_name="photons-LST-d12.0-za20.0-off0.000_lst_integral.lis",
+        file_name="photons-North-LST-1-d10.0-za20.0-off0.000_tune_psf.lis.gz",
         test=True,
     )
     image = PSFImage(focal_length=2800.0)
     image.read_photon_list_from_simtel_file(test_file)
     logger.info(image.get_psf(0.8, "cm"))
 
-    assert 3.2248259134010397 == pytest.approx(image.get_psf(0.8, "cm"))
+    assert 3.1020426300932917 == pytest.approx(image.get_psf(0.8, "cm"))
