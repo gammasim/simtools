089a10f5eff5572927315b6df7dbd2c930792417
Orel Gueta <orel.gueta@desy.de>
2023-09-01 17:53:25 +0200

    Add a test for single mirror mode

--- tests/unit_tests/test_ray_tracing.py
+++ tests/unit_tests/test_ray_tracing.py
@@ -1,67 +1,38 @@
 #!/usr/bin/python3
 
+import logging
+
 import astropy.units as u
 
-import simtools.utils.general as gen
-from simtools.model.telescope_model import TelescopeModel
 from simtools.ray_tracing import RayTracing
 
 
-def test_config_data_from_dict(db_config, simtel_path, io_handler):
-    label = "test-config-data"
-    version = "prod5"
-
+def test_config_data_from_dict(simtel_path, io_handler, telescope_model_mst, caplog):
     config_data = {
         "source_distance": 10 * u.km,
         "zenith_angle": 30 * u.deg,
         "off_axis_angle": [0, 2] * u.deg,
     }
 
-    tel = TelescopeModel(
-        site="north",
-        telescope_model_name="mst-FlashCam-D",
-        model_version=version,
-        label=label,
-        mongo_db_config=db_config,
-    )
-
-    ray = RayTracing(
-        telescope_model=tel,
-        simtel_source_path=simtel_path,
-        config_data=config_data,
-    )
+    with caplog.at_level(logging.DEBUG):
+        ray = RayTracing(
+            telescope_model=telescope_model_mst,
+            simtel_source_path=simtel_path,
+            config_data=config_data,
+        )
 
     assert ray.config.zenith_angle == 30
     assert len(ray.config.off_axis_angle) == 2
+    assert "Initializing RayTracing class" in caplog.text
 
 
-def test_from_kwargs(db, io_handler, simtel_path):
-    label = "test-from-kwargs"
-
+def test_from_kwargs(io_handler, simtel_path, telescope_model_mst):
     source_distance = 10 * u.km
     zenith_angle = 30 * u.deg
     off_axis_angle = [0, 2] * u.deg
 
-    test_file_name = "CTA-North-LST-1-Current_test-telescope-model.cfg"
-    db.export_file_db(
-        db_name="test-data",
-        dest=io_handler.get_output_directory(sub_dir="model", dir_type="test"),
-        file_name=test_file_name,
-    )
-
-    cfg_file = gen.find_file(
-        test_file_name, io_handler.get_output_directory(sub_dir="model", dir_type="test")
-    )
-
-    tel = TelescopeModel.from_config_file(
-        site="north",
-        telescope_model_name="lst-1",
-        config_file_name=cfg_file,
-        label=label,
-    )
-
     ray = RayTracing.from_kwargs(
-        telescope_model=tel,
+        telescope_model=telescope_model_mst,
         simtel_source_path=simtel_path,
         source_distance=source_distance,
         zenith_angle=zenith_angle,
@@ -70,3 +41,27 @@ def test_from_kwargs(db, io_handler, simtel_path):
 
     assert ray.config.zenith_angle == 30
     assert len(ray.config.off_axis_angle) == 2
+
+
+def test_config_data_from_dict_single_mirror_mode(
+    simtel_path, io_handler, telescope_model_mst, caplog
+):
+    telescope_model_mst.export_config_file()
+    config_data = {
+        "source_distance": 10 * u.km,
+        "zenith_angle": 30 * u.deg,
+        "off_axis_angle": [0, 2] * u.deg,
+        "single_mirror_mode": True,
+    }
+
+    with caplog.at_level(logging.DEBUG):
+        ray = RayTracing(
+            telescope_model=telescope_model_mst,
+            simtel_source_path=simtel_path,
+            config_data=config_data,
+        )
+
+    assert ray.config.zenith_angle == 30
+    assert len(ray.config.off_axis_angle) == 2
+    assert ray.config.single_mirror_mode == True
+    assert "Single mirror mode is activated" in caplog.text
