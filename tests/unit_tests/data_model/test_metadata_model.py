d644a5aeb428842eb5d00997de429b9e783ffee8
Gernot Maier <gernot.maier@desy.de>
2023-11-15 17:34:15 +0100

    simplifications

--- tests/unit_tests/data_model/test_metadata_model.py
+++ tests/unit_tests/data_model/test_metadata_model.py
@@ -1,7 +1,3 @@
-import copy
-
-import pytest
-
 from simtools.data_model import metadata_model
 
 
@@ -12,58 +8,3 @@ def test_top_level_reference_schema():
     assert len(_top_meta) > 0
 
     assert "VERSION" in _top_meta["CTA"]["REFERENCE"]
-
-
-def test_metadata_input_reference_schema():
-    """(very hard to test this)"""
-
-    _top_ref = metadata_model.metadata_input_reference_schema()
-
-    assert isinstance(_top_ref, dict)
-    assert len(_top_ref) > 0
-
-    assert "VERSION" in _top_ref["REFERENCE"]
-
-
-def test_metadata_input_reference_document_list():
-    assert "SITE" in metadata_model.metadata_input_reference_document_list("instrument_list")
-    assert "SITE" in metadata_model.metadata_input_reference_document_list("INSTRUMENT_LIST")
-    assert "TYPE" in metadata_model.metadata_input_reference_document_list("document_list")
-    with pytest.raises(metadata_model.InvalidSchemaList, match=r"Invalid schema list: wrong_list"):
-        metadata_model.metadata_input_reference_document_list("wrong_list")
-
-
-def test_metadata_dict_with_defaults():
-    _test_dict = {
-        "INSTRUMENT": {
-            "SITE": {"type": "str", "required": True, "default": "North"},
-            "SUBTYPE": {"type": "str", "required": False, "default": None},
-        }
-    }
-    assert metadata_model._metadata_dict_with_defaults(_test_dict) == {
-        "INSTRUMENT": {"SITE": "North", "SUBTYPE": None}
-    }
-    _test_dict_2 = {
-        "INSTRUMENT": {
-            "SITE": {"type": "str", "required": True},
-            "SUBTYPE": {"type": "str", "required": False, "default": None},
-        }
-    }
-
-    with pytest.raises(
-        metadata_model.InvalidSchemaList,
-        match=r"Invalid schema list with missing type, required, or default fields",
-    ):
-        metadata_model._metadata_dict_with_defaults(_test_dict_2)
-
-
-def test_remove_empty_lists():
-    _test_dict_1 = {"SITE": {"type": "str", "required": True, "default": "North"}}
-
-    assert metadata_model._remove_empty_lists(copy.deepcopy(_test_dict_1)) == _test_dict_1
-
-    _test_dict_2 = {
-        "SITE": {"type": "str", "required": True, "default": "North"},
-        "ASSOCIATION": [],
-    }
-    assert metadata_model._remove_empty_lists(copy.deepcopy(_test_dict_2)) == _test_dict_1
