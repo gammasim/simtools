53969ecbac51fa3138e40d55efe3aa34fe9bb8a3
Gernot Maier <gernot.maier@desy.de>
2023-12-05 16:29:57 +0100

    pylint

--- simtools/data_model/metadata_collector.py
+++ simtools/data_model/metadata_collector.py
@@ -114,7 +114,7 @@ class MetadataCollector:
             pass
 
         self._logger.warning("No schema file found.")
-        return ''
+        return None
 
     def get_data_model_schema_dict(self):
         """
