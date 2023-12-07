11123bd00f66ab2bc8ef679756d32be101902ff7
tobiaskleiner <tobias.kleiner@desy.de>
2023-12-07 11:58:01 +0100

    add test

--- tests/unit_tests/model/test_mirrors.py
+++ tests/unit_tests/model/test_mirrors.py
@@ -26,3 +26,14 @@ def test_read_list(db, io_handler):
     assert 198 == mirrors.number_of_mirrors
     assert 151.0 == pytest.approx(mirrors.diameter)
     assert 3 == mirrors.shape
+
+def test_read_mirror_list_from_ecsv():
+    test_file = io_handler.get_input_data_file(
+        file_name="mirror_list_CTA-N-LST1_v2019-03-31_rotated.ecsv",
+        test=True,
+    )
+    logger.info(f"Using mirror list {mirror_list_file}")
+    mirrors = Mirrors(test_file)
+    assert 198 == mirrors.number_of_mirrors
+    assert 151.0 == pytest.approx(mirrors.diameter)
+    assert 3 == mirrors.shape
