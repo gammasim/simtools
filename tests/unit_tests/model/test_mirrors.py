f5354f5237cd3cab1e3fb44577a5e40f8f7d55ef
tobiaskleiner <tobias.kleiner@desy.de>
2023-12-08 15:06:55 +0100

    use simtel mirror list in updated format

--- tests/unit_tests/model/test_mirrors.py
+++ tests/unit_tests/model/test_mirrors.py
@@ -12,14 +12,9 @@ logger.setLevel(logging.DEBUG)
 
 
 def test_read_list(db, io_handler):
-    test_file_name = "mirror_CTA-LST-flen_grouped.dat"
-    db.export_file_db(
-        db_name=db.DB_CTA_SIMULATION_MODEL,
-        dest=io_handler.get_output_directory(sub_dir="model", dir_type="test"),
-        file_name=test_file_name,
-    )
-    mirror_list_file = gen.find_file(
-        test_file_name, io_handler.get_output_directory(sub_dir="model", dir_type="test")
+    mirror_list_file = io_handler.get_input_data_file(
+        file_name="mirror_list_CTA-N-LST1_v2019-03-31_rotated_simtel.dat",
+        test=True,
     )
     logger.info(f"Using mirror list {mirror_list_file}")
     mirrors = Mirrors(mirror_list_file)
