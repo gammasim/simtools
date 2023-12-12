8b91565f2e24141256ce7491c00d6b03368e7c19
Gernot Maier <gernot.maier@desy.de>
2023-12-12 20:45:41 +0100

    remove debug statement

--- tests/integration_tests/test_application_from_config.py
+++ tests/integration_tests/test_application_from_config.py
@@ -55,7 +55,6 @@ def get_list_of_test_configurations(get_test_names=False):
         _dict = gen.remove_substring_recursively_from_dict(
             gen.collect_data_from_yaml_or_dict(in_yaml=config_file, in_dict=None), substring="\n"
         )
-        print("AAAA", _dict)
         configs.append(_dict.get("CTA_SIMPIPE", None))
 
     # list of all applications
