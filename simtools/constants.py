56c5ecba7e0b6146e543d15e7a60e4565eb6761c
Gernot Maier <gernot.maier@desy.de>
2024-01-03 12:34:53 +0100

    add model url

--- simtools/constants.py
+++ simtools/constants.py
@@ -7,3 +7,9 @@ DATA_JSON_SCHEMA = "schemas/data.metaschema.yml"
 
 # URL to the schema repository
 SCHEMA_URL = "https://raw.githubusercontent.com/gammasim/workflows/main/schemas/"
+
+# URL to the repository with the simulation model
+# (temporary used for the development of the simulation model schema;
+# will be replaced by the database
+# TODO Temporarily set to a local path for easier development
+SIMULATION_MODEL_URL = "../simulation_model/verified_model/"
