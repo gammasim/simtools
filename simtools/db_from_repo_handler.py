4c4a356fb49f29bb8d413ff8dda4cdea3f1d9a32
Gernot Maier <gernot.maier@desy.de>
2024-01-12 14:06:11 +0100

    simplify reading from simulation model repo

--- simtools/db_from_repo_handler.py
+++ simtools/db_from_repo_handler.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 def update_site_parameters_from_repo(parameters, site, model_version):
     """
-    Update site parameters with values from repositories.
+    Update site parameters with values from a repository.
     Existing entries will be updated, new entries will be added.
 
     Parameters
@@ -24,6 +24,13 @@ def update_site_parameters_from_repo(parameters, site, model_version):
         Dictionary with parameters to be updated.
     site: str
         South or North.
+    model_version: str
+        Model version to use.
+
+    Returns
+    -------
+    dict
+        Updated dictionary with parameters.
 
     """
 
@@ -32,14 +39,28 @@ def update_site_parameters_from_repo(parameters, site, model_version):
         f" and model version {model_version}"
     )
 
-    for key, value in parameters.items():
-        file_path = Path(simtools.constants.SIMULATION_MODEL_URL, "Site", site, f"{key}.json")
-        if file_path.exists():
+    for key, value in site_parameters.items():
+        file_path = Path(simtools.constants.SIMULATION_MODEL_URL, "Site", site, f"{value}.json")
+        try:
             logger.debug(f"Updating parameter {key} for {site} from repository file {file_path}")
             parameters[key] = gen.collect_data_from_file_or_dict(file_name=file_path, in_dict=None)
-            logger.debug(f"Old value: {value}, new value: {parameters[key]}")
-        else:
-            logger.debug(f"Parameter {key} for {site} not found in repository (file {file_path})")
-            logger.debug(f"Keeping old value: {value}")
+        except FileNotFoundError:
+            logger.error("Missing parameter file %s", file_path)
+            raise
+        try:
+            logger.debug(f"Updated parameter {key} for {site} from old value: {parameters[key]}")
+        except KeyError:
+            logger.debug(f"Added parameter {key} for {site}")
+        parameters[key] = gen.collect_data_from_file_or_dict(file_name=file_path, in_dict=None)
 
     return parameters
+
+
+# simulation_model parameter naming to DB parameter naming mapping
+# TODO - decide if this should be moved somewhere else
+site_parameters = {
+    "altitude": "reference_point_altitude",
+    "ref_lon": "reference_point_longitude",
+    "ref_lat": "reference_point_latitude",
+    "corsika_obs_level": "corsika_observation_level",
+}
