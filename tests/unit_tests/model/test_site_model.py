ac413598e9f1d87b91b9895264ef31f54e0ce0d5
Gernot Maier <gernot.maier@desy.de>
2024-02-05 14:05:33 +0100

    tests

--- tests/unit_tests/model/test_site_model.py
+++ tests/unit_tests/model/test_site_model.py
@@ -11,4 +11,12 @@ logger.setLevel(logging.DEBUG)
 def test_site_model(db_config):
     _south = SiteModel(site="South", mongo_db_config=db_config, label="testing-sitemodel")
 
-    print(_south._parameters)
+    assert isinstance(_south.get_reference_point(), dict)
+    for key in ["center_altitude", "center_northing", "center_easting", "epsg_code"]:
+        assert key in _south.get_reference_point()
+
+    assert "corsika_observation_level" in _south.get_corsika_site_parameters()
+
+    _pars = _south.get_simtel_parameters()
+    assert "altitude" in _pars
+    assert isinstance(_pars["altitude"], float)
