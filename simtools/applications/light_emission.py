46f09715c4c576e153586444ee7b161faaf718d6
tobiaskleiner <tobias.kleiner@desy.de>
2024-02-09 15:22:18 +0100

    initial commit

---
+++ simtools/applications/light_emission.py
@@ -0,0 +1,128 @@
+import logging
+import subprocess
+from pathlib import Path
+
+import astropy.units as u
+
+import simtools.utils.general as gen
+from simtools.configuration import configurator
+from simtools.io_operations import io_handler
+from simtools.model.telescope_model import TelescopeModel
+from simtools.simtel.simtel_light_emission import SimulatorLightEmission
+
+
+def _parse(label):
+    """
+    Parse command line configuration
+    """
+
+    config = configurator.Configurator(
+        label=label,
+        description=(
+            "Simulate the light emission by an artificial light source for calibration purposes."
+        ),
+    )
+
+    config.parser.add_argument(
+        "--ls_distance",
+        help="Light source distance in m",
+        type=float,
+        default=1000,
+    )
+    config.parser.add_argument(
+        "--off_axis_angle",
+        help="Off axis angle for light source direction",
+        type=float,
+        default=0.0,
+    )
+    config.parser.add_argument(
+        "--plot",
+        help="Produce a multiple pages pdf file with the image plots.",
+        action="store_true",
+    )
+    return config.initialize(db_config=True, telescope_model=True, require_command_line=False)
+
+
+def default_le_configs(le_application):
+    """Predefined angular distribution names not requiring to read any table are
+    "Isotropic", "Gauss:<rms>", "Rayleigh", "Cone:<angle>", and "FilledCone:<angle>", "Parallel",
+     with all angles given in degrees, all with respect to the given direction vector
+    (vertically downwards if missing). If the light source has a non-zero length and velocity
+    (in units of the vaccum speed of light), it is handled as a moving source,
+    in the given direction.
+    """
+
+    if le_application == "xyzls":
+        default_config = {
+            "beam_shape": {
+                "len": 1,
+                "unit": str,
+                "default": "Gauss",
+                "names": ["beam_shape", "angular_distribution"],
+            },
+            "beam_width": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 0.0 * u.deg,
+                "names": ["rms"],
+            },
+            "pulse_shape": {
+                "len": 1,
+                "unit": str,
+                "default": "Gauss",
+                "names": ["pulse_shape"],
+            },
+            "pulse_width": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 5 * u.ns,
+                "names": ["rms"],
+            },
+        }
+    return default_config
+
+
+def main():
+    label = Path(__file__).stem
+    args_dict, db_config = _parse(label)
+    # TODO: generalize the following
+    args_dict["telescope"] = "LST-1"
+    args_dict["site"] = "north"
+    le_application = "xyzls"
+
+    default_le_config = default_le_configs(le_application)
+    logger = logging.getLogger()
+    logger.setLevel(gen.get_log_level_from_user(args_dict["log_level"]))
+
+    # Output directory to save files related directly to this app
+    _io_handler = io_handler.IOHandler()
+    output_dir = _io_handler.get_output_directory(label)
+
+    telescope_model = TelescopeModel(
+        site=args_dict["site"],
+        telescope_model_name=args_dict["telescope"],
+        mongo_db_config=db_config,
+        model_version=args_dict["model_version"],
+        label=label,
+    )
+    le = SimulatorLightEmission.from_kwargs(
+        telescope_model=telescope_model,
+        default_le_config=default_le_config,
+        le_application=le_application,
+        output_dir=output_dir,
+        label=label,
+        simtel_source_path=args_dict["simtel_path"],
+    )
+    # command = le._make_light_emission_script()
+    # command = le._make_simtel_script(output_dir)
+    run_script = le.prepare_script()
+
+    # currently to make this work we need to copy the atmospheric profile:
+    # cp ./sim_telarray/cfg/common/atmprof1.dat ./
+    # or need to change how this file is searched for
+
+    subprocess.run(run_script, shell=False, check=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ simtools/simtel/simtel_light_emission.py
@@ -0,0 +1,260 @@
+import logging
+import os
+
+import astropy.units as u
+
+import simtools.utils.general as gen
+from simtools.io_operations import io_handler
+from simtools.simtel.simtel_runner import SimtelRunner
+
+__all__ = ["SimulatorLightEmission"]
+
+
+class SimulatorLightEmission(SimtelRunner):
+    """
+    SimulatorLightEmission is the interface with sim_telarray to perform
+    light emission package simulations.
+
+    Configurable parameters:
+        zenith_angle:
+            len: 1
+            unit: deg
+            default: 20 deg
+
+    Parameters
+    ----------
+    simtel_source_path: str or Path
+        Location of sim_telarray installation.
+    config_data: dict
+        Dict containing the configurable parameters.
+    config_file: str or Path
+        Path of the yaml file containing the configurable parameters.
+    force_simulate: bool
+        Remove existing files and force re-running of the light emission simulation.
+    """
+
+    def __init__(
+        self,
+        telescope_model,
+        default_le_config,
+        le_application,
+        output_dir,
+        label=None,
+        simtel_source_path=None,
+        config_data=None,
+        config_file=None,
+        test=False,
+    ):
+        """
+        Initialize SimtelRunner
+        """
+        super().__init__(label=label, simtel_source_path=simtel_source_path)
+
+        self._logger = logging.getLogger(__name__)
+        self._logger.debug("Init SimtelRunnerLightEmission")
+
+        self._simtel_source_path = simtel_source_path
+        self._telescope_model = telescope_model
+        self.label = label if label is not None else self._telescope_model.label
+
+        self.io_handler = io_handler.IOHandler()
+        self._base_directory = self.io_handler.get_output_directory(self.label)
+        print("base directory: ", self._base_directory)
+        # Loading config_data, currently use default config
+        self.config = gen.validate_config_data(
+            gen.collect_data_from_file_or_dict(config_file, config_data),
+            self.light_emission_default_configuration(),
+        )
+
+        # LightEmission - default parameters
+        self._rep_number = 0
+        self.runs = 1
+        self.photons_per_run = 100000 if not test else 10000
+
+        self.le_application = le_application
+        self.default_le_config = default_le_config
+        self.output_dir = output_dir
+        # self._load_required_files(force_simulate)
+
+    @classmethod
+    def from_kwargs(cls, **kwargs):
+        """
+        Builds a LightEmission object from kwargs only.
+        The configurable parameters can be given as kwargs, instead of using the
+        config_data or config_file arguments.
+
+        Parameters
+        ----------
+        kwargs
+            Containing the arguments and the configurable parameters.
+
+        Returns
+        -------
+        Instance of this class.
+        """
+        args, config_data = gen.separate_args_and_config_data(
+            expected_args=[
+                "telescope_model",
+                "default_le_config",
+                "le_application",
+                "output_dir",
+                "label",
+                "simtel_source_path",
+            ],
+            **kwargs,
+        )
+        return cls(**args, config_data=config_data)
+
+    def _make_light_emission_script(self, **kwargs):  # pylint: disable=unused-argument
+        print("default config", self.default_le_config["beam_shape"])
+        # ./xyzls -a Gauss:3 -p Gauss:0.1 -n 1e5,1e6,1e7
+        command = f" rm {self.le_application}.simtel.gz\n"
+        command += str(self._simtel_source_path.joinpath("sim_telarray/LightEmission/"))
+        command += f"/{self.le_application}"
+        command += f" -a {self.default_le_config['beam_shape']['default']}:"
+        command += f"{self.default_le_config['beam_width']['default'].value}"
+        command += f" -p {self.default_le_config['pulse_shape']['default']}:"
+        command += f"{self.default_le_config['pulse_width']['default'].value}"
+        command += " -n 1e6,1e7"
+        command += "\n"
+        print(command)
+        return command
+
+    def _make_simtel_script(self, **kwargs):  # pylint: disable=unused-argument
+        """Return the command to run simtel_array."""
+
+        # LightEmission
+        command = str(self._simtel_source_path.joinpath("sim_telarray/bin/sim_telarray/"))
+        command += f" -c {self._telescope_model.get_config_file()}"
+        command += " -DNUM_TELESCOPES=1"
+        command += " -I../cfg/CTA"
+        command += "iobuf_maximum=1000000000"
+        command += super()._config_option(
+            "altitude", self._telescope_model.get_parameter_value("altitude"), weak_option=True
+        )
+        command += super()._config_option("maximum_telescopes", "1", weak_option=True)
+        command += super()._config_option("trigger_telescopes", "1", weak_option=True)
+        command += super()._config_option(
+            "atmospheric_transmission",
+            self._telescope_model.get_parameter_value("atmospheric_transmission"),
+            weak_option=True,
+        )
+        # from light_emission_default config
+        command += super()._config_option(
+            "telescope_theta",
+            self.config.zenith_angle + self.config.off_axis_angle,
+            weak_option=True,
+        )
+        command += super()._config_option("telescope_phi", "0")
+        command += super()._config_option("power_law", "2.68")
+        command += super()._config_option("FADC_BINS", str(int(self.config.fadc_bins)))
+        command += super()._config_option("input_file", f"{self.le_application}.iact.gz")
+        command += super()._config_option("output_file", f"{self.le_application}.simtel.gz\n")
+        command += f"mv {self.le_application}.iact.gz {self.output_dir}\n"
+        command += f"mv {self.le_application}.simtel.gz {self.output_dir}"
+
+        return command
+
+    def _make_plot_script(self, **kwargs):  # pylint: disable=unused-argument
+        command = str(self._simtel_source_path.joinpath("hessioxxx/bin/read_cta_nr"))
+        command += f" -p {self.le_application}.ps {self.le_application}.simtel.gz"
+        return command
+
+    @staticmethod
+    def light_emission_default_configuration():
+        """
+        Get default light emission configuration.
+
+        Returns
+        -------
+        dict
+            Default configuration light emission.
+
+        """
+
+        return {
+            "zenith_angle": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 20.0 * u.deg,
+                "names": ["zenith", "theta"],
+            },
+            "azimuth_angle": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 0.0 * u.deg,
+                "names": ["azimuth", "phi"],
+            },
+            "source_distance": {
+                "len": 1,
+                "unit": u.Unit("m"),
+                "default": 1000 * u.m,
+                "names": ["sourcedist", "srcdist"],
+            },
+            "off_axis_angle": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 0 * u.deg,
+                "names": ["off_axis"],
+            },
+            "fadc_bins": {
+                "len": 1,
+                "unit": u.dimensionless_unscaled,
+                "default": 128,
+                "names": ["fadc_bins"],
+            },
+        }
+
+    def prepare_script(self, test=False, plot_result=False, extra_commands=None):
+        """
+        Builds and returns the full path of the bash run script
+        containing the light-emission command.
+
+        Parameters
+        ----------
+        plot_result: bool
+            If output should be plotted.
+
+        extra_commands: str
+            Additional commands for running simulations given in config.yml.
+
+        Returns
+        -------
+        Path
+            Full path of the run script.
+        """
+        self._logger.debug("Creating run bash script")
+
+        self._script_dir = self._base_directory.joinpath("scripts")
+        self._script_dir.mkdir(parents=True, exist_ok=True)
+        self._script_file = self._script_dir.joinpath(f"{self.le_application}-lightemission")
+        self._logger.debug(f"Run bash script - {self._script_file}")
+
+        self._logger.debug(f"Extra commands to be added to the run script {extra_commands}")
+
+        command_le = self._make_light_emission_script()
+        command_simtel = self._make_simtel_script()
+        command_plot = self._make_plot_script()
+
+        with self._script_file.open("w", encoding="utf-8") as file:
+            file.write("#!/usr/bin/env bash\n\n")
+
+            # if extra_commands is not None:
+            #    file.write("# Writing extras\n")
+            #    for line in extra_commands:
+            #        file.write(f"{line}\n")
+            #    file.write("# End of extras\n\n")
+
+            file.write(f"{command_le}\n\n")
+            file.write(f"{command_simtel}\n\n")
+            if plot_result:
+                file.write(f"{command_plot}\n\n")
+
+            #  TODO: Add functionality to run several telescope configs at once
+
+        if test:
+            #  TODO: Add
+            pass
+
+        os.system(f"chmod ug+x {self._script_file}")
+        return self._script_file
