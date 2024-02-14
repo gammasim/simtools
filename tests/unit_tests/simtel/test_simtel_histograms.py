4418348abc3d6caf6a914cd15cc3d55db74ad362
Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>
2024-02-14 13:08:45 +0100

    increase coverage and fix unit tests
    Signed-off-by: Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>

--- tests/unit_tests/simtel/test_simtel_histograms.py
+++ tests/unit_tests/simtel/test_simtel_histograms.py
@@ -137,7 +137,7 @@ def test_export_histograms(simtel_array_histograms_instance, io_handler):
 
     assert file_with_path.exists()
 
-    # Read hdf5 file
+    # Read simtel file
     list_of_tables = read_hdf5(file_with_path)
     assert len(list_of_tables) == 10
     for table in list_of_tables:
