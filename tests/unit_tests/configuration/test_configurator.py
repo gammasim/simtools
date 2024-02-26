52e4ea6b6e671b9e3f44b2aba3aaa93fa5fd3793
Gernot Maier <gernot.maier@desy.de>
2024-02-26 14:08:54 +0100

    pre-commit failure

--- tests/unit_tests/configuration/test_configurator.py
+++ tests/unit_tests/configuration/test_configurator.py
@@ -209,9 +209,9 @@ def test_get_db_parameters_from_env(configurator, args_dict):
     args_dict["db_api_port"] = 42
     args_dict["db_server"] = "abc@def.de"
     args_dict["db_api_authentication_database"] = "admin"
-    args_dict[
-        "db_simulation_model_url"
-    ] = "https://raw.githubusercontent.com/gammasim/simulation_model/main"
+    args_dict["db_simulation_model_url"] = (
+        "https://raw.githubusercontent.com/gammasim/simulation_model/main"
+    )
 
     assert configurator.config == args_dict
 
