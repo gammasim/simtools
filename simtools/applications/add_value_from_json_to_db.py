57131ffe3e39089f2a3665980daa50d3a70d683c
Gernot Maier <gernot.maier@desy.de>
2024-03-29 16:51:58 +0100

    allow to add all files from a path

--- simtools/applications/add_value_from_json_to_db.py
+++ simtools/applications/add_value_from_json_to_db.py
@@ -30,6 +30,7 @@
 """
 
 import logging
+from pathlib import Path
 
 import simtools.utils.general as gen
 from simtools.configuration import configurator
@@ -39,7 +40,13 @@ from simtools.db import db_handler
 def main():
     _db_tmp = db_handler.DatabaseHandler(mongo_db_config=None)
     config = configurator.Configurator(description="Add a new parameter to the DB.")
-    config.parser.add_argument("--file_name", help="file to be added", required=True)
+    group = config.parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--file_name", help="file to be added", type=str)
+    group.add_argument(
+        "--input_path",
+        help="A directory with json files to upload to the DB.",
+        type=Path,
+    )
     config.parser.add_argument(
         "--db_collection", help="DB collection to which to add new values.", required=True
     )
@@ -63,29 +70,47 @@ def main():
 
     db = db_handler.DatabaseHandler(mongo_db_config=db_config)
 
-    par_dict = gen.collect_data_from_file_or_dict(file_name=args_dict["file_name"], in_dict=None)
+    files_to_insert = []
+    if args_dict.get("file_name", None) is not None:
+        files_to_insert.append(args_dict["file_name"])
+    else:
+        files_to_insert.extend(Path(args_dict["input_path"]).glob("*json"))
 
-    logger.info(f"Adding the following parameter to the DB: {par_dict['parameter']}")
+    if len(files_to_insert) < 1:
+        raise ValueError("No files were provided to upload")
+    plural = "s" if len(files_to_insert) > 1 else ""
 
-    print(f"Should {par_dict} be inserted to the {args_dict['db_collection']} collection?:\n")
+    print(
+        f"Should the following parameter{plural} be inserted to the "
+        f"{args_dict['db_collection']} DB collection?:\n"
+    )
+    print(*files_to_insert, sep="\n")
+    print()
+
     if gen.user_confirm():
-        db.add_new_parameter(
-            db_name=db.DB_CTA_SIMULATION_MODEL,
-            telescope=par_dict["instrument"],
-            parameter=par_dict["parameter"],
-            version=par_dict["version"],
-            value=par_dict["value"],
-            site=par_dict["site"],
-            type=par_dict["type"],
-            collection_name=args_dict["db_collection"],
-            applicable=par_dict["applicable"],
-            file=par_dict["file"],
-            unit=par_dict.get("unit", None),
-            file_prefix="./",
-        )
-        logger.info(
-            f"Value for {par_dict['parameter']} added to {args_dict['db_collection']} collection"
-        )
+        for file_to_insert_now in files_to_insert:
+            par_dict = gen.collect_data_from_file_or_dict(
+                file_name=file_to_insert_now, in_dict=None
+            )
+            logger.info(f"Adding the following parameter to the DB: {par_dict['parameter']}")
+            db.add_new_parameter(
+                db_name=db.DB_CTA_SIMULATION_MODEL,
+                telescope=par_dict["instrument"],
+                parameter=par_dict["parameter"],
+                version=par_dict["version"],
+                value=par_dict["value"],
+                site=par_dict["site"],
+                type=par_dict["type"],
+                collection_name=args_dict["db_collection"],
+                applicable=par_dict["applicable"],
+                file=par_dict["file"],
+                unit=par_dict.get("unit", None),
+                file_prefix="./",
+            )
+            logger.info(
+                f"Value for {par_dict['parameter']} added to "
+                f"{args_dict['db_collection']} collection."
+            )
     else:
         logger.info("Aborted, no change applied to the database")
 
