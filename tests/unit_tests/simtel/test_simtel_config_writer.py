9d8157981bae7556bdfc8d47e7006ab0645ca309
Gernot Maier <gernot.maier@desy.de>
2024-04-25 15:53:38 +0200

    typo

--- tests/unit_tests/simtel/test_simtel_config_writer.py
+++ tests/unit_tests/simtel/test_simtel_config_writer.py
@@ -48,8 +48,15 @@ def test_write_tel_config_file(simtel_config_writer, io_handler, file_has_text):
     file = io_handler.get_output_file(
         file_name="simtel-config-writer_telescope.txt", dir_type="test"
     )
-    simtel_config_writer.write_telescope_config_file(config_file_path=file, parameters={"par": 1})
-    assert file_has_text(file, "par = 1")
+    simtel_config_writer.write_telescope_config_file(
+        config_file_path=file, parameters={"num_gains": 1}
+    )
+    assert file_has_text(file, "num_gains = 1")
+    # not a valid parameter name
+    with pytest.raises(KeyError):
+        simtel_config_writer.write_telescope_config_file(
+            config_file_path=file, parameters={"par": 1}
+        )
 
 
 def test_add_simtel_metadata(simtel_config_writer):
