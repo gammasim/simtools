ab673973c5416a692679bfcd99ffa30734a1177c
Gernot Maier <gernot.maier@desy.de>
2024-06-10 16:23:51 +0200

    fix dumping script

--- database_scripts/dump_remote_db.sh
+++ database_scripts/dump_remote_db.sh
@@ -11,11 +11,36 @@ if [ -f ../.env ]; then
     done < ../.env
 fi
 
-echo "Dumping simulation model database $SIMTOOLS_DB_SIMULATION_MODEL"
+
+# Clean up environment variables
+clean_var() {
+    echo "$1" | sed "s/[ ']*//g"
+}
+
+SIMTOOLS_DB_SERVER=$(clean_var "$SIMTOOLS_DB_SERVER")
+SIMTOOLS_DB_API_PORT=$(clean_var "$SIMTOOLS_DB_API_PORT")
+SIMTOOLS_DB_API_AUTHENTICATION_DATABASE=$(clean_var "$SIMTOOLS_DB_API_AUTHENTICATION_DATABASE")
+SIMTOOLS_DB_API_USER=$(clean_var "$SIMTOOLS_DB_API_USER")
+SIMTOOLS_DB_API_PW=$(clean_var "$SIMTOOLS_DB_API_PW")
+
+[[ "$1" ]] && DB_TO_DUMP="$1" || DB_TO_DUMP="$SIMTOOLS_DB_SIMULATION_MODEL"
+
+# Debugging information
+echo "Server: $SIMTOOLS_DB_SERVER"
+echo "Port: $SIMTOOLS_DB_API_PORT"
+echo "Auth DB: $SIMTOOLS_DB_API_AUTHENTICATION_DATABASE"
+echo "User: $SIMTOOLS_DB_API_USER"
+echo "Database to dump: $DB_TO_DUMP"
+
 mkdir -p dump
 
 mongodump --uri="mongodb://${SIMTOOLS_DB_SERVER}:${SIMTOOLS_DB_API_PORT}" \
  --ssl --tlsInsecure \
- --username="$SIMTOOLS_DB_USER" --password="$SIMTOOLS_DB_API_PW" \
+ --username="$SIMTOOLS_DB_API_USER" --password="$SIMTOOLS_DB_API_PW" \
  --authenticationDatabase="$SIMTOOLS_DB_API_AUTHENTICATION_DATABASE" \
- --db="$DB_TO_COPY" --out="./dump/"
+ --db="$DB_TO_DUMP" --out="./dump/"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to connect to MongoDB server. Please check your connection settings and try again."
+    exit 1
+fi
--- database_scripts/upload_dump_to_local_db.sh
+++ database_scripts/upload_dump_to_local_db.sh
@@ -6,6 +6,7 @@
 SIMTOOLS_NETWORK="simtools-mongo-network"
 CONTAINER_NAME="simtools-mongodb"
 SIMTOOLS_DB_SIMULATION_MODEL='Staging-CTA-Simulation-Model-v0-3-0'
+SIMTOOLS_DB_SIMULATION_MODEL='Staging-CTA-Simulation-Model-Derived-Values'
 
 # Check if podman is available, if not use docker
 if command -v podman &> /dev/null; then
