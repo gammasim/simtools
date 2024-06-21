b7207bf3a15aab4427a8a1fe623b2c4a1b66ede0
Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>
2024-06-21 17:13:40 +0200

    include run_number
    Signed-off-by: Victor Barbosa Martins <victorbarbosamartins@yahoo.com.br>

--- simtools/applications/simulate_showers_for_trigger_rates.py
+++ simtools/applications/simulate_showers_for_trigger_rates.py
@@ -180,7 +180,7 @@ def main():
             "data_directory": Path(args_dict["data_directory"]) / label,
             "site": args_dict["site"],
             "layout_name": args_dict["array_layout_name"],
-            "run_range": [args_dict["run_number"], args_dict["nruns"]],
+            "run_range": [args_dict["run_number"], args_dict["run_number"] + args_dict["nruns"]],
             "nshow": args_dict["nevents"],
             "primary": args_dict["primary"],
             "zenith": args_dict["zenith"] * u.deg,
