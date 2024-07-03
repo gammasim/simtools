30abf5e2f5ab5028482dd587b2141eb6f0a6cf6c
Gernot Maier <gernot.maier@desy.de>
2024-07-03 16:40:24 +0200

    sonar issues

--- tests/unit_tests/job_execution/test_job_manager.py
+++ tests/unit_tests/job_execution/test_job_manager.py
@@ -20,6 +20,37 @@ def job_submitter():
     return submitter
 
 
+@pytest.fixture()
+def output_log():
+    """Fixture for the output log file."""
+    return Path("output.log")
+
+
+@pytest.fixture()
+def logfile_log():
+    """Fixture for the general log file."""
+    return Path("logfile.log")
+
+
+@pytest.fixture()
+def script_file():
+    """Fixture for the script file."""
+    return Path("script.sh")
+
+
+@pytest.fixture()
+def job_messages(script_file):
+    """Fixture for the script message."""
+    return {
+        "script_message": f"Submitting script {script_file}",
+        "job_output": "Job output stream output.out",
+        "job_error_stream": "Job error stream output.err",
+        "job_log_stream": "Job log stream output.job",
+        "running_locally": "Running script locally",
+        "log_excerpt": "log excerpt",
+    }
+
+
 def test_test_submission_system():
     jm.JobManager(submit_engine=None)
     jm.JobManager(submit_engine="local")
@@ -48,44 +79,46 @@ def test_check_submission_system(mock_program_is_executable, job_submitter):
 
 
 @patch("simtools.utils.general")
-def test_submit_local(mock_gen, job_submitter, mocker):
+def test_submit_local(
+    mock_gen, job_submitter, mocker, output_log, logfile_log, script_file, job_messages
+):
     mocker.patch("os.system", return_value=0)
     mock_gen.get_log_excerpt.return_value = "log excerpt"
     mocker.patch("pathlib.Path.exists", return_value=False)
 
-    job_submitter.submit("script.sh", "output.log", "logfile.log")
+    job_submitter.submit(script_file, output_log, logfile_log)
 
-    job_submitter._logger.info.assert_any_call("Submitting script script.sh")
-    job_submitter._logger.info.assert_any_call("Job output stream output.out")
-    job_submitter._logger.info.assert_any_call("Job error stream output.err")
-    job_submitter._logger.info.assert_any_call("Job log stream output.job")
-    job_submitter._logger.info.assert_any_call("Running script locally")
+    job_submitter._logger.info.assert_any_call(job_messages["script_message"])
+    job_submitter._logger.info.assert_any_call(job_messages["job_output"])
+    job_submitter._logger.info.assert_any_call(job_messages["job_error_stream"])
+    job_submitter._logger.info.assert_any_call(job_messages["job_log_stream"])
+    job_submitter._logger.info.assert_any_call(job_messages["running_locally"])
     job_submitter._logger.info.assert_any_call("Testing (local)")
 
 
 @patch("simtools.utils.general")
-def test_submit_htcondor(mock_gen, job_submitter, mocker):
+def test_submit_htcondor(mock_gen, job_submitter, mocker, output_log, logfile_log, script_file):
     job_submitter.submit_engine = "htcondor"
     mock_file = mocker.mock_open()
     mocker.patch("builtins.open", mock_file)
     mock_execute = mocker.patch.object(job_submitter, "_execute")
 
-    job_submitter.submit("script.sh", "output.log", "logfile.log")
+    job_submitter.submit(script_file, output_log, logfile_log)
 
     mock_execute.assert_called_with(
-        "htcondor", job_submitter.engines["htcondor"] + " script.sh.condor"
+        "htcondor", job_submitter.engines["htcondor"] + f" {script_file}.condor"
     )
     mock_file().write.assert_has_calls(
-        [call("Executable = script.sh\n"), call("Output = output.out\n")],
+        [call(f"Executable = {script_file}\n"), call("Output = output.out\n")],
         any_order=True,
     )
 
     # extra submit options
     job_submitter.submit_options = "max_materialize = 800, priority = 5"
-    job_submitter.submit("script.sh", "output.log", "logfile.log")
+    job_submitter.submit(script_file, output_log, logfile_log)
     mock_file().write.assert_has_calls(
         [
-            call("Executable = script.sh\n"),
+            call(f"Executable = {script_file}\n"),
             call("Output = output.out\n"),
             call("max_materialize = 800\n"),
             call("priority = 5\n"),
@@ -102,11 +135,11 @@ def mock_open_side_effect(*args, **kwargs):
 
 
 @patch("builtins.open", side_effect=mock_open_side_effect)
-def test_submit_htcondor_no_script(mock_gen, job_submitter, mocker):
+def test_submit_htcondor_no_script(mock_gen, job_submitter, mocker, output_log, logfile_log):
     job_submitter.submit_engine = "htcondor"
 
     with pytest.raises(JobExecutionError):
-        job_submitter.submit("invalid_path/non_existent_script.sh", "output.log", "logfile.log")
+        job_submitter.submit("invalid_path/non_existent_script.sh", output_log, logfile_log)
 
     job_submitter._logger.error.assert_any_call(
         "Failed creating condor submission file invalid_path/non_existent_script.sh.condor"
@@ -114,11 +147,11 @@ def test_submit_htcondor_no_script(mock_gen, job_submitter, mocker):
 
 
 @patch("simtools.utils.general")
-def test_submit_gridengine(mock_gen, job_submitter, mocker):
+def test_submit_gridengine(mock_gen, job_submitter, mocker, output_log, logfile_log, script_file):
     job_submitter.submit_engine = "gridengine"
     mock_execute = mocker.patch.object(job_submitter, "_execute")
 
-    job_submitter.submit("script.sh", "output.log", "logfile.log")
+    job_submitter.submit(script_file, output_log, logfile_log)
 
     expected_command = (
         f"{job_submitter.engines['gridengine']} -o output.out -e output.err script.sh"
@@ -151,40 +184,43 @@ def job_submitter_real():
 
 
 @patch("simtools.utils.general")
-def test_submit_local_real(mock_gen, job_submitter_real, mocker):
+def test_submit_local_real(
+    mock_gen, job_submitter_real, mocker, output_log, logfile_log, script_file, job_messages
+):
     mock_system = mocker.patch("os.system", return_value=0)
-    mock_gen.get_log_excerpt.return_value = "log excerpt"
+    mock_gen.get_log_excerpt.return_value = job_messages["log_excerpt"]
     mocker.patch("pathlib.Path.exists", return_value=False)
 
-    job_submitter_real.submit("script.sh", "output.log", "logfile.log")
+    job_submitter_real.submit(script_file, output_log, logfile_log)
 
-    job_submitter_real._logger.info.assert_any_call("Submitting script script.sh")
-    job_submitter_real._logger.info.assert_any_call("Job output stream output.out")
-    job_submitter_real._logger.info.assert_any_call("Job error stream output.err")
-    job_submitter_real._logger.info.assert_any_call("Job log stream output.job")
-    job_submitter_real._logger.info.assert_any_call("Running script locally")
-    mock_system.assert_called_with("script.sh > output.out 2> output.err")
+    job_submitter_real._logger.info.assert_any_call(job_messages["script_message"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_output"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_error_stream"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_log_stream"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["running_locally"])
+    mock_system.assert_called_with(f"{script_file} > output.out 2> output.err")
 
 
 @patch("simtools.utils.general")
-def test_submit_local_real_failure(mock_gen, job_submitter_real, mocker):
-    mock_system = mocker.patch("os.system", return_value=1)  # Simulate command failure
-    mock_gen.get_log_excerpt.return_value = "log excerpt"
+def test_submit_local_real_failure(
+    mock_gen, job_submitter_real, mocker, output_log, logfile_log, script_file, job_messages
+):
+    mock_system = mocker.patch("os.system", return_value=1)
+    mock_gen.get_log_excerpt.return_value = job_messages["log_excerpt"]
     mocker.patch("pathlib.Path.exists", return_value=True)
-    mock_gen.get_file_age.return_value = 4  # Simulate recent log file
+    mock_gen.get_file_age.return_value = 4
 
     mocker.patch("simtools.utils.general.get_log_excerpt", return_value="log excerpt")
     mocker.patch("simtools.utils.general.get_file_age", return_value=4)
 
-    # Mocking the file reading process to avoid FileNotFoundError
     with patch("builtins.open", mock_open(read_data="")):
         with pytest.raises(JobExecutionError):
-            job_submitter_real.submit("script.sh", Path("output.log"), Path("logfile.log"))
+            job_submitter_real.submit(script_file, output_log, logfile_log)
 
-    job_submitter_real._logger.info.assert_any_call("Submitting script script.sh")
-    job_submitter_real._logger.info.assert_any_call("Job output stream output.out")
-    job_submitter_real._logger.info.assert_any_call("Job error stream output.err")
-    job_submitter_real._logger.info.assert_any_call("Job log stream output.job")
-    job_submitter_real._logger.info.assert_any_call("Running script locally")
-    mock_system.assert_called_with("script.sh > output.out 2> output.err")
-    job_submitter_real._logger.error.assert_any_call("log excerpt")
+    job_submitter_real._logger.info.assert_any_call(job_messages["script_message"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_output"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_error_stream"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["job_log_stream"])
+    job_submitter_real._logger.info.assert_any_call(job_messages["running_locally"])
+    mock_system.assert_called_with(f"{script_file} > output.out 2> output.err")
+    job_submitter_real._logger.error.assert_any_call(job_messages["log_excerpt"])
