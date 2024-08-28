c210f56baaa7fb377a064bc458e6f5732e866f36
Gernot Maier <gernot.maier@desy.de>
2024-08-28 15:11:34 +0200

    simplify and generalize database upload

--- database_scripts/upload_from_model_repository_to_local_db.sh
+++ database_scripts/upload_from_model_repository_to_local_db.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
-# Upload model parameter from repository a local mongoDB.
+# Upload model parameter from repository to a local or remote mongoDB.
 #
-# Assumes that podman or docker is installed and running.
-# Requires configuration of local DB in .env file
 
-CONTAINER_NAME="simtools-mongodb"
-SIMTOOLS_DB_SIMULATION_MODEL='CTAO-Simulation-Model-v0-3-0'  # Name of the database to be created
 SIMTOOLS_DB_SIMULATION_MODEL_URL="https://gitlab.cta-observatory.org/cta-science/simulations/simulation-model/model_parameters.git"
 SIMTOOLS_DB_SIMULATION_MODEL_BRANCH="main"
 
-# Check if podman is available, if not use docker
-if command -v podman &> /dev/null; then
-    CMD=podman
-elif command -v docker &> /dev/null; then
-    CMD=docker
-else
-    echo "Error: Neither podman nor docker is available."
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <DB simulation model name>"
+    echo ""
+    echo "Uses the .env file in the simtools directory for database configuration."
     exit 1
 fi
 
-echo "Create user for DB $SIMTOOLS_DB_SIMULATION_MODEL"
-$CMD exec -it $CONTAINER_NAME mongosh admin -u root -p example --eval "
-db.createUser({
-  user: 'api',
-  pwd: 'password',
-  roles: [
-    { role: 'readWriteAnyDatabase', db: 'admin' },
-    { role: 'dbAdminAnyDatabase', db: 'admin' },
-    { role: 'userAdminAnyDatabase', db: 'admin' },
-    { role: 'readWrite', db: '$SIMTOOLS_DB_SIMULATION_MODEL' }
-  ]
-});
-"
+SIMTOOLS_DB_SIMULATION_MODEL="$1"
 
 echo "Cloning model parameters from $SIMTOOLS_DB_SIMULATION_MODEL_URL"
 rm -rf ./tmp_model_parameters
@@ -39,17 +20,7 @@ git clone --depth=1 -b $SIMTOOLS_DB_SIMULATION_MODEL_BRANCH $SIMTOOLS_DB_SIMULAT
 
 CURRENTDIR=$(pwd)
 cd ./tmp_model_parameters/ || exit
-
-# setup environment
-filename=".env"
-cat << EOF > "$filename"
-SIMTOOLS_DB_API_PORT=27017 #Port on the MongoDB server
-SIMTOOLS_DB_SERVER='localhost'
-SIMTOOLS_DB_API_USER='api' # username for MongoDB
-SIMTOOLS_DB_API_PW='password' # Password for MongoDB
-SIMTOOLS_DB_API_AUTHENTICATION_DATABASE='admin'
-SIMTOOLS_DB_SIMULATION_MODEL='$SIMTOOLS_DB_SIMULATION_MODEL'
-EOF
+cp -f "$CURRENTDIR"/../.env .env
 
 # upload files to DB
 model_directory="./model_versions/"
