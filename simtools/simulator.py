00391ea100fbce3429cb66d77a11b37de71af60e
Orel Gueta <orel.gueta@desy.de>
2024-09-04 17:07:58 +0200

    Change name of option to fit only corsika and add a sim_telarray seeds option

--- simtools/simulator.py
+++ simtools/simulator.py
@@ -233,7 +233,9 @@ class Simulator:
         }
 
         if runner_class is not SimulatorArray:
-            runner_args["keep_seeds"] = self.args_dict.get("predefined_seeds", False)
+            runner_args["keep_seeds"] = self.args_dict.get("corsika_test_seeds", False)
+        if runner_class is not CorsikaRunner:
+            runner_args["sim_telarray_seeds"] = self.args_dict.get("sim_telarray_seeds")
 
         return runner_class(**runner_args)
 
