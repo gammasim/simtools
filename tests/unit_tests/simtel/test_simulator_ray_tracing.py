86b51d6772f572896c13629cdffe43727f324681
Orel Gueta <orel.gueta@desy.de>
2024-09-23 17:30:12 +0200

    Improve test coverage and reduce lines of code

--- tests/unit_tests/simtel/test_simulator_ray_tracing.py
+++ tests/unit_tests/simtel/test_simulator_ray_tracing.py
@@ -48,6 +48,44 @@ def simulator_ray_tracing(ray_tracing_sst, telescope_model_sst, simtel_path):
     )
 
 
+@pytest.fixture
+def simulator_ray_tracing_single_mirror(ray_tracing_sst, telescope_model_sst, simtel_path):
+    return SimulatorRayTracing(
+        simtel_path=simtel_path,
+        telescope_model=telescope_model_sst,
+        config_data={
+            "zenith_angle": ray_tracing_sst.config.zenith_angle * u.deg,
+            "source_distance": ray_tracing_sst._source_distance * u.km,
+            "off_axis_angle": 0 * u.deg,
+            "mirror_numbers": 0,
+            "use_random_focal_length": ray_tracing_sst.config.use_random_focal_length,
+            "single_mirror_mode": True,
+        },
+        label="test-simtel-runner-ray-tracing",
+    )
+
+
+@pytest.fixture
+def single_pixel_camera_file_content():
+    return [
+        "# Single pixel camera",
+        'PixType 1   0  0 300   1 300 0.00   "funnel_perfect.dat"',
+        "Pixel 0 1 0. 0.  0  0  0 0x00 1",
+        "Trigger 1 of 0",
+    ]
+
+
+@pytest.fixture
+def funnel_perfect_file_content():
+    return [
+        "# Perfect light collection where the angular efficiency of funnels is needed",
+        "0    1.0",
+        "30   1.0",
+        "60   1.0",
+        "90   1.0",
+    ]
+
+
 def test_load_required_files(simulator_ray_tracing):
     simulator_ray_tracing._load_required_files(force_simulate=False)
 
@@ -57,6 +95,55 @@ def test_load_required_files(simulator_ray_tracing):
     assert not simulator_ray_tracing._corsika_file.exists()
     assert simulator_ray_tracing._photons_file.exists()
     assert simulator_ray_tracing._stars_file.exists()
+    assert not simulator_ray_tracing.telescope_model.config_file_directory.joinpath(
+        "single_pixel_camera.dat"
+    ).exists()
+
+
+def assert_single_pixel_and_perfect_funnel_files(
+    simulator_ray_tracing_to_test, single_pixel_camera_file_content, funnel_perfect_file_content
+):
+    single_pixel_camera_file = (
+        simulator_ray_tracing_to_test.telescope_model.config_file_directory.joinpath(
+            "single_pixel_camera.dat"
+        )
+    )
+    funnel_perfect_file = (
+        simulator_ray_tracing_to_test.telescope_model.config_file_directory.joinpath(
+            "funnel_perfect.dat"
+        )
+    )
+
+    for created_file, content in zip(
+        [single_pixel_camera_file, funnel_perfect_file],
+        [single_pixel_camera_file_content, funnel_perfect_file_content],
+    ):
+        assert created_file.exists()
+        with created_file.open("r") as file:
+            lines = file.readlines()
+            for i_line, line in enumerate(lines):
+                assert line.strip() == content[i_line]
+
+
+def test_load_required_files_single_mirror(
+    simulator_ray_tracing_single_mirror,
+    single_pixel_camera_file_content,
+    funnel_perfect_file_content,
+):
+    simulator_ray_tracing_single_mirror._load_required_files(force_simulate=False)
+
+    # This file is not actually needed and does not exist in simtools.
+    # However, its name is needed too provide the name of a CORSIKA input file to sim_telarray
+    # so here we check the it does not actually exist.
+    assert not simulator_ray_tracing_single_mirror._corsika_file.exists()
+    assert simulator_ray_tracing_single_mirror._photons_file.exists()
+    assert simulator_ray_tracing_single_mirror._stars_file.exists()
+
+    assert_single_pixel_and_perfect_funnel_files(
+        simulator_ray_tracing_single_mirror,
+        single_pixel_camera_file_content,
+        funnel_perfect_file_content,
+    )
 
 
 def test_make_run_command(simulator_ray_tracing, model_version):
@@ -103,29 +190,11 @@ def test_is_photon_list_file_ok(simulator_ray_tracing):
     assert simulator_ray_tracing._is_photon_list_file_ok()
 
 
-def test_write_out_single_pixel_camera_file(simulator_ray_tracing):
+def test_write_out_single_pixel_camera_file(
+    simulator_ray_tracing, single_pixel_camera_file_content, funnel_perfect_file_content
+):
     simulator_ray_tracing._write_out_single_pixel_camera_file()
 
-    single_pixel_camera_file = simulator_ray_tracing.telescope_model.config_file_directory.joinpath(
-        "single_pixel_camera.dat"
+    assert_single_pixel_and_perfect_funnel_files(
+        simulator_ray_tracing, single_pixel_camera_file_content, funnel_perfect_file_content
     )
-    funnel_perfect_file = simulator_ray_tracing.telescope_model.config_file_directory.joinpath(
-        "funnel_perfect.dat"
-    )
-
-    assert single_pixel_camera_file.exists()
-    assert funnel_perfect_file.exists()
-
-    with single_pixel_camera_file.open("r") as file:
-        lines = file.readlines()
-        assert lines[0].strip() == "# Single pixel camera"
-        assert lines[1].strip() == 'PixType 1   0  0 300   1 300 0.00   "funnel_perfect.dat"'
-        assert lines[2].strip() == "Pixel 0 1 0. 0.  0  0  0 0x00 1"
-        assert lines[3].strip() == "Trigger 1 of 0"
-
-    with funnel_perfect_file.open("r") as file:
-        lines = file.readlines()
-        assert lines[1].strip() == "0    1.0"
-        assert lines[2].strip() == "30   1.0"
-        assert lines[3].strip() == "60   1.0"
-        assert lines[4].strip() == "90   1.0"
