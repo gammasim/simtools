fix return values for cumulative calculation

simtools/psf_analysis.py
@@ -419,7 +419,7 @@ class PSFImage:
         (radius, intensity)
         """
         if radius is not None:
-            radius_all = [radius.to(u.cm).value]
+            radius_all = radius.to(u.cm).value if isinstance(radius, u.Quantity) else radius
         else:
             radius_all = list(np.linspace(0, 1.6 * self.get_psf(0.8), 30))
 
@@ -431,8 +431,8 @@ class PSFImage:
             "formats": ("f8", "f8"),
         }
         result = np.recarray((len(radius_all),), dtype=d_type)
-        result.Radius = radius_all
-        result.Intensity = intensity
+        result["Radius [cm]"] = radius_all
+        result["Cumulative PSF"] = intensity
 
         return result
 

tests/unit_tests/test_psf_analysis.py
@@ -47,14 +47,14 @@ def test_get_cumulative_data(psf_image):
 
     cumulative_data = image.get_cumulative_data()
     assert len(cumulative_data) == 30
-    assert cumulative_data.Radius[0] == 0
-    assert cumulative_data.Intensity[0] == 0
+    assert cumulative_data["Radius [cm]"][0] == 0
+    assert cumulative_data["Cumulative PSF"][0] == 0
 
     # test with radius
-    radius = 50.0 * u.cm
+    radius = np.array([50.0] * u.cm)
     cumulative_data_radius = image.get_cumulative_data(radius=radius)
     assert len(cumulative_data_radius) == 1
-    assert pytest.approx(cumulative_data_radius.Radius[0]) == 50.0
+    assert pytest.approx(cumulative_data_radius["Radius [cm]"][0]) == 50.0
 
 
 def test_get_image_data(psf_image):