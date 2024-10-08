e801d6d1df02e7d16fb86b11877880897be224b6
Eshita Joshi <eshita.joshi@desy.de>
2024-10-08 10:42:34 +0200

    Naming convention changed to snake_case and general cleanup

--- simtools/applications/produce_reports.py
+++ simtools/applications/produce_reports.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 import logging
-import numpy as np
 import simtools.utils.general as gen
 from pathlib import Path
-from simtools.reporting.read_parameters import ParamDicts
+from simtools.reporting.read_parameters import ReadParameters
 from simtools.configuration import configurator
 from simtools.io_operations import io_handler
 from simtools.model.telescope_model import TelescopeModel
@@ -15,7 +14,7 @@ def _parse(label):
     config = configurator.Configurator(
             label=label,
             description=(
-                "Generate an html report for all the model parameter at a given version associated with a given telescope at a given site."
+                "Generate an html report for all the model parameters for a given model version associated with a given telescope at a given site."
                 ),
             )
 
@@ -23,19 +22,19 @@ def _parse(label):
 
 
 
-def generate_html_report(args_dict, data):
+def generate_html_report(label, args_dict, data):
     '''
     A function to generate an html file to report the parameter values.
     '''
 
     io_handler_instance = io_handler.IOHandler()
-    output_path = io_handler_instance.get_output_directory(Path(__file__).stem, sub_dir="html-reports")
+    output_path = io_handler_instance.get_output_directory(label)
     output_filename = f'{output_path}/{args_dict["telescope"]}_v_{args_dict["model_version"]}'
 
     # Start writing the HTML file
     with open(output_filename + '.html', 'w') as file:
         # HTML boilerplate
-        file.write("<html><head><title>%s</title>\n" %output_filename)
+        file.write("<html><head><title>%s</title>\n" %args_dict["telescope"])
         file.write("<style>\n")
         file.write("body { font-family: Arial, sans-serif; margin: 40px; }\n")
         file.write("h1 { color: #333; }\n")
@@ -44,7 +43,7 @@ def generate_html_report(args_dict, data):
         file.write("th { background-color: #f2f2f2; }\n")
         file.write("</style>\n")
         file.write("</head><body>\n")
-        file.write("<h1>Parameter report for %s in model version %s<h1>" %(args_dict["telescope"], args_dict["model_version"]))
+        file.write("<h1>Parameter report for %s telescope %s in model version %s<h1>" %(args_dict["site"], args_dict["telescope"], args_dict["model_version"]))
 
         # Write the section header to specify the telescope
         file.write("<h2>%s</h2>\n" %args_dict['telescope'])
@@ -52,11 +51,10 @@ def generate_html_report(args_dict, data):
 
         # Start the table for displaying parameters
         file.write("<table>\n")
-        file.write("<tr><th>Parameter</th><th>Value</th><th>Unit</th><th>Short Description</th></tr>\n")
+        file.write("<tr><th>Parameter Name</th><th>Value</th><th>Unit</th><th>Short Description</th></tr>\n")
 
         # Write each parameter to the table
         for item in data:
-            #print('item: ', item)
             file.write(f"<tr><td>{item[0]}</td><td>{item[1]}</td><td>{item[2]}</td><td>{item[4]}</td></tr>\n")
 
         file.write("</table>\n")
@@ -70,17 +68,13 @@ def generate_html_report(args_dict, data):
 
 def main(): 
     label = Path(__file__).stem
-    print('label: ', label)
     args_dict, db_config = _parse(label)
-    print(args_dict, db_config)
 
     logger = logging.getLogger()
     logger.setLevel(gen.get_log_level_from_user(args_dict["log_level"]))
 
-    _io_handler = io_handler.IOHandler()
-    output_dir = _io_handler.get_output_directory(label, sub_dir="html-reports")
 
-    tel_model = TelescopeModel(
+    telescope_model = TelescopeModel(
             site=args_dict["site"],
             telescope_name=args_dict["telescope"],
             model_version=args_dict["model_version"],
@@ -89,9 +83,9 @@ def main():
             )
 
 
-    data = ParamDicts().get_telescope_param_data(tel_model)
+    data = ReadParameters().get_telescope_parameter_data(telescope_model)
     
-    generate_html_report(args_dict, data) 
+    generate_html_report(label, args_dict, data) 
 
     logger.info(f"HTML report generated: {args_dict['telescope']} for model version {args_dict['model_version']}")
 
--- simtools/reporting/read_parameters.py
+++ simtools/reporting/read_parameters.py
@@ -1,64 +1,56 @@
 #!/usr/bin/python3
 
 import logging
-import numpy as np
 import simtools.utils.names as names
 
 
-class ParamDicts:
+class ReadParameters:
 
     def __init__(
             self
             ):
 
         self._logger = logging.getLogger(__name__)
-        #self.Desc = {}
-        #self.shortDesc = {}
-        #self.telType = {}
 
 
-
-    #@cache
-    def _get_paramDesc(self):
+    def _get_parameter_descriptions(self):
         '''
         A function to return the descriptions and short descriptions
-        of parameters as dictionary values
+        of all parameters as dictionary values
         with the model parameter names as the keys.
         '''
-        all_paramDicts = names.telescope_parameters()
-        paramNames = all_paramDicts.keys()
+        all_parameter_dictionaries = names.telescope_parameters()
+        all_parameter_names = all_parameter_dictionaries.keys()
 
-        paramDesc = {}
-        telType = {}
-        shortDesc = {}
+        parameter_description = {}
+        short_description = {}
 
-        for param in paramNames:
+        for parameter in all_parameter_names:
 
-            paramDesc[param] =f'{all_paramDicts[param].get("description")}'
-            shortDesc[param] = f'{all_paramDicts[param].get("short_description")}'
+            parameter_description[parameter] =f'{all_parameter_dictionaries[parameter].get("description")}'
+            short_description[parameter] = f'{all_parameter_dictionaries[parameter].get("short_description")}'
             
 
-        return paramDesc, shortDesc
+        return parameter_description, short_description
 
 
 
-    def get_telescope_param_data(self, tel_model):
+    def get_telescope_parameter_data(self, telescope_model):
         '''
-        A function to get all the relevant parameters associated with a given telescope.
+        A function to get the name, value, unit, and descriptions
+        of all the relevant parameters associated with a given telescope.
         '''
 
-        #telType = self._get_dicts()[2]
-        #return [key for key, values in telType.items() if telescope in values]
-        paramDicts = tel_model._parameters
-        paramNames = paramDicts.keys()
+        parameter_dictionaries = telescope_model._parameters
+        parameter_names = parameter_dictionaries.keys()
 
         data = []
-        for parameter in paramNames:
-            value = paramDicts[parameter].get('value')
-            unit = paramDicts[parameter].get('unit')
-            desc = self._get_paramDesc()[0].get(parameter)
-            shortDesc = self._get_paramDesc()[1].get(parameter)	   
-            data.append([parameter, value, unit, desc, shortDesc])
+        for parameter in parameter_names:
+            value = parameter_dictionaries[parameter].get('value')
+            unit = parameter_dictionaries[parameter].get('unit')
+            description = self._get_parameter_descriptions()[0].get(parameter)
+            short_description = self._get_parameter_descriptions()[1].get(parameter)	   
+            data.append([parameter, value, unit, description, short_description])
 
 
         return data
