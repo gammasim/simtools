621ad193f58f2ca144357293eb323f1e8c2bf6c1
Gernot Maier <gernot.maier@desy.de>
2024-10-09 10:56:37 +0200

    Address code smell

--- simtools/data_model/format_checkers.py
+++ simtools/data_model/format_checkers.py
@@ -30,7 +30,7 @@ def check_array_element(element):
 @format_checker.checks("array_trigger_name", raises=ValueError)
 def check_array_trigger_name(name):
     """Validate array trigger names for jsonschema."""
-    pattern = r"(.*?)(?=_single_telescope|_array)"
+    pattern = r"(.*)(?=_single_telescope|_array)"
     if not re.match(pattern, name):
         raise ValueError(f"Array trigger name '{name}' does not match pattern '{pattern}'")
     names.validate_array_element_type(re.match(pattern, name).group(1))
