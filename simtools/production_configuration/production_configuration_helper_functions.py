0a5e23d12d9c97f1be39275b62b94684de672c08
tobiaskleiner <tobias.kleiner@desy.de>
2024-11-14 13:54:33 +0100

    helper function

---
+++ simtools/production_configuration/production_configuration_helper_functions.py
@@ -0,0 +1,27 @@
+"""Helper functions for the production configuration tools."""
+
+import os
+
+import simtools.utils.general as gen
+
+
+def load_metrics(file_path: str) -> dict:
+    """
+    Load metrics from a YAML file or dict.
+
+    This function reads the metrics defined in the given YAML file and returns
+    them as a dictionary.
+
+    Parameters
+    ----------
+    file_path : str
+        Path to the metrics YAML file.
+
+    Returns
+    -------
+    dict
+        Dictionary of metrics loaded from the YAML file.
+    """
+    if file_path and os.path.exists(file_path):
+        return gen.collect_data_from_file_or_dict(file_name=file_path, in_dict=True)
+    return {}
