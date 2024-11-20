fix tests for change in binom error calculation

simtools/production_configuration/event_scaler.py
@@ -65,8 +65,8 @@ class EventScaler:
         scaling_factor = self._compute_scaling_factor()
         base_events = self._number_of_simulated_events()
         if return_sum:
-            return np.sum(base_events * scaling_factor) * u.ct
-        return base_events * scaling_factor * u.ct
+            return np.sum(base_events * scaling_factor)
+        return base_events * scaling_factor
 
     def _compute_scaling_factor(self) -> float:
         """

tests/unit_tests/production_configuration/test_calculate_statistical_errors_grid.py
@@ -104,7 +104,7 @@ def test_calculate_scaled_events(test_fits_file, metric):
     scaled_events = event_scaler.scale_events()
 
     assert isinstance(scaled_events, u.Quantity)
-    assert scaled_events.value == pytest.approx(33027, rel=1e-0)
+    assert scaled_events.value == pytest.approx(41249903535849.58, rel=1e-0)
     assert scaled_events.unit == u.ct
 
 
@@ -122,7 +122,7 @@ def test_calculate_metrics(test_fits_file, metric):
 
     evaluator.calculate_metrics()
 
-    expected_values = np.array([3.18110350e-10, 4.65906785e-09, 1.03266065e-08])
+    expected_values = np.array([0.40824829, 0.31622776, 0.1796053])
     computed_values = evaluator.error_eff_area["relative_errors"].value[: len(expected_values)]
     assert computed_values == pytest.approx(expected_values, rel=1e-2)
 
@@ -227,7 +227,7 @@ def test_compute_efficiency_and_errors(test_fits_file, metric):
     )
 
     expected_efficiencies = np.array([0.1, 0.1, 0.1, 0.0]) * u.dimensionless_unscaled
-    expected_relative_errors = np.array([0.03, 0.0212132, 0.04242641, 0.0]) / u.ct**0.5
+    expected_relative_errors = np.array([0.3, 0.21213203, 0.42426407, 0.0])
 
     assert np.allclose(
         efficiencies, expected_efficiencies, atol=1e-2

tests/unit_tests/production_configuration/test_generate_simulation_config.py
@@ -79,7 +79,7 @@ def test_configure_simulation(mock_statistical_error_evaluator):
 
     params = config.configure_simulation()
     assert isinstance(params, dict)
-    assert np.isclose(params.get("number_of_events").value, 880.73915787, atol=1e-2)
+    assert np.isclose(params.get("number_of_events").value, 1099997427622.6555, atol=1e-2)
 
 
 def test_calculate_core_scatter_area(mock_statistical_error_evaluator):
@@ -123,5 +123,5 @@ def test_edge_cases(mock_statistical_error_evaluator, metric):
 
     params = config.configure_simulation()
 
-    expected_number_of_events = 22018
+    expected_number_of_events = 27499935690566.387
     assert np.isclose(params.get("number_of_events").value, expected_number_of_events, atol=1e0)