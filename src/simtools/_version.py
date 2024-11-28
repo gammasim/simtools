f0e6deb55cd40c514754c6d7757ba204b6788f07
Gernot Maier <gernot.maier@desy.de>
2024-11-28 19:03:22 +0100

    src layout

--- src/simtools/_version.py
+++ src/simtools/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.8.1.dev18+g45eaa0f51"
-__version_tuple__ = version_tuple = (0, 8, 1, "dev18", "g45eaa0f51")
+__version__ = version = "0.8.2.dev13+gcbf0450f1.d20241128"
+__version_tuple__ = version_tuple = (0, 8, 2, "dev13", "gcbf0450f1.d20241128")
