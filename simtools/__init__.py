805ab9db6a3f14938a7fd618d707e3d99dd778f9
Gernot Maier <gernot.maier@desy.de>
2024-11-28 18:13:30 +0100

    simtools move to src_layout

--- simtools/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from .version import __version__
-
-logging.basicConfig(format="%(levelname)s::%(module)s(l%(lineno)s)::%(funcName)s::%(message)s")
-logging.getLogger("matplotlib").setLevel(logging.WARNING)
-
-__all__ = ["__version__"]
