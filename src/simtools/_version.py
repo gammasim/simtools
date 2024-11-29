d55ddf648896d919364add6527e0f2fdcaf3bfde
Gernot Maier <gernot.maier@desy.de>
2024-11-29 19:37:25 +0100

    sync versioning with ctapip-style writing

--- src/simtools/_version.py
+++ src/simtools/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.8.2.dev13+gcbf0450f1.d20241128"
-__version_tuple__ = version_tuple = (0, 8, 2, "dev13", "gcbf0450f1.d20241128")
+__version__ = version = "0.8.2.dev14+gf0e6deb55.d20241129"
+__version_tuple__ = version_tuple = (0, 8, 2, "dev14", "gf0e6deb55.d20241129")
