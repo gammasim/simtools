ef753a968c6aa31f3d28ee834127122acc99e5d4
Eshita Joshi <eshita.joshi@desy.de>
2024-12-03 11:35:46 +0100

    unit test updated

--- tests/unit_tests/reporting/test_read_parameters.py
+++ tests/unit_tests/reporting/test_read_parameters.py
@@ -8,14 +8,11 @@ def test_get_all_parameter_descriptions(telescope_model_lst):
     read_parameters = ReadParameters(telescope_model=telescope_model_lst)
 
     # Call get_all_parameter_descriptions
-    descriptions, short_descriptions = read_parameters.get_all_parameter_descriptions()
+    descriptions, short_descriptions, inst_class = read_parameters.get_all_parameter_descriptions()
 
     assert isinstance(descriptions.get("focal_length"), str)
     assert isinstance(short_descriptions.get("focal_length"), str)
-    assert (
-        short_descriptions.get("focal_length")
-        == "Nominal overall focal length of the entire telescope."
-    )
+    assert isinstance(inst_class.get("focal_length"), str)
 
 
 def test_get_telescope_parameter_data(telescope_model_lst):
@@ -25,18 +22,7 @@ def test_get_telescope_parameter_data(telescope_model_lst):
     result = read_parameters.get_telescope_parameter_data()
 
     # Assert the result contains the expected data
-    if result[0] == "focal_length":
-        assert result[1] == (2800.0 * u.cm)
-        assert (
-            result[2]
-            == "Nominal overall focal length of the entire telescope. This defines the \
-                image scale near the centre of the field of view. For segmented primary \
-                focus telescopes this determines the alignment of the segments and the \
-                separation from the reflector, at its centre to the camera. The \
-                alignment focus is on the surface determined by the par:focus-offset \
-                (see parameter description for details).  For secondary mirror \
-                configurations this value is not actually used in the optics simulation \
-                but only reported as a nominal value, typically close to the effective \
-                focal length."
-        )
+    if result[1] == "focal_length":
+        assert result[0] == "Structure"
+        assert result[2] == (2800.0 * u.cm)
         assert result[3] == "Nominal overall focal length of the entire telescope."
