932b9aba89490af4e40548e9abae7d4b29faa538
Gernot Maier <gernot.maier@desy.de>
2024-12-06 15:34:34 +0100

    code smell

--- tests/unit_tests/io_operations/test_legacy_data_handler.py
+++ tests/unit_tests/io_operations/test_legacy_data_handler.py
@@ -6,26 +6,20 @@ from astropy.table import Table
 from simtools.io_operations import legacy_data_handler
 
 
-def test_read_legacy_data_file():
+@pytest.fixture
+def test_spe_file():
+    return "tests/resources/SinglePhe_spectrum_totalfit_19pixel-average_20200601.csv"
 
-    table = legacy_data_handler.read_legacy_data_as_table(
-        "tests/resources/SinglePhe_spectrum_totalfit_19pixel-average_20200601.csv",
-        "legacy_lst_single_pe",
-    )
+
+def test_read_legacy_data_file(test_spe_file):
+
+    table = legacy_data_handler.read_legacy_data_as_table(test_spe_file, "legacy_lst_single_pe")
     assert table.colnames == ["amplitude", "response"]
 
     with pytest.raises(ValueError, match="Unsupported legacy data file type: not_a_file_type"):
-        legacy_data_handler.read_legacy_data_as_table(
-            "tests/resources/SinglePhe_spectrum_totalfit_19pixel-average_20200601.csv",
-            "not_a_file_type",
-        )
+        legacy_data_handler.read_legacy_data_as_table(test_spe_file, "not_a_file_type")
 
 
-def test_read_legacy_lst_single_pe():
+def test_read_legacy_lst_single_pe(test_spe_file):
 
-    assert isinstance(
-        legacy_data_handler.read_legacy_lst_single_pe(
-            "tests/resources/SinglePhe_spectrum_totalfit_19pixel-average_20200601.csv",
-        ),
-        Table,
-    )
+    assert isinstance(legacy_data_handler.read_legacy_lst_single_pe(test_spe_file), Table)
