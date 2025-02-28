ac8ab8ea8509850ecf56e3ba3ceb02a040bc1b84
tobiaskleiner <tobias.kleiner@desy.de>
2025-02-28 11:31:29 +0100

    update tests

--- tests/unit_tests/production_configuration/test_limits_calculation.py
+++ tests/unit_tests/production_configuration/test_limits_calculation.py
@@ -1,51 +1,28 @@
 import astropy.units as u
+import h5py
+import numpy as np
 import pytest
-from astropy.table import Table
 
 from simtools.production_configuration.limits_calculation import LimitCalculator
 
 
 @pytest.fixture
-def hdf5_file():
-    angle_table = Table(
-        data={"angle": [0.1, 0.2, 0.3] * u.deg},
-        meta={
-            "Title": "angle_to_observing_position__triggered_showers_",
-            "x_bin_edges": [0, 0.1, 0.2, 0.3, 0.4],
-            "x_bin_edges_units": "deg",
-        },
-    )
-    event_table = Table(
-        data={"weight": [1, 2, 3]},
-        meta={
-            "Title": "event_weight__ra3d__log10_e__",
-            "x_bin_edges": [0, 1, 2, 3, 4],
-            "y_bin_edges": [0, 1, 2, 3, 4],
-            "x_bin_edges_units": "TeV",
-            "y_bin_edges_units": "m",
-        },
-    )
-    return [angle_table, event_table]
-
-
-@pytest.fixture
-def hdf5_file_no_units():
-    angle_table = Table(
-        data={"angle": [0.1, 0.2, 0.3]},
-        meta={
-            "Title": "angle_to_observing_position__triggered_showers_",
-            "x_bin_edges": [0, 0.1, 0.2, 0.3, 0.4],
-        },
-    )
-    event_table = Table(
-        data={"weight": [1, 2, 3]},
-        meta={
-            "Title": "event_weight__ra3d__log10_e__",
-            "x_bin_edges": [0, 1, 2, 3, 4],
-            "y_bin_edges": [0, 1, 2, 3, 4],
-        },
-    )
-    return [angle_table, event_table]
+def hdf5_file(tmp_path):
+    file_path = tmp_path / "test_data.h5"
+    with h5py.File(file_path, "w") as f:
+        grp = f.create_group("data")
+        grp.create_dataset("core_x", data=np.array([0.1, 0.2, 0.3, 0.1, 0.2, 0.3]))
+        grp.create_dataset("core_y", data=np.array([0.1, 0.2, 0.3, 0.1, 0.2, 0.3]))
+        grp.create_dataset("simulated", data=np.array([1.0, 2.0, 3.0, 1.0, 2.0, 3.0]))
+        grp.create_dataset("shower_id_triggered", data=np.array([0, 1, 2, 0, 1, 2]))
+        grp.create_dataset(
+            "file_names", data=np.array(["file1", "file2"], dtype=h5py.string_dtype())
+        )
+        grp.create_dataset("shower_sim_azimuth", data=np.array([0.1, 0.5, 1.0, 1.5, 2.0, 2.5]))
+        grp.create_dataset("shower_sim_altitude", data=np.array([0.1, 0.4, 0.8, 1.2, 1.6, 2.0]))
+        grp.create_dataset("array_azimuth", data=np.array([0.1, 0.5, 1.0, 1.5, 2.0, 2.5]))
+        grp.create_dataset("array_altitude", data=np.array([0.2, 0.6, 1.0, 1.4, 1.8, 2.2]))
+    return file_path
 
 
 @pytest.fixture
@@ -53,42 +30,26 @@ def limit_calculator(hdf5_file):
     return LimitCalculator(hdf5_file)
 
 
-@pytest.fixture
-def limit_calculator_no_units(hdf5_file_no_units):
-    return LimitCalculator(hdf5_file_no_units)
-
-
 def test_compute_lower_energy_limit(limit_calculator):
     loss_fraction = 0.5
     lower_energy_limit = limit_calculator.compute_lower_energy_limit(loss_fraction)
-    assert lower_energy_limit == 1 * u.TeV
-
-
-def test_compute_lower_energy_limit_no_units(limit_calculator_no_units):
-    loss_fraction = 0.5
-    lower_energy_limit = limit_calculator_no_units.compute_lower_energy_limit(loss_fraction)
-    assert lower_energy_limit == 1 * u.TeV  # LimitCalculator adds units
+    assert lower_energy_limit.unit == u.TeV
+    assert lower_energy_limit.value > 0
 
 
 def test_compute_upper_radial_distance(limit_calculator):
     loss_fraction = 0.2
     upper_radial_distance = limit_calculator.compute_upper_radial_distance(loss_fraction)
-    assert upper_radial_distance == 2 * u.m
-
-
-def test_compute_upper_radial_distance_no_units(limit_calculator_no_units):
-    loss_fraction = 0.2
-    upper_radial_distance = limit_calculator_no_units.compute_upper_radial_distance(loss_fraction)
-    assert upper_radial_distance == 2 * u.m  # LimitCalculator adds units
+    assert upper_radial_distance.unit == u.m
+    assert upper_radial_distance.value > 0
 
 
 def test_compute_viewcone(limit_calculator):
-    loss_fraction = 0.1
+    loss_fraction = 0.001
     viewcone = limit_calculator.compute_viewcone(loss_fraction)
-    assert viewcone == 0 * u.deg
+    assert viewcone.unit == u.deg
+    assert viewcone.value > 0
 
 
-def test_compute_viewcone_no_units(limit_calculator_no_units):
-    loss_fraction = 0.1
-    viewcone = limit_calculator_no_units.compute_viewcone(loss_fraction)
-    assert viewcone == 0 * u.deg  # LimitCalculator adds units
+def test_plot_data(limit_calculator):
+    limit_calculator.plot_data()
