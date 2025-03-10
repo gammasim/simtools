772f1db9419da0b9ea22aaf2fb9fe73c72c5fa9f
Gernot Maier <gernot.maier@desy.de>
2025-03-10 10:46:58 +0100

    env file

--- database_scripts/upload_from_model_repository_to_db.sh
+++ database_scripts/upload_from_model_repository_to_db.sh
@@ -32,7 +32,7 @@ git clone --depth=1 -b $DB_SIMULATION_MODEL_BRANCH $DB_SIMULATION_MODEL_URL ./tm
 CURRENT_DIR=$(pwd)
 cd ./tmp_model_parameters/ || exit
 if [[ -e "$CURRENT_DIR"/../.env ]]; then
-    cp -f ../.env .env
+    cp -f "$CURRENT_DIR"/../.env .env
 fi
 
 # ask for confirmation before uploading to remote DB
