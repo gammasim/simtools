64da91b7388adb1018ac0fab2062011e2cbe36a4
Gernot Maier <gernot.maier@desy.de>
2025-03-14 13:15:16 +0100

    cleanup

--- tests/unit_tests/model/test_array_model.py
+++ tests/unit_tests/model/test_array_model.py
@@ -13,154 +13,6 @@ from simtools.model.array_model import ArrayModel
 logger = logging.getLogger()
 
 
-@pytest.fixture(autouse=True)
-def patch_models(mocker):
-    site_model_mock = mocker.patch("simtools.model.array_model.SiteModel", autospec=True)
-
-    def site_model_side_effect(site=None, *args, **kwargs):
-        mock_instance = mocker.MagicMock()
-        mock_instance.site = site
-
-        site_data = {
-            "North": {
-                "elements": [
-                    "LSTN-01",
-                    "LSTN-02",
-                    "LSTN-03",
-                    "LSTN-04",
-                    "MSTN-01",
-                    "MSTN-02",
-                    "MSTN-03",
-                    "MSTN-04",
-                    "MSTN-05",
-                ],
-                "easting": 217611.227,
-                "northing": 3185066.278,
-                "altitude": 2177.0,
-                "obs_level": 2156.0,
-                "epsg": 32628,
-            },
-            "South": {
-                "elements": [
-                    "LSTS-01",
-                    "LSTS-02",
-                    "LSTS-03",
-                    "LSTS-04",
-                    "MSTS-01",
-                    "MSTS-02",
-                    "MSTS-03",
-                    "MSTS-04",
-                    "MSTS-05",
-                    "SSTS-01",
-                    "SSTS-02",
-                    "SSTS-03",
-                    "SSTS-04",
-                ],
-                "easting": 366822.017,
-                "northing": 7269466.999,
-                "altitude": 2162.35,
-                "obs_level": 2147.0,
-                "epsg": 32719,
-            },
-        }
-
-        data = site_data.get(site, site_data["North"])
-
-        mock_instance.get_array_elements_for_layout.return_value = data["elements"]
-        mock_instance.get_corsika_site_parameters.return_value = {
-            "corsika_observation_level": data["obs_level"] * u.m
-        }
-        mock_instance.get_reference_point.return_value = {
-            "center_easting": data["easting"] * u.m,
-            "center_northing": data["northing"] * u.m,
-            "center_altitude": data["altitude"] * u.m,
-            "epsg_code": data["epsg"],
-            "array_name": "test_layout",
-        }
-        return mock_instance
-
-    site_model_mock.side_effect = site_model_side_effect
-
-    telescope_model_mock = mocker.patch("simtools.model.array_model.TelescopeModel", autospec=True)
-
-    def telescope_model_side_effect(site=None, telescope_name=None, *args, **kwargs):
-        mock_instance = mocker.MagicMock()
-        mock_instance.site = site
-        mock_instance.name = telescope_name
-        mock_instance.extra_label = ""
-
-        tel_type = telescope_name[:4] if telescope_name else ""
-        tel_num = int(telescope_name[-2:]) if telescope_name and len(telescope_name) >= 3 else 0
-
-        sphere_radii = {
-            "LSTN": 12.5,
-            "LSTS": 12.5,
-            "MSTN": 8.0,
-            "MSTS": 8.0,
-            "SSTN": 4.0,
-            "SSTS": 4.0,
-        }
-
-        def position_side_effect(coordinate_system="ground"):
-            if not telescope_name:
-                return [0.0 * u.m, 0.0 * u.m, 0.0 * u.m]
-
-            if coordinate_system == "ground":
-                coord_map = {
-                    "LST": (tel_num * 50.0, tel_num * 30.0, 2177.0),
-                    "MST": (tel_num * -40.0, tel_num * 20.0, 2177.0),
-                    "SST": (tel_num * 20.0, tel_num * -40.0, 2177.0),
-                }
-                base_type = tel_type[:3]
-                coords = coord_map.get(base_type, (0.0, 0.0, 0.0))
-                return [coords[0] * u.m, coords[1] * u.m, coords[2] * u.m]
-
-            if coordinate_system == "utm":
-                center_easting = 366822.017 if site == "South" else 217611.227
-                center_northing = 7269466.999 if site == "South" else 3185066.278
-                altitude = 2162.35 if site == "South" else 2177.0
-
-                return [
-                    (center_easting + tel_num * 50.0) * u.m,
-                    (center_northing + tel_num * 30.0) * u.m,
-                    altitude * u.m,
-                ]
-
-            return [0.0 * u.m, 0.0 * u.m, 0.0 * u.m]
-
-        mock_instance.position.side_effect = position_side_effect
-
-        def get_parameter_value_with_unit(param_name):
-            if param_name == "telescope_axis_height":
-                return 16.0 * u.m
-            if param_name == "telescope_sphere_radius":
-                if not telescope_name:
-                    return 8.0 * u.m
-                return sphere_radii.get(tel_type, 8.0) * u.m
-            return None
-
-        mock_instance.get_parameter_value_with_unit.side_effect = get_parameter_value_with_unit
-
-        return mock_instance
-
-    telescope_model_mock.side_effect = telescope_model_side_effect
-
-    db_mock = mocker.patch("simtools.model.array_model.db_handler.DatabaseHandler", autospec=True)
-    db_instance = db_mock.return_value
-
-    telescope_lists = {
-        "LSTN": ["LSTN-01", "LSTN-02", "LSTN-03", "LSTN-04"],
-        "MSTN": ["MSTN-" + f"{i:02d}" for i in range(1, 16)],
-        "LSTS": ["LSTS-01", "LSTS-02", "LSTS-03", "LSTS-04"],
-        "MSTS": ["MSTS-" + f"{i:02d}" for i in range(1, 16)],
-        "SSTS": ["SSTS-01", "SSTS-02", "SSTS-03", "SSTS-04"],
-    }
-
-    db_instance.get_array_elements_of_type.side_effect = (
-        lambda array_element_type, *args, **kwargs: telescope_lists.get(array_element_type, [])
-    )
-
-
 @pytest.fixture
 def array_model_from_list(db_config, io_handler, model_version):
     return ArrayModel(
@@ -196,7 +48,7 @@ def test_array_model_init_without_layout_or_telescope_list(db_config, io_handler
 def test_input_validation(array_model):
     am = array_model
     am.print_telescope_list()
-    assert am.number_of_telescopes == 9  # mock data has 9 telescopes
+    assert am.number_of_telescopes == 13
 
 
 def test_site(array_model):
@@ -204,7 +56,7 @@ def test_site(array_model):
     assert am.site == "North"
 
 
-def test_exporting_config_files(db_config, io_handler, model_version, mocker):
+def test_exporting_config_files(db_config, io_handler, model_version):
     am = ArrayModel(
         label="test",
         site="North",
@@ -240,10 +92,6 @@ def test_exporting_config_files(db_config, io_handler, model_version, mocker):
         "spe_afterpulse_pdf_NectarCam_18122019.dat",
         "transmission_lst_window_No7-10_ave.dat",
     ]
-
-    # Mock the file check since we don't actually create the files in tests
-    mock_path = mocker.patch("pathlib.Path.exists")
-    mock_path.return_value = True
 
     for model_file in list_of_export_files:
         logger.info("Checking file: %s", model_file)
