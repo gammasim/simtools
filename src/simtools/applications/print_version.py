107ae4908def20ecfa07447d2181d355573f7c7b
Orel Gueta <orel.gueta@desy.de>
2025-03-25 14:58:09 +0100

    Add application and its integration test

---
+++ src/simtools/applications/print_version.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Print the versions of the simtools software.
+
+The versions of simtools, the DB, sim_telarray, and CORSIKA are printed.
+
+"""
+
+import logging
+from pathlib import Path
+
+from simtools import dependencies, version
+from simtools.configuration import configurator
+from simtools.utils import general as gen
+
+
+def _parse(label, description, usage):
+    """
+    Parse command line configuration.
+
+    Parameters
+    ----------
+    label : str
+        Label describing the application.
+    description : str
+        Description of the application.
+    usage : str
+        Example on how to use the application.
+
+    Returns
+    -------
+    CommandLineParser
+        Command line parser object.
+    """
+    config = configurator.Configurator(label=label, description=description, usage=usage)
+
+    return config.initialize(db_config=True, require_command_line=False)
+
+
+def main():
+    """Print the versions of the simtools software."""
+    args_dict, db_config = _parse(
+        Path(__file__).stem,
+        description="Print the versions of simtools, the DB, sim_telarray and CORSIKA.",
+        usage="simtools-print-version",
+    )
+    logger = logging.getLogger()
+    logger.setLevel(gen.get_log_level_from_user(args_dict["log_level"]))
+
+    version_string = dependencies.get_version_string(db_config)
+
+    print()
+    print(f"simtools version: {version.__version__}")
+    print(version_string)
+
+
+if __name__ == "__main__":
+    main()
