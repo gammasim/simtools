9fe57d8965ec6459020c98b409c043e35d91a1fa
Gernot Maier <gernot.maier@desy.de>
2025-04-22 18:31:19 +0200

    fix reading parameters for primary particles

--- src/simtools/corsika/corsika_config.py
+++ src/simtools/corsika/corsika_config.py
@@ -296,7 +296,18 @@ class CorsikaConfig:
 
     def _input_config_corsika_starting_grammage(self, entry):
         """Return FIXCHI parameter CORSIKA format."""
-        return f"{entry['value'] * u.Unit(entry['unit']).to('g/cm2')}"
+        if isinstance(entry["value"], list):
+            value = None
+            default_value = None
+            for data in entry["value"]:
+                if data.get("primary_particle") == self.primary_particle.name:
+                    value = data.get("value")
+                if data.get("primary_particle") == "default":
+                    default_value = data.get("value")
+            value = value if value is not None else default_value
+        else:
+            value = entry["value"]
+        return f"{value * u.Unit(entry['unit']).to('g/cm2')}"
 
     def _input_config_corsika_particle_kinetic_energy_cutoff(self, entry):
         """Return ECUTS parameter CORSIKA format."""
