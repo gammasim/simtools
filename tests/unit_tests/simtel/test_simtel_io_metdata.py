ae735ca58b83c8e99219835130bce170a4bb4724
Orel Gueta <orel.gueta@desy.de>
2025-04-24 12:02:28 +0200

    Add two cases to test with real files and add a comment

--- tests/unit_tests/simtel/test_simtel_io_metdata.py
+++ tests/unit_tests/simtel/test_simtel_io_metdata.py
@@ -44,10 +44,29 @@ def test_get_sim_telarray_telescope_id(test_sim_telarray_file):
 
 
 def test_get_corsika_run_number_with_run_header():
+    assert (
+        get_corsika_run_number(
+            "tests/resources/run000010_gamma_za20deg_azm000deg_North_test_layout_"
+            "6.0.0_test-production-North.simtel.zst"
+        )
+        == 10
+    )
+    # The following file was actually created with the LightEmission package,
+    # but it should still return the run number correctly.
     assert get_corsika_run_number("tests/resources/xyzls_layout.simtel.gz") == 1
 
 
-def test_get_corsika_run_number_without_run_header():
+def test_get_corsika_run_number_without_run_header_real_file():
+    assert (
+        get_corsika_run_number(
+            "tests/resources/gamma_20deg_0deg_run1___cta-prod5-lapalma_"
+            "desert-2158m-LaPalma-dark.hdata.zst"
+        )
+        is None
+    )
+
+
+def test_get_corsika_run_number_without_run_header_mocking():
     mock_eventio_file = MagicMock()
     mock_eventio_file.__enter__.return_value = []
 
