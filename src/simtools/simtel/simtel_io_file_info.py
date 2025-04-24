26d13be0fd3a42458985e78d6c363d6353bc84f4
Gernot Maier <gernot.maier@desy.de>
2025-04-24 20:11:06 +0200

    read run header

--- src/simtools/simtel/simtel_io_file_info.py
+++ src/simtools/simtel/simtel_io_file_info.py
@@ -2,7 +2,7 @@
 """Read file info and run headers from sim_telarray files."""
 
 from eventio import EventIOFile
-from eventio.simtel import RunHeader
+from eventio.simtel import MCRunHeader, RunHeader
 
 
 def get_corsika_run_number(file):
@@ -19,16 +19,39 @@ def get_corsika_run_number(file):
     int, None
         CORSIKA run number. Returns None if not found.
     """
-    run_header = None
-    with EventIOFile(file) as f:
-        found_run_header = False
-        for o in f:
-            if isinstance(o, RunHeader):
-                found_run_header = True
-            else:
-                if found_run_header:
-                    break
-                continue
-            run_header = o.parse()
-
+    run_header = get_corsika_run_header(file)
     return run_header.get("run") if run_header else None
+
+
+def get_corsika_run_header(file):
+    """
+    Return the CORSIKA run header from a sim_telarray file.
+
+    Reads both RunHeader and MCRunHeader object from file and
+    returns a merged dictionary.
+
+    Parameters
+    ----------
+    file: str
+        Path to the sim_telarray file.
+
+    Returns
+    -------
+    dict, None
+        CORSIKA run header. Returns None if not found.
+    """
+    run_header = None
+    mc_run_header = None
+
+    with EventIOFile(file) as f:
+        for o in f:
+            if isinstance(o, RunHeader) and run_header is None:
+                run_header = o.parse()
+            elif isinstance(o, MCRunHeader) and mc_run_header is None:
+                mc_run_header = o.parse()
+            if run_header and mc_run_header:
+                break
+
+    run_header = run_header or {}
+    mc_run_header = mc_run_header or {}
+    return run_header | mc_run_header or None
--- tests/unit_tests/simtel/test_simtel_io_file_info.py
+++ tests/unit_tests/simtel/test_simtel_io_file_info.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 
+import pytest
 
-from simtools.simtel.simtel_io_file_info import get_corsika_run_number
+from simtools.simtel.simtel_io_file_info import get_corsika_run_header, get_corsika_run_number
 
 
-def test_get_corsika_run_number_with_run_header():
-    assert (
-        get_corsika_run_number(
-            "tests/resources/run000010_gamma_za20deg_azm000deg_North_test_layout_"
-            "6.0.0_test-production-North.simtel.zst"
-        )
-        == 10
+@pytest.fixture
+def test_file():
+    return (
+        "tests/resources/"
+        "run000010_gamma_za20deg_azm000deg_North_test_layout_6.0.0"
+        "_test-production-North.simtel.zst"
     )
+
+
+def test_get_corsika_run_number_with_run_header(test_file):
+    assert get_corsika_run_number(test_file) == 10
+
     # The following file was actually created with the LightEmission package,
     # but it should still return the run number correctly.
     assert get_corsika_run_number("tests/resources/xyzls_layout.simtel.gz") == 1
@@ -25,3 +30,9 @@ def test_get_corsika_run_number_without_run_header_real_file():
         )
         is None
     )
+
+
+def test_get_corsika_run_header(test_file):
+    run_header = get_corsika_run_header(test_file)
+    assert isinstance(run_header, dict)
+    assert run_header["run"] == 10
