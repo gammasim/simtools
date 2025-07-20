3ba6b5da9fb3519c9f0880543d0d41cbd942acc5
Gernot Maier <gernot.maier@desy.de>
2025-07-20 12:12:37 +0200

    docs

--- src/simtools/applications/simulate_calibration_events.py
+++ src/simtools/applications/simulate_calibration_events.py
@@ -3,16 +3,38 @@
 r"""
 Simulate calibration events like pedestal or flasher events.
 
+Use sim_telarray to simulate calibration events for an array of telescopes.
+The following types of calibration events are supported:
+
+* Pedestal events (includes night-sky background and camera noise)
+* Dark pedestal events (closed camera lid, camera noise only)
+* Flasher events (simulated flasher light source)
+
 Example
 -------
 
-Simulate pedestal events for MSTN-04 at the North site:
+Simulate pedestal events for alpha North. The assumed level night-sky background is 2.0 times the
+nominal value. A list of stars is provided to simulate to simulate additional contributions.
 
-    .. code-block:: console
+.. code-block:: console
 
-        simtools-simulate-calibration-events --run_mode="pedestals \\
-            --telescope MSTN-04 --site North \\
-            --number_of_events 1000 --model_version 6.0.0
+    simtools-simulate-calibration-events --run_mode=pedestals \\
+        --array_layout_name alpha --site North \\
+        --number_of_events 1000 --model_version 6.0.0 \\
+        --zenith_angle 20 --azimuth_angle 0 \\
+        --nsb_scaling_factor 2.0 --stars stars.txt
+
+Simulate flasher events for alpha South. Note that the same flasher configuration is used
+for all telescopes.
+
+.. code-block:: console
+
+    simtools-simulate-calibration-events --run_mode=flasher \\
+        --array_layout_name subsystem_msts --site South \\
+        --number_of_events 1000 --model_version 6.0.0 \\
+        --zenith_angle 20 --azimuth_angle 0 \\
+        --flasher_photons 500 --flasher_var_photons 0.05 \\
+        --flasher_exp_time 1.59 --flasher_sig_time 0.4
 
 Command Line Arguments
 ----------------------
@@ -20,12 +42,31 @@ run_mode (str, required)
     Run mode, e.g. "pedestals" or "flasher".
 number_of_events (int, required)
     Number of calibration events to simulate.
-telescope (str, required)
-    Telescope model name (e.g. LSTN-01, SSTS-design, SSTS-25, ...)
+array_layout_name (str, required)
+    Array layout name, e.g. "alpha".
 site (str, required)
     Site name.
 model_version (str, optional)
     Version of the simulation model.
+nsb_scaling_factor (float, optional)
+    Scaling factor for the night-sky background rate. Default is 1.0, which
+    corresponds to the nominal NSB rate.
+stars (str, optional)
+    Path to a file containing a list of stars (azimuth, zenith, weighting factor
+    separated by whitespace). If provided, the stars will be used to simulate
+    additional contributions to the night-sky background.
+zenith_angle (float, optional)
+    Zenith angle in degrees.
+azimuth_angle (float, optional)
+    Azimuth angle in degrees.
+flasher_photons (float, optional)
+    Number of photons in the flasher pulse at each photodetector.
+flasher_var_photons (float, optional)
+    Relative variance of the number of photons in the flasher pulse.
+flasher_exp_time (float, optional)
+    Exponential decay time of the flasher pulse in nano-seconds.
+flasher_sig_time (float, optional)
+    Sigma of Gaussian-shaped flasher pulse in nano-seconds.
 
 """
 
