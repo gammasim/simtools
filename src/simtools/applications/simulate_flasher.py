c0d369f21635a5cb56654663304f3e0ce3dabb08
tobiaskleiner <tobias.kleiner@desy.de>
2025-07-30 16:38:33 +0200

    Add flasher application

---
+++ src/simtools/applications/simulate_flasher.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python3
+
+r"""
+Simulate flasher devices (flat fielding) using the light emission package.
+
+Run the application in the command line.
+
+Example Usage
+-------------
+
+1. Simulate flashers for an MST telescope:
+
+    .. code-block:: console
+
+        simtools-simulate-flasher --telescope MSTN-04 --site North \
+        --flasher FLSN-01 --model_version 6.0.0
+
+2. Simulate flashers for an SST telescope:
+
+    .. code-block:: console
+
+        simtools-simulate-flasher --telescope SSTS-04 --site South \
+        --flasher FLSS-01 --model_version 6.0.0
+
+Command Line Arguments
+----------------------
+telescope (str, required)
+    Telescope model name (e.g. LSTN-01, MSTN-04, SSTS-04, ...)
+site (str, required)
+    Site name (North or South).
+flasher (str, required)
+    Flasher device in array, e.g., FLSN-01.
+model_version (str, optional)
+    Version of the simulation model.
+plot (flag, optional)
+    Produce a multiple pages pdf file with the image plots.
+integration_window (list, optional)
+    Integration window for the signal.
+boundary_thresh, picture_thresh, min_neighbors (int, optional)
+    Parameters for the image cleaning.
+return_cleaned (bool, optional)
+    Whether to return the cleaned image.
+
+Example
+-------
+
+Simulate flashers for MSTN-04:
+
+.. code-block:: console
+
+    simtools-simulate-flasher --telescope MSTN-04 --site North \
+    --flasher FLSN-01 --model_version 6.0.0
+
+Expected Output:
+    The simulation will run the light emission package for the flasher
+    devices and produce the output files.
+"""
+
+import logging
+from pathlib import Path
+
+import simtools.utils.general as gen
+from simtools.configuration import configurator
+from simtools.model.flasher_model import FlasherModel
+from simtools.model.model_utils import initialize_simulation_models
+from simtools.simtel.simulator_light_emission import SimulatorLightEmission
+
+
+def _parse(label):
+    """Parse command line configuration."""
+    config = configurator.Configurator(
+        label=label,
+        description=("Simulate flasher devices (flat fielding) using the light emission package."),
+    )
+    config.parser.add_argument(
+        "--plot",
+        help="Produce a multiple pages pdf file with the image plots.",
+        action="store_true",
+    )
+    config.parser.add_argument(
+        "--flasher",
+        help="Flasher device in array, i.e. FLSN-01",
+        type=str,
+        required=True,
+    )
+    config.parser.add_argument(
+        "--return_cleaned",
+        help="ctapipe, if image should be cleaned, notice as well image cleaning parameters",
+        type=str,
+        default=False,
+        required=False,
+    )
+    config.parser.add_argument(
+        "--picture_thresh",
+        help="ctapipe, threshold above which all pixels are retained",
+        type=int,
+        required=False,
+    )
+    config.parser.add_argument(
+        "--boundary_thresh",
+        help="ctapipe, threshold above which pixels are retained if\
+              they have a neighbor already above the picture_thresh",
+        type=int,
+        required=False,
+    )
+    config.parser.add_argument(
+        "--min_neighbors",
+        help="ctapipe, A picture pixel survives cleaning only if it has at\
+              least this number of picture neighbors. This has no effect in\
+              case keep_isolated_pixels is True",
+        type=int,
+        required=False,
+    )
+    config.parser.add_argument(
+        "--level",
+        help="read 5",
+        type=int,
+        default=5,
+        required=False,
+    )
+    config.parser.add_argument(
+        "--integration_window",
+        help="ctapipe, A picture pixel survives cleaning only if it has at\
+              least this number of picture neighbors. This has no effect in\
+              case keep_isolated_pixels is True",
+        nargs="*",
+        default=["7", "3"],
+        required=False,
+    )
+    return config.initialize(
+        db_config=True,
+        simulation_model=["telescope", "model_version"],
+        require_command_line=True,
+    )
+
+
+def flasher_configs(telescope_model_name):
+    """
+    Get flasher configurations based on telescope type.
+
+    Parameters
+    ----------
+    telescope_model_name
+        Telescope model name (LST-01, LST-02, MST-04, ...)
+
+    Returns
+    -------
+    tuple
+        Application name and mode
+
+    """
+    application = None
+
+    if "SST" in telescope_model_name:
+        # For dual-mirror telescopes (SST)
+        application = ("ff-gct", "flasher")
+    else:
+        # For single-mirror telescopes (MST, LST)
+        application = ("ff-1m", "flasher")
+
+    return application
+
+
+def main():
+    """Run the application."""
+    label = Path(__file__).stem
+    logger = logging.getLogger(__name__)
+
+    args_dict, db_config = _parse(label)
+    logger.setLevel(gen.get_log_level_from_user(args_dict["log_level"]))
+
+    telescope_model, site_model = initialize_simulation_models(
+        label=label,
+        db_config=db_config,
+        site=args_dict["site"],
+        telescope_name=args_dict["telescope"],
+        model_version=args_dict["model_version"],
+    )
+
+    flasher_model = FlasherModel(
+        site=args_dict["site"],
+        flasher_device_model_name=args_dict["flasher"],
+        mongo_db_config=db_config,
+        model_version=args_dict["model_version"],
+        label=label,
+    )
+
+    le_application = flasher_configs(telescope_model.name)
+
+    picture_thresh = int(args_dict["picture_thresh"]) if args_dict.get("picture_thresh") else 50
+    boundary_thresh = int(args_dict["boundary_thresh"]) if args_dict.get("boundary_thresh") else 20
+    min_neighbors = int(args_dict["min_neighbors"]) if args_dict.get("min_neighbors") else 2
+
+    sim_runner = SimulatorLightEmission(
+        telescope_model=telescope_model,
+        flasher_model=flasher_model,
+        site_model=site_model,
+        light_emission_config=args_dict,
+        le_application=le_application,
+        simtel_path=Path(args_dict["sim_telarray_path"]),
+        light_source_type="flasher",
+        label=args_dict["label"],
+        test=args_dict.get("test", False),
+    )
+
+    figures = []
+    simulation_args = {
+        "boundary_thresh": boundary_thresh,
+        "picture_thresh": picture_thresh,
+        "min_neighbors": min_neighbors,
+        "return_cleaned": args_dict.get("return_cleaned", False),
+        "level": args_dict.get("level", 5),
+        "integration_window": args_dict.get("integration_window", ["7", "3"]),
+    }
+
+    sim_runner.run_simulation(simulation_args, figures)
+
+    if args_dict.get("plot", False) and figures:
+        sim_runner.save_figures_to_pdf(figures, args_dict["telescope"])
+
+    logger.info("Flasher simulation completed.")
+
+
+if __name__ == "__main__":
+    main()
--- src/simtools/simtel/simulator_light_emission.py
+++ src/simtools/simtel/simulator_light_emission.py
@@ -1,4 +1,4 @@
-"""Simulation using the light emission package for calibration."""
+"""Simulation using the light emission package for calibration and flasher devices."""
 
 import logging
 import stat
@@ -27,16 +27,41 @@ class SimulatorLightEmission(SimtelRunner):
     def __init__(
         self,
         telescope_model,
-        calibration_model,
-        site_model,
-        light_emission_config,
-        le_application,
-        simtel_path,
-        light_source_type,
+        calibration_model=None,
+        flasher_model=None,
+        site_model=None,
+        light_emission_config=None,
+        le_application=None,
+        simtel_path=None,
+        light_source_type=None,
         label=None,
         test=False,
     ):
-        """Initialize SimtelRunner."""
+        """Initialize SimtelRunner.
+
+        Parameters
+        ----------
+        telescope_model : TelescopeModel
+            Model of the telescope to be simulated
+        calibration_model : CalibrationModel, optional
+            Model of the calibration device to be simulated
+        flasher_model : FlasherModel, optional
+            Model of the flasher device to be simulated
+        site_model : SiteModel, optional
+            Model of the site
+        light_emission_config : dict, optional
+            Configuration for the light emission
+        le_application : tuple, optional
+            Light emission application to be used
+        simtel_path : Path, optional
+            Path to the sim_telarray installation
+        light_source_type : str, optional
+            Type of light source: 'led', 'laser', or 'flasher'
+        label : str, optional
+            Label for the simulation
+        test : bool, optional
+            Whether this is a test run
+        """
         super().__init__(label=label, simtel_path=simtel_path)
 
         self._logger = logging.getLogger(__name__)
@@ -49,6 +74,7 @@ class SimulatorLightEmission(SimtelRunner):
         self.test = test
 
         self._calibration_model = calibration_model
+        self._flasher_model = flasher_model
         self._site_model = site_model
         self.io_handler = io_handler.IOHandler()
         self.output_directory = self.io_handler.get_output_directory(self.label)
@@ -56,11 +82,22 @@ class SimulatorLightEmission(SimtelRunner):
         # LightEmission - default parameters
         self._rep_number = 0
         self.runs = 1
-        self.photons_per_run = (
-            (self._calibration_model.get_parameter_value("photons_per_run"))
-            if not self.test
-            else 1e8
-        )
+
+        # Set photons per run based on the model (calibration or flasher)
+        if self._calibration_model is not None:
+            self.photons_per_run = (
+                (self._calibration_model.get_parameter_value("photons_per_run"))
+                if not self.test
+                else 1e8
+            )
+        elif self._flasher_model is not None:
+            self.photons_per_run = (
+                (self._flasher_model.get_parameter_value("photons_per_flasher"))
+                if not self.test
+                else 1e8
+            )
+        else:
+            self.photons_per_run = 1e8
 
         self.le_application = le_application
         self.light_emission_config = light_emission_config
@@ -109,6 +146,79 @@ class SimulatorLightEmission(SimtelRunner):
                 "unit": u.dimensionless_unscaled,
                 "default": 128,
                 "names": ["fadc_bins"],
+            },
+        }
+
+    @staticmethod
+    def flasher_default_configuration():
+        """
+        Get default flasher configuration.
+
+        Returns
+        -------
+        dict
+            Default configuration for flasher devices.
+        """
+        return {
+            "events": {
+                "len": 1,
+                "unit": None,
+                "default": 1,
+                "names": ["events"],
+            },
+            "photons_per_flasher": {
+                "len": 1,
+                "unit": None,
+                "default": 2.5e6,
+                "names": ["photons"],
+            },
+            "bunch_size": {
+                "len": 1,
+                "unit": None,
+                "default": 1.0,
+                "names": ["bunchsize"],
+            },
+            "flasher_position": {
+                "len": 2,
+                "unit": u.Unit("cm"),
+                "default": [0.0, 0.0] * u.cm,
+                "names": ["xy", "position"],
+            },
+            "flasher_depth": {
+                "len": 1,
+                "unit": u.Unit("cm"),
+                "default": 2.46 * u.cm,
+                "names": ["depth", "distance"],
+            },
+            "flasher_inclination": {
+                "len": 1,
+                "unit": u.Unit("deg"),
+                "default": 0.0 * u.deg,
+                "names": ["inclination"],
+            },
+            "spectrum": {
+                "len": 1,
+                "unit": u.Unit("nm"),
+                "default": 400 * u.nm,
+                "names": ["wavelength"],
+            },
+            "lightpulse": {
+                "len": 1,
+                "unit": None,
+                "default": "Simple:0",
+                "names": ["pulse"],
+            },
+            "angular_distribution": {
+                "len": 1,
+                "unit": None,
+                "default": "isotropic",
+                "names": ["angular"],
+            },
+            "flasher_pattern": {
+                "len": 1,
+                "unit": None,
+                "default": "all",
+                "names": ["fire", "pattern"],
             },
         }
 
@@ -192,9 +302,6 @@ class SimulatorLightEmission(SimtelRunner):
         str
             The commands to run the Light Emission package
         """
-        x_cal, y_cal, z_cal = self._calibration_model.get_parameter_value_with_unit(
-            "array_element_position_ground"
-        )
         x_tel, y_tel, z_tel = self._telescope_model.get_parameter_value_with_unit(
             "array_element_position_ground"
         )
@@ -215,69 +322,218 @@ class SimulatorLightEmission(SimtelRunner):
         command += f" -h  {corsika_observation_level.to(u.m).value}"
         command += f" --telpos-file {telpos_file}"
 
-        if self.light_source_type == "led":
-            if self.le_application[1] == "variable":
-                command += f" -x {self.light_emission_config['x_pos']['default'].to(u.cm).value}"
-                command += f" -y {self.light_emission_config['y_pos']['default'].to(u.cm).value}"
-                command += f" -z {self.light_emission_config['z_pos']['default'].to(u.cm).value}"
-                command += (
-                    f" -d {','.join(map(str, self.light_emission_config['direction']['default']))}"
-                )
-                command += f" -n {self.photons_per_run}"
+        if self.light_source_type == "flasher":
+            command = self._add_flasher_command_options(command)
+        elif self.light_source_type == "led":
+            command = self._add_led_command_options(command)
+        elif self.light_source_type == "laser":
+            command = self._add_laser_command_options(
+                command, x_tel, y_tel, z_tel, config_directory
+            )
 
-            elif self.le_application[1] == "layout":
-                command += f" -x {x_cal.to(u.cm).value}"
-                command += f" -y {y_cal.to(u.cm).value}"
-                command += f" -z {z_cal.to(u.cm).value}"
-                pointing_vector = self.calibration_pointing_direction()[0]
-                command += f" -d {','.join(map(str, pointing_vector))}"
-
-                command += f" -n {self.photons_per_run}"
-                self._logger.info(f"Photons per run: {self.photons_per_run} ")
-
-                laser_wavelength = self._calibration_model.get_parameter_value_with_unit(
-                    "laser_wavelength"
-                )
-                command += f" -s {int(laser_wavelength.to(u.nm).value)}"
-
-                led_pulse_sigtime = self._calibration_model.get_parameter_value_with_unit(
-                    "led_pulse_sigtime"
-                )
-                command += f" -p Gauss:{led_pulse_sigtime.to(u.ns).value}"
-                command += " -a isotropic"
-
+        # Add common options for the specific light source type
+        if self.light_source_type in ["flasher", "led"]:
             command += f" -A {config_directory}/"
             command += f"{self._telescope_model.get_parameter_value('atmospheric_profile')}"
 
-        elif self.light_source_type == "laser":
-            command += " --events 1"
-            command += " --bunches 2500000"
-            command += " --step 0.1"
-            command += " --bunchsize 1"
-            spectrum = self._calibration_model.get_parameter_value_with_unit("laser_wavelength")
-            command += f" --spectrum {int(spectrum.to(u.nm).value)}"
-            command += " --lightpulse Gauss:"
-            pulse_sigtime = self._calibration_model.get_parameter_value_with_unit(
-                "laser_pulse_sigtime"
-            )
-            command += f"{pulse_sigtime.to(u.ns).value}"
-            x_origin = x_cal - x_tel
-            y_origin = y_cal - y_tel
-            z_origin = z_cal - z_tel
-            _, angles = self.calibration_pointing_direction()
-            angle_theta = angles[0]
-            angle_phi = angles[1]
-            positions = x_origin.to(u.cm).value, y_origin.to(u.cm).value, z_origin.to(u.cm).value
-            command += f" --laser-position '{positions[0]},{positions[1]},{positions[2]}'"
-
-            command += f" --telescope-theta {angle_theta}"
-            command += f" --telescope-phi {angle_phi}"
-            command += f" --laser-theta {90 - angles[2]}"
-            command += f" --laser-phi {angles[3]}"
-            command += f" --atmosphere {config_directory}/"
-            command += f"{self._telescope_model.get_parameter_value('atmospheric_profile')}"
         command += f" -o {self.output_directory}/{self.le_application[0]}.iact.gz"
         command += "\n"
+
+        return command
+
+    def _add_flasher_command_options(self, command):
+        """
+        Add flasher-specific command options to the light emission script.
+
+        Parameters
+        ----------
+        command : str
+            The command string to add options to
+
+        Returns
+        -------
+        str
+            The updated command string
+        """
+        telescope_name = self._telescope_model.name
+        if "SST" in telescope_name:
+            command = self._add_sst_flasher_options(command)
+        else:
+            command = self._add_mst_lst_flasher_options(command)
+        return command
+
+    def _add_sst_flasher_options(self, command):
+        """
+        Add SST-specific flasher options to the command.
+
+        Parameters
+        ----------
+        command : str
+            The command string to add options to
+
+        Returns
+        -------
+        str
+            The updated command string
+        """
+        # For SST we use ff-gct style flashers (dual-mirror design)
+        flasher_xy = self._flasher_model.get_parameter_value("flasher_position")
+        flasher_depth = self._flasher_model.get_parameter_value("flasher_depth")
+        flasher_inclination = self._flasher_model.get_parameter_value("flasher_inclination")
+        mirror_camera_distance = self._flasher_model.get_parameter_value("mirror_camera_distance")
+        spectrum = self._flasher_model.get_parameter_value("spectrum")
+        pulse = self._flasher_model.get_parameter_value("lightpulse")
+        angular = self._flasher_model.get_parameter_value("angular_distribution")
+        fire_pattern = self._flasher_model.get_parameter_value("flasher_pattern")
+        bunch_size = self._flasher_model.get_parameter_value("bunch_size")
+
+        command += f" --events {self.runs}"
+        command += f" --photons {self.photons_per_run}"
+        command += f" --bunchsize {bunch_size}"
+        command += f" --flasher-xy {flasher_xy[0]}"
+        command += f" --flasher-depth {flasher_depth}"
+        command += f" --flasher-inclination {flasher_inclination}"
+        command += f" --mirror-camera-distance {mirror_camera_distance}"
+        command += f" --spectrum {spectrum}"
+        command += f" --lightpulse {pulse}"
+        command += f" --angular-distribution {angular}"
+        command += f" --fire {fire_pattern}"
+        return command
+
+    def _add_mst_lst_flasher_options(self, command):
+        """
+        Add MST/LST-specific flasher options to the command.
+
+        Parameters
+        ----------
+        command : str
+            The command string to add options to
+
+        Returns
+        -------
+        str
+            The updated command string
+        """
+        # For MST/LST we use ff-1m style flashers (single-mirror design)
+        flasher_xy = self._flasher_model.get_parameter_value("flasher_position")
+        flasher_distance = self._flasher_model.get_parameter_value("flasher_depth")
+        camera_radius = (
+            self._telescope_model.get_parameter_value_with_unit("camera_radius").to(u.cm).value
+        )
+        spectrum = self._flasher_model.get_parameter_value("spectrum")
+        pulse = self._flasher_model.get_parameter_value("lightpulse")
+        angular = self._flasher_model.get_parameter_value("angular_distribution")
+        bunch_size = self._flasher_model.get_parameter_value("bunch_size")
+
+        command += f" --events {self.runs}"
+        command += f" --photons {self.photons_per_run}"
+        command += f" --bunchsize {bunch_size}"
+        command += f" --xy {flasher_xy[0]},{flasher_xy[1]}"
+        command += f" --distance {flasher_distance}"
+        command += f" --camera-radius {camera_radius}"
+        command += f" --spectrum {spectrum}"
+        command += f" --lightpulse {pulse}"
+        command += f" --angular-distribution {angular}"
+        return command
+
+    def _add_led_command_options(self, command):
+        """
+        Add LED-specific command options to the light emission script.
+
+        Parameters
+        ----------
+        command : str
+            The command string to add options to
+
+        Returns
+        -------
+        str
+            The updated command string
+        """
+        if self.le_application[1] == "variable":
+            command += f" -x {self.light_emission_config['x_pos']['default'].to(u.cm).value}"
+            command += f" -y {self.light_emission_config['y_pos']['default'].to(u.cm).value}"
+            command += f" -z {self.light_emission_config['z_pos']['default'].to(u.cm).value}"
+            command += (
+                f" -d {','.join(map(str, self.light_emission_config['direction']['default']))}"
+            )
+            command += f" -n {self.photons_per_run}"
+
+        elif self.le_application[1] == "layout":
+            x_cal, y_cal, z_cal = self._calibration_model.get_parameter_value_with_unit(
+                "array_element_position_ground"
+            )
+            command += f" -x {x_cal.to(u.cm).value}"
+            command += f" -y {y_cal.to(u.cm).value}"
+            command += f" -z {z_cal.to(u.cm).value}"
+            pointing_vector = self.calibration_pointing_direction()[0]
+            command += f" -d {','.join(map(str, pointing_vector))}"
+
+            command += f" -n {self.photons_per_run}"
+            self._logger.info(f"Photons per run: {self.photons_per_run} ")
+
+            laser_wavelength = self._calibration_model.get_parameter_value_with_unit(
+                "laser_wavelength"
+            )
+            command += f" -s {int(laser_wavelength.to(u.nm).value)}"
+
+            led_pulse_sigtime = self._calibration_model.get_parameter_value_with_unit(
+                "led_pulse_sigtime"
+            )
+            command += f" -p Gauss:{led_pulse_sigtime.to(u.ns).value}"
+            command += " -a isotropic"
+
+        return command
+
+    def _add_laser_command_options(self, command, x_tel, y_tel, z_tel, config_directory):
+        """
+        Add laser-specific command options to the light emission script.
+
+        Parameters
+        ----------
+        command : str
+            The command string to add options to
+        x_tel : astropy.units.Quantity
+            X position of the telescope
+        y_tel : astropy.units.Quantity
+            Y position of the telescope
+        z_tel : astropy.units.Quantity
+            Z position of the telescope
+        config_directory : Path
+            Path to the configuration directory
+
+        Returns
+        -------
+        str
+            The updated command string
+        """
+        x_cal, y_cal, z_cal = self._calibration_model.get_parameter_value_with_unit(
+            "array_element_position_ground"
+        )
+        command += " --events 1"
+        command += " --bunches 2500000"
+        command += " --step 0.1"
+        command += " --bunchsize 1"
+        spectrum = self._calibration_model.get_parameter_value_with_unit("laser_wavelength")
+        command += f" --spectrum {int(spectrum.to(u.nm).value)}"
+        command += " --lightpulse Gauss:"
+        pulse_sigtime = self._calibration_model.get_parameter_value_with_unit("laser_pulse_sigtime")
+        command += f"{pulse_sigtime.to(u.ns).value}"
+        x_origin = x_cal - x_tel
+        y_origin = y_cal - y_tel
+        z_origin = z_cal - z_tel
+        _, angles = self.calibration_pointing_direction()
+        angle_theta = angles[0]
+        angle_phi = angles[1]
+        positions = x_origin.to(u.cm).value, y_origin.to(u.cm).value, z_origin.to(u.cm).value
+        command += f" --laser-position '{positions[0]},{positions[1]},{positions[2]}'"
+
+        command += f" --telescope-theta {angle_theta}"
+        command += f" --telescope-phi {angle_phi}"
+        command += f" --laser-theta {90 - angles[2]}"
+        command += f" --laser-phi {angles[3]}"
+        command += f" --atmosphere {config_directory}/"
+        command += f"{self._telescope_model.get_parameter_value('atmospheric_profile')}"
 
         return command
 
