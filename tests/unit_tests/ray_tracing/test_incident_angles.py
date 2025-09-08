7be903135883e8cff9a64e78e81e066f150ca83c
tobiaskleiner <tobias.kleiner@desy.de>
2025-09-08 13:29:39 +0200

    remove

--- tests/unit_tests/ray_tracing/test_incident_angles.py
+++ tests/unit_tests/ray_tracing/test_incident_angles.py
@@ -628,7 +628,6 @@ def test_parse_float_success_and_failure():
 
 
 def test_contains_axis_cases():
-    # Function expects lower-cased descriptions (callers lower-case header text)
     assert IncidentAnglesCalculator._contains_axis("x reflection point on primary", "x")
     assert IncidentAnglesCalculator._contains_axis("y reflection point", "y")
     # Should not match axis embedded in a word
