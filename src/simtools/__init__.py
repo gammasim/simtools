0cdb87a199ac8b90c91992542071f8fe88afc549
Gernot Maier <gernot.maier@desy.de>
2025-10-01 15:21:59 +0200

    keep it here

--- src/simtools/__init__.py
+++ src/simtools/__init__.py
@@ -4,3 +4,5 @@ from .version import __version__
 
 logging.basicConfig(format="%(levelname)s::%(module)s(l%(lineno)s)::%(funcName)s::%(message)s")
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
+__all__ = ["__version__"]
