cd7fd7f531c3d57a6496ab490efe235935b83ed6
Gernot Maier <gernot.maier@desy.de>
2025-11-11 18:37:11 +0100

    not needed

--- database_scripts/setup_local_db.sh
+++ database_scripts/setup_local_db.sh
@@ -59,7 +59,6 @@ db.createUser({
     { role: 'readWriteAnyDatabase', db: 'admin' },
     { role: 'dbAdminAnyDatabase', db: 'admin' },
     { role: 'userAdminAnyDatabase', db: 'admin' },
-    { role: 'clusterMonitor', db: 'admin' },
   ]
 });
 "; then
