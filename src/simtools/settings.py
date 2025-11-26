2d4ba2004fd6b6011d00db40ddf804ecf141d473
Gernot Maier <gernot.maier@desy.de>
2025-11-26 17:50:46 +0100

    unit tests

--- src/simtools/settings.py
+++ src/simtools/settings.py
@@ -6,7 +6,10 @@ from types import MappingProxyType
 
 
 class _Config:
+    """Centralized (immutable) settings object with command line and environment variables."""
+
     def __init__(self):
+        """Initialize empty config."""
         self._args = {}
         self._db_config = {}
         self._sim_telarray_path = None
@@ -15,6 +18,19 @@ class _Config:
         self._corsika_exe = None
 
     def load(self, args=None, db_config=None):
+        """
+        Load configuration from command line arguments and environment variables.
+
+        For paths, first check for environment variables, then command line arguments.
+
+        Parameters
+        ----------
+        args : dict, optional
+            Command line arguments.
+        db_config : dict, optional
+            Database configuration.
+
+        """
         self._args = MappingProxyType(args) if args is not None else {}
         self._db_config = MappingProxyType(db_config) if db_config is not None else {}
         self._sim_telarray_path = os.getenv(
@@ -36,18 +52,22 @@ class _Config:
 
     @property
     def args(self):
+        """Command line arguments."""
         return self._args
 
     @property
     def db_config(self):
+        """Database configuration."""
         return self._db_config
 
     @property
     def sim_telarray_path(self):
+        """Path to the sim_telarray installation directory."""
         return Path(self._sim_telarray_path) if self._sim_telarray_path is not None else None
 
     @property
     def sim_telarray_exe(self):
+        """Path to the sim_telarray executable."""
         return (
             Path(self._sim_telarray_path) / "bin" / self._sim_telarray_exe
             if self._sim_telarray_path is not None
@@ -56,6 +76,7 @@ class _Config:
 
     @property
     def sim_telarray_exe_debug_trace(self):
+        """Path to the debug trace version of the sim_telarray executable."""
         return (
             Path(self._sim_telarray_path) / "bin" / (self._sim_telarray_exe + "_debug_trace")
             if self._sim_telarray_path is not None
@@ -64,16 +85,19 @@ class _Config:
 
     @property
     def corsika_path(self):
+        """Path to the CORSIKA installation directory."""
         return Path(self._corsika_path) if self._corsika_path is not None else None
 
     @property
     def corsika_exe(self):
+        """Path to the CORSIKA executable."""
         return (
             Path(self._corsika_path) / self._corsika_exe if self._corsika_path is not None else None
         )
 
     @property
     def corsika_exe_curved(self):
+        """Path to the curved version of the CORSIKA executable."""
         corsika_curved = (
             self._corsika_exe.replace("_flat", "_curved")
             if "_flat" in self._corsika_exe
