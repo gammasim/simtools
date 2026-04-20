602db58da94ab6b6b080cee1c185a6533fe160fb
Gernot Maier <gernot.maier@desy.de>
2026-04-20 15:18:28 +0200

    Add possibility to plot single layout from a array layout parameter json file.

--- tests/unit_tests/configuration/test_commandline_parser.py
+++ tests/unit_tests/configuration/test_commandline_parser.py
@@ -382,15 +382,25 @@ def test_layout_parsers():
         simulation_model=["layout", "layout_file", "plot_all_layouts", "layout_parameter_file"]
     )
     job_groups = _parser_9._action_groups
+    parser_actions = _parser_9._actions
     for group in job_groups:
         if str(group.title) == SIMULATION_MODEL_STRING:
             assert any(action.dest == "array_layout_name" for action in group._group_actions)
             assert any(action.dest == "array_element_list" for action in group._group_actions)
             assert any(action.dest == "array_layout_file" for action in group._group_actions)
             assert any(action.dest == "plot_all_layouts" for action in group._group_actions)
-            assert any(
-                action.dest == "array_layout_parameter_file" for action in group._group_actions
-            )
+    assert any(action.dest == "array_layout_parameter_file" for action in parser_actions)
+
+    args = _parser_9.parse_args(
+        [
+            "--array_layout_name",
+            "alpha",
+            "--array_layout_parameter_file",
+            "array_layouts.json",
+        ]
+    )
+    assert args.array_layout_name == ["alpha"]
+    assert args.array_layout_parameter_file == "array_layouts.json"
 
 
 def test_simulation_configuration():
