2592884760798417ab9a522faade42b07f5261f7
Gernot Maier <gernot.maier@desy.de>
2026-05-03 21:07:03 +0200

    unit tests for convert simtel parameters

---
+++ src/simtools/model/sim_telarray_parameter_converter.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python3
+"""Utilities to convert simulation model parameters from sim_telarray files."""
+
+import logging
+from pathlib import Path
+
+import numpy as np
+
+import simtools.data_model.model_data_writer as writer
+from simtools.data_model import schema
+from simtools.io import ascii_handler
+from simtools.simtel import simtel_config_reader
+
+_logger = logging.getLogger(__name__)
+
+
+def _read_simtel_config_file(args_dict, schema_file, camera_pixels=None):
+    """Read the sim_telarray configuration file."""
+    config_reader = simtel_config_reader.SimtelConfigReader(
+        schema_file=schema_file,
+        simtel_config_file=args_dict["simtel_cfg_file"],
+        simtel_telescope_name=args_dict["simtel_telescope_name"],
+        camera_pixels=camera_pixels,
+    )
+    if config_reader.parameter_dict is None or len(config_reader.parameter_dict) == 0:
+        return None
+    return config_reader
+
+
+def _get_number_of_camera_pixel(args_dict):
+    """
+    Get the number of camera pixels from the sim_telarray configuration file.
+
+    Required to set the dimension some of the parameter correctly, as simtel
+    in some cases does not provide the dimension ('all:' in the parameter files).
+    """
+    try:
+        config_reader = _read_simtel_config_file(
+            args_dict, schema.get_model_parameter_schema_file("camera_pixels")
+        )
+        _camera_pixel = config_reader.parameter_dict.get(args_dict["simtel_telescope_name"])
+    except (FileNotFoundError, AttributeError):
+        _logger.warning("Unable to retrieve camera pixel parameter.")
+        _camera_pixel = None
+    _logger.info(f"Number of camera pixels: {_camera_pixel}")
+    return _camera_pixel
+
+
+def read_and_export_parameters(args_dict, io_handler):
+    """
+    Read and export parameters from sim_telarray configuration file to json files.
+
+    Parameters
+    ----------
+    args_dict: dict
+        Dictionary with command line arguments.
+    io_handler: IOHandler
+        IOHandler object
+
+    Returns
+    -------
+    list
+        List of sim_telarray parameters not found in schema files.
+    list
+        List of simtools parameter not found in sim_telarray configuration file.
+
+    """
+    _parameters, _schema_files = schema.get_model_parameter_schema_files()
+    simtel_parameters = simtel_config_reader.get_list_of_simtel_parameters(
+        args_dict["simtel_cfg_file"]
+    )
+    _logger.info(f"Found {len(simtel_parameters)} parameters in sim_telarray configuration file.")
+
+    _camera_pixel = _get_number_of_camera_pixel(args_dict)
+
+    parameters_not_in_simtel = []
+
+    for _parameter, _schema_file in zip(_parameters, _schema_files):
+        _logger.info(f"Parameter: {_parameter} Schema file: {_schema_file}")
+        if _parameter in args_dict["skip_parameter"]:
+            _logger.info(f"Skipping {_parameter}")
+            continue
+        config_reader = _read_simtel_config_file(args_dict, _schema_file, _camera_pixel)
+
+        if config_reader is None:
+            _logger.info("Parameter not found in sim_telarray configuration file.")
+            parameters_not_in_simtel.append(_parameter)
+            continue
+
+        _logger.info(f"sim_telarray parameter: {config_reader.parameter_dict}")
+
+        _json_dict = writer.ModelDataWriter.write_model_parameter(
+            parameter_name=_parameter,
+            value=config_reader.parameter_dict.get(args_dict["simtel_telescope_name"]),
+            instrument=args_dict["telescope"],
+            parameter_version=args_dict["parameter_version"],
+            output_file=io_handler.get_output_file(
+                f"{_parameter}-{args_dict['parameter_version']}.json",
+                sub_dir=[f"{args_dict['telescope']}", f"{_parameter}"],
+            ),
+        )
+
+        config_reader.compare_simtel_config_with_schema()
+
+        if config_reader.simtel_parameter_name.lower() in simtel_parameters:
+            simtel_parameters.remove(config_reader.simtel_parameter_name.lower())
+
+        if _json_dict["file"]:
+            _logger.info(f"File name for {_parameter} is {_json_dict['value']}")
+
+    return parameters_not_in_simtel, simtel_parameters
+
+
+def print_parameters_not_found(parameters_not_in_simtel, simtel_parameters, args_dict):
+    """
+    Print simtel/simtools parameter not found in schema and configuration files.
+
+    For sim_telarray parameters not found, check if the setting for the chose
+    telescope is different from the default values.
+
+    Parameters
+    ----------
+    parameters_not_in_simtel: list
+        List of sim_telarray parameters not found in schema files.
+    simtel_parameters: list
+        List of sim_telarray parameters not found in simtools schema files.
+    args_dict: dict
+        Dictionary with command line arguments.
+
+    """
+    _logger.info(
+        f"Parameters not found in simtools schema files ({len(parameters_not_in_simtel)}):"
+    )
+    for para in sorted(parameters_not_in_simtel):
+        _logger.info(f"  {para}")
+
+    _logger.info(f"sim_telarray parameters not found in schema files ({len(simtel_parameters)}):")
+    for para in sorted(simtel_parameters):
+        _logger.info(f"sim_telarray parameter: {para}")
+        config_reader = simtel_config_reader.SimtelConfigReader(
+            schema_file=None,
+            simtel_config_file=args_dict["simtel_cfg_file"],
+            simtel_telescope_name=args_dict["simtel_telescope_name"],
+            parameter_name=para,
+        )
+        _default = config_reader.parameter_dict.get("default")
+        _tel_value = config_reader.parameter_dict.get(args_dict["simtel_telescope_name"])
+        # simple comparison of default value and telescope values, does not work for lists
+        try:
+            if _default == _tel_value or np.isclose(_default, _tel_value):
+                _logger.info(f"    Default and telescope values for {para} are equal: {_default}")
+                continue
+        except (ValueError, TypeError):
+            pass
+        if isinstance(_default, np.ndarray):
+            _logger.warning(f"    Default value ({para}): {_default} (length: {_default.size})")
+        else:
+            _logger.warning(f"    Default value ({para}): {_default}")
+        if isinstance(_tel_value, np.ndarray):
+            _logger.warning(
+                f"    Telescope value ({para}): {_tel_value} (length: {_tel_value.size})"
+            )
+        else:
+            _logger.warning(f"    Telescope value ({para}): {_tel_value}")
+
+
+def print_list_of_files(args_dict):
+    """
+    Print model parameters which describe a file name.
+
+    This is useful to find files which are part of the model.
+
+    Parameters
+    ----------
+    args_dict: dict
+        Dictionary with command line arguments.
+    """
+    model_files = sorted(Path(args_dict["output_path"]).rglob("*.json"))
+    for file in model_files:
+        model_dict = ascii_handler.collect_data_from_file(file_name=file)
+        if model_dict.get("file"):
+            _logger.info(f"{file.name}: {model_dict['value']}")
+
+
+def run_conversion_workflow(args_dict, io_handler):
+    """
+    Run the full model-parameter conversion workflow.
+
+    Parameters
+    ----------
+    args_dict: dict
+        Dictionary with command line arguments.
+    io_handler: IOHandler
+        IOHandler object
+    """
+    parameters_not_in_simtel, simtel_parameters = read_and_export_parameters(args_dict, io_handler)
+    print_parameters_not_found(parameters_not_in_simtel, simtel_parameters, args_dict)
+    print_list_of_files(args_dict)
---
+++ tests/unit_tests/model/test_sim_telarray_parameter_converter.py
@@ -0,0 +1,298 @@
+#!/usr/bin/python3
+
+
+import numpy as np
+
+from simtools.model import sim_telarray_parameter_converter as converter
+
+
+class _DummyLogger:
+    def __init__(self):
+        self.messages = []
+
+    def info(self, message):
+        self.messages.append(("info", message))
+
+    def warning(self, message):
+        self.messages.append(("warning", message))
+
+
+def test_read_simtel_config_file_returns_none_when_parameter_dict_empty(monkeypatch):
+    class _Reader:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = {}
+
+    monkeypatch.setattr(converter.simtel_config_reader, "SimtelConfigReader", _Reader)
+
+    result = converter._read_simtel_config_file(
+        {
+            "simtel_cfg_file": "dummy.cfg",
+            "simtel_telescope_name": "CT1",
+        },
+        schema_file="dummy.schema.yml",
+    )
+
+    assert result is None
+
+
+def test_get_number_of_camera_pixel_handles_missing_data(monkeypatch):
+    monkeypatch.setattr(converter.schema, "get_model_parameter_schema_file", lambda _name: "schema")
+
+    def _raise_file_not_found(_args_dict, _schema_file, _camera_pixels=None):
+        raise FileNotFoundError
+
+    monkeypatch.setattr(converter, "_read_simtel_config_file", _raise_file_not_found)
+
+    camera_pixels = converter._get_number_of_camera_pixel(
+        {
+            "simtel_cfg_file": "dummy.cfg",
+            "simtel_telescope_name": "CT1",
+        },
+    )
+
+    assert camera_pixels is None
+
+
+def test_read_and_export_parameters(monkeypatch):
+    args_dict = {
+        "simtel_cfg_file": "dummy.cfg",
+        "simtel_telescope_name": "CT1",
+        "skip_parameter": ["skip_me"],
+        "telescope": "LSTN-01",
+        "parameter_version": "1.0.0",
+    }
+
+    monkeypatch.setattr(
+        converter.schema,
+        "get_model_parameter_schema_files",
+        lambda: (["keep_me", "skip_me", "missing_me"], ["schema1", "schema2", "schema3"]),
+    )
+    monkeypatch.setattr(
+        converter.simtel_config_reader,
+        "get_list_of_simtel_parameters",
+        lambda _cfg: ["keep_simtel", "missing_simtel"],
+    )
+    monkeypatch.setattr(converter, "_get_number_of_camera_pixel", lambda _args_dict: 1855)
+
+    class _ConfigReader:
+        def __init__(self, value, simtel_name):
+            self.parameter_dict = {"CT1": value}
+            self.simtel_parameter_name = simtel_name
+            self.compared = False
+
+        def compare_simtel_config_with_schema(self):
+            self.compared = True
+
+    config_reader = _ConfigReader(value=42, simtel_name="keep_simtel")
+
+    def _read_simtel_config_file(_args_dict, schema_file, camera_pixels=None):
+        assert camera_pixels == 1855
+        if schema_file == "schema1":
+            return config_reader
+        return None
+
+    monkeypatch.setattr(converter, "_read_simtel_config_file", _read_simtel_config_file)
+
+    written = {}
+
+    class _DummyIOHandler:
+        @staticmethod
+        def get_output_file(file_name, sub_dir=None):
+            return f"/tmp/{'/'.join(sub_dir)}/{file_name}"
+
+    def _write_model_parameter(**kwargs):
+        written["kwargs"] = kwargs
+        return {"file": False, "value": kwargs["output_file"]}
+
+    monkeypatch.setattr(
+        converter.writer.ModelDataWriter, "write_model_parameter", _write_model_parameter
+    )
+
+    parameters_not_in_simtel, simtel_parameters = converter.read_and_export_parameters(
+        args_dict,
+        _DummyIOHandler(),
+    )
+
+    assert parameters_not_in_simtel == ["missing_me"]
+    assert simtel_parameters == ["missing_simtel"]
+    assert written["kwargs"]["parameter_name"] == "keep_me"
+    assert config_reader.compared is True
+
+
+def test_print_parameters_not_found_handles_scalar_and_array(monkeypatch):
+    args_dict = {
+        "simtel_cfg_file": "dummy.cfg",
+        "simtel_telescope_name": "CT1",
+    }
+
+    class _ReaderScalar:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = {"default": 10.0, "CT1": 11.0}
+
+    class _ReaderArray:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = {
+                "default": np.array([1.0, 2.0]),
+                "CT1": np.array([1.5, 2.5]),
+            }
+
+    def _reader_factory(**kwargs):
+        if kwargs["parameter_name"] == "scalar_parameter":
+            return _ReaderScalar()
+        return _ReaderArray()
+
+    monkeypatch.setattr(converter.simtel_config_reader, "SimtelConfigReader", _reader_factory)
+
+    warning_messages = []
+    monkeypatch.setattr(converter._logger, "warning", lambda msg: warning_messages.append(msg))
+
+    converter.print_parameters_not_found(
+        ["missing_a"],
+        ["scalar_parameter", "array_parameter"],
+        args_dict,
+    )
+
+    assert any("Default value (scalar_parameter)" in msg for msg in warning_messages)
+    assert any("Default value (array_parameter)" in msg for msg in warning_messages)
+
+
+def test_run_conversion_workflow(monkeypatch):
+    calls = []
+
+    monkeypatch.setattr(
+        converter,
+        "read_and_export_parameters",
+        lambda _args_dict, _io_handler: (["a"], ["b"]),
+    )
+
+    def _print_parameters_not_found(parameters_not_in_simtel, simtel_parameters, _args_dict):
+        calls.append("print_parameters_not_found")
+
+    def _print_list_of_files(_args_dict):
+        calls.append("print_list_of_files")
+
+    monkeypatch.setattr(converter, "print_parameters_not_found", _print_parameters_not_found)
+    monkeypatch.setattr(converter, "print_list_of_files", _print_list_of_files)
+
+    converter.run_conversion_workflow(args_dict={}, io_handler=object())
+
+    assert calls == ["print_parameters_not_found", "print_list_of_files"]
+
+
+def test_read_simtel_config_file_returns_none_when_parameter_dict_is_none(monkeypatch):
+    class _Reader:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = None
+
+    monkeypatch.setattr(converter.simtel_config_reader, "SimtelConfigReader", _Reader)
+
+    result = converter._read_simtel_config_file(
+        {"simtel_cfg_file": "dummy.cfg", "simtel_telescope_name": "CT1"},
+        schema_file="dummy.schema.yml",
+    )
+
+    assert result is None
+
+
+def test_get_number_of_camera_pixel_happy_path(monkeypatch):
+    monkeypatch.setattr(converter.schema, "get_model_parameter_schema_file", lambda _name: "schema")
+
+    class _Reader:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = {"CT1": 1855}
+
+    monkeypatch.setattr(converter.simtel_config_reader, "SimtelConfigReader", _Reader)
+
+    result = converter._get_number_of_camera_pixel(
+        {"simtel_cfg_file": "dummy.cfg", "simtel_telescope_name": "CT1"}
+    )
+
+    assert result == 1855
+
+
+def test_read_and_export_parameters_logs_file_name(monkeypatch):
+    args_dict = {
+        "simtel_cfg_file": "dummy.cfg",
+        "simtel_telescope_name": "CT1",
+        "skip_parameter": [],
+        "telescope": "LSTN-01",
+        "parameter_version": "1.0.0",
+    }
+
+    monkeypatch.setattr(
+        converter.schema,
+        "get_model_parameter_schema_files",
+        lambda: (["file_param"], ["schema1"]),
+    )
+    monkeypatch.setattr(
+        converter.simtel_config_reader,
+        "get_list_of_simtel_parameters",
+        lambda _cfg: ["file_simtel"],
+    )
+    monkeypatch.setattr(converter, "_get_number_of_camera_pixel", lambda _args_dict: None)
+
+    class _ConfigReader:
+        parameter_dict = {"CT1": "some_file.fits"}
+        simtel_parameter_name = "file_simtel"
+
+        def compare_simtel_config_with_schema(self):
+            pass
+
+    monkeypatch.setattr(converter, "_read_simtel_config_file", lambda *a, **kw: _ConfigReader())
+
+    class _IOHandler:
+        @staticmethod
+        def get_output_file(file_name, sub_dir=None):
+            return f"/tmp/{file_name}"
+
+    monkeypatch.setattr(
+        converter.writer.ModelDataWriter,
+        "write_model_parameter",
+        lambda **kwargs: {"file": True, "value": "some_file.fits"},
+    )
+
+    info_messages = []
+    monkeypatch.setattr(converter._logger, "info", lambda msg: info_messages.append(msg))
+
+    converter.read_and_export_parameters(args_dict, _IOHandler())
+
+    assert any("File name for file_param" in msg for msg in info_messages)
+
+
+def test_print_parameters_not_found_equal_values(monkeypatch):
+    args_dict = {"simtel_cfg_file": "dummy.cfg", "simtel_telescope_name": "CT1"}
+
+    class _ReaderEqual:
+        def __init__(self, **_kwargs):
+            self.parameter_dict = {"default": 5.0, "CT1": 5.0}
+
+    monkeypatch.setattr(
+        converter.simtel_config_reader, "SimtelConfigReader", lambda **kw: _ReaderEqual()
+    )
+
+    info_messages = []
+    monkeypatch.setattr(converter._logger, "info", lambda msg: info_messages.append(msg))
+
+    converter.print_parameters_not_found([], ["equal_param"], args_dict)
+
+    assert any(
+        "Default and telescope values for equal_param are equal" in msg for msg in info_messages
+    )
+
+
+def test_print_list_of_files(monkeypatch, tmp_test_directory):
+    json_file = tmp_test_directory.join("param-1.0.0.json")
+    json_file.write("{}")
+
+    monkeypatch.setattr(
+        converter.ascii_handler,
+        "collect_data_from_file",
+        lambda file_name: {"file": True, "value": "some_file.fits"},
+    )
+
+    info_messages = []
+    monkeypatch.setattr(converter._logger, "info", lambda msg: info_messages.append(msg))
+
+    converter.print_list_of_files({"output_path": str(tmp_test_directory)})
+
+    assert any("param-1.0.0.json" in msg for msg in info_messages)
