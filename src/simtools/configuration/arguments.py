e8a2749c807c6dc7afc4ca8389861b1115193dc8
GernotMaier <gernot.maier@desy.de>
2026-08-03 12:25:55 +0200

    store true for b-field

--- src/simtools/configuration/arguments.py
+++ src/simtools/configuration/arguments.py
@@ -628,7 +628,7 @@ CORRECT_FOR_B_FIELD_ALIGNMENT = _argument(
     "correct_for_b_field_alignment",
     _SIMULATION_CONFIGURATION_GROUP,
     help="Align North with geographic North (and not magnetic North).",
-    action="store_true",
+    action=argparse.BooleanOptionalAction,
     default=True,
 )
 
