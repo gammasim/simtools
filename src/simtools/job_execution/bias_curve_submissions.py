69e82ac498beebe8c030d0ff67c5e901cd58ddef
GernotMaier <gernot.maier@desy.de>
2026-08-07 11:11:44 +0200

    update schema

--- src/simtools/job_execution/bias_curve_submissions.py
+++ src/simtools/job_execution/bias_curve_submissions.py
@@ -125,7 +125,6 @@ def _scan_config(curve_name, telescope, args):
                     args.get("trigger_thresholds"),
                 )
             ],
-            "job_grid_updates": {"telescope": telescope},
         },
     }
 
