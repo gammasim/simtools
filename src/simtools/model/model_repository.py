27fd46d3c4db39559b672247c2f2b74224a7cb5a
GernotMaier <gernot.maier@desy.de>
2026-08-19 16:53:26 +0200

    Recursively merging of simulation models

--- src/simtools/model/model_repository.py
+++ src/simtools/model/model_repository.py
@@ -215,6 +215,7 @@ def generate_new_production(model_version, simulation_models_path, setting_workf
     changes, base_model_version = _get_changes_to_production(
         modification_dict, simulation_models_path, update_type
     )
+    model_parameter_changes = modification_dict.get("changes", {})
 
     _apply_changes_to_production_tables(
         changes,
@@ -225,7 +226,7 @@ def generate_new_production(model_version, simulation_models_path, setting_workf
     )
 
     _apply_changes_to_model_parameters(
-        changes,
+        model_parameter_changes,
         simulation_models_path,
         setting_workflows_git_tag,
         setting_workflows_git_repository,
@@ -760,6 +761,12 @@ def _create_new_model_parameter_entry(telescope, param, param_data, simulation_m
             param_data["value"] = [param_data["value"]] * len(json_data["value"])
         param_data["meta_parameter"] = json_data.get("meta_parameter", False)
 
+    target_file = param_dir / f"{param}-{param_data['version']}.json"
+    if target_file.exists():
+        _validate_existing_model_parameter_file(target_file, telescope, param, param_data)
+        _logger.info("Model parameter file already matches requested version: '%s'.", target_file)
+        return
+
     writer.ModelDataWriter.write_model_parameter(
         parameter_name=param,
         value=param_data["value"],
@@ -771,6 +778,28 @@ def _create_new_model_parameter_entry(telescope, param, param_data, simulation_m
         meta_parameter=param_data.get("meta_parameter", False),
         model_parameter_schema_version=param_data.get("model_parameter_schema_version", None),
     )
+
+
+def _validate_existing_model_parameter_file(target_file, telescope, param, param_data):
+    """Validate an existing generated model parameter file before reusing it."""
+    existing_data = ascii_handler.collect_data_from_file(target_file)
+    expected_data = {
+        "parameter": param,
+        "instrument": telescope,
+        "parameter_version": param_data["version"],
+        "value": param_data["value"],
+        "unit": param_data.get("unit"),
+    }
+    mismatches = {
+        key: (existing_data.get(key), value)
+        for key, value in expected_data.items()
+        if existing_data.get(key) != value
+    }
+    if mismatches:
+        raise ValueError(
+            f"Existing model parameter file '{target_file}' does not match the requested "
+            f"value for '{telescope} - {param}': {mismatches}"
+        )
 
 
 def _get_latest_model_parameter_file(directory, parameter, max_version):
