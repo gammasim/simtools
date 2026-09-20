32af9843dea41eb1fafc7bf4c5e90d7895bd2992
github-action-benchmark <github@users.noreply.github.com>
2026-09-20 05:34:07 +0000

    add simtools CI test benchmarks (customSmallerIsBetter) benchmark result for 703c0a6bb9ff091b4228541d97a3cf9551dfac87

--- dev/test-resources/data.js
+++ dev/test-resources/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789794863988,
+  "lastUpdate": 1789882446255,
   "repoUrl": "https://github.com/gammasim/simtools",
   "entries": {
     "simtools CI test benchmarks": [
@@ -26934,6 +26934,46 @@ window.BENCHMARK_DATA = {
           {
             "name": "unit-session / peak_rss_mib",
             "value": 420.62890625,
+            "unit": "peak_rss_mib",
+            "extra": "outcome=exit_status=0 | python=3.14.7 | runner=Linux/X64 | runner_image=ubuntu24/20260907.300.1 | container=None | sample_interval_s=0.2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Gernot Maier",
+            "username": "GernotMaier",
+            "email": "gernot.maier@desy.de"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "703c0a6bb9ff091b4228541d97a3cf9551dfac87",
+          "message": "Bugfix in HTCondor submission: enforce bining of simtools from container image (#2544)\n\n* Bugfix in HTCondor submission: enforce bining of simtools from container image",
+          "timestamp": "2026-09-18T14:29:09Z",
+          "url": "https://github.com/gammasim/simtools/commit/703c0a6bb9ff091b4228541d97a3cf9551dfac87"
+        },
+        "date": 1789882431322,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "unit-session / wall_time_s",
+            "value": 71.61137826399997,
+            "unit": "wall_time_s",
+            "extra": "outcome=exit_status=0 | python=3.14.7 | runner=Linux/X64 | runner_image=ubuntu24/20260907.300.1 | container=None | sample_interval_s=0.2"
+          },
+          {
+            "name": "unit-session / cpu_time_s",
+            "value": 74.41999999999999,
+            "unit": "cpu_time_s",
+            "extra": "outcome=exit_status=0 | python=3.14.7 | runner=Linux/X64 | runner_image=ubuntu24/20260907.300.1 | container=None | sample_interval_s=0.2"
+          },
+          {
+            "name": "unit-session / peak_rss_mib",
+            "value": 420.69140625,
             "unit": "peak_rss_mib",
             "extra": "outcome=exit_status=0 | python=3.14.7 | runner=Linux/X64 | runner_image=ubuntu24/20260907.300.1 | container=None | sample_interval_s=0.2"
           }
